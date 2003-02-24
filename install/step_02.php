5c2ec04e65d97e14a12a233a3963c8e9d56cba6d
Blair Robertson <brobertson@squiz.net>
2003-02-24 06:46:45 +0000

    Now checks to see whether the root folder has been installed before trying to setup the default set of assets

--- install/step_02.php
+++ install/step_02.php
@@ -19,38 +19,43 @@ include_once(SQ_INCLUDE_PATH.'/package_manager.inc');
 $pm = new Package_Manager('__core__');
 if (!$pm->updatePackageDetails()) exit(1);
 
-
 // Firstly let's create some Assets that we require to run
 
-$GLOBALS['SQ_RESOLVE']->am->includeAsset('root_folder');
-$root_folder = new Root_Folder();
-$assetid = $root_folder->create();
-pre_echo('Asset Id : '.$assetid);
-if ($assetid != 1) {
-	trigger_error('Major Problem: The new Root Folder Asset was not given assetid #1. This needs to be fixed by You, before the installation/upgrade can be completed', E_USER_ERROR);
-}
-pre_echo($root_folder);
+$root_folder = &$GLOBALS['SQ_RESOLVE']->am->getAsset(1, 'root_folder', true);
+// if there is no root folder assume that this section hasn't been run
+if (is_null($root_folder)) {
+
+	$GLOBALS['SQ_RESOLVE']->am->includeAsset('root_folder');
+	$root_folder = new Root_Folder();
+	$assetid = $root_folder->create();
+	pre_echo('Asset Id : '.$assetid);
+	if ($assetid != 1) {
+		trigger_error('Major Problem: The new Root Folder Asset was not given assetid #1. This needs to be fixed by You, before the installation/upgrade can be completed', E_USER_ERROR);
+	}
+	pre_echo($root_folder);
 
 
-$GLOBALS['SQ_RESOLVE']->am->includeAsset('root_user');
-$root_user = new Root_User();
-$assetid = $root_user->create('root', 'root', 'Root', 'User');
-pre_echo('Root User Asset Id : '.$assetid);
+	$GLOBALS['SQ_RESOLVE']->am->includeAsset('root_user');
+	$root_user = new Root_User();
+	$assetid = $root_user->create('root', 'root', 'Root', 'User');
+	pre_echo('Root User Asset Id : '.$assetid);
 
 
-$GLOBALS['SQ_RESOLVE']->am->includeAsset('trash_folder');
-$trash_folder = new Trash_Folder();
-$assetid = $trash_folder->create();
-$root_folder->createLink($trash_folder, SQ_LINK_EXCLUSIVE);
-pre_echo('Trash Asset Id : '.$assetid);
+	$GLOBALS['SQ_RESOLVE']->am->includeAsset('trash_folder');
+	$trash_folder = new Trash_Folder();
+	$assetid = $trash_folder->create();
+	$root_folder->createLink($trash_folder, SQ_LINK_EXCLUSIVE);
+	pre_echo('Trash Asset Id : '.$assetid);
 
 
 
-// Now just create some useful folders 
-$users_folder = new Folder();
-$users_folder->create('Users');
-$root_folder->createLink($users_folder, SQ_LINK_UNITE);
-$users_folder->createLink($root_user,   SQ_LINK_UNITE);
+	// Now just create some useful folders 
+	$users_folder = new Folder();
+	$users_folder->create('Users');
+	$root_folder->createLink($users_folder, SQ_LINK_UNITE);
+	$users_folder->createLink($root_user,   SQ_LINK_UNITE);
+
+}// end if
 
 exit();
 
