adfafe62d49f47135f8546c860d30cb457cbc8c1
Christopher Smith <csmith@squiz.com.au>
2003-04-07 01:30:41 +0000

    This commit was manufactured by cvs2svn to create tag 'mysource_2-7-2'.

--- check_refs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# A simple grep to check to see that all 'get...' calls to 
-# the system object are being called by reference
-
-
-grep -rsn "[^&]\$GLOBALS\[.\?SQ_SYSTEM.\?\]->get" *
-grep -rsn "[^&]\$GLOBALS\[.\?SQ_SYSTEM.\?\]->db;" *
-grep -rsn "[^&]\$GLOBALS\[.\?SQ_SYSTEM.\?\]->am->getAsset(" *
-
