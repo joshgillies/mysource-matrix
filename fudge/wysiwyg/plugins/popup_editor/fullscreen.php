405b825d09a20aec6c58640ac343ca8ee6eb6623
Greg Sherwood <gsherwood@squiz.net>
2003-05-02 00:49:25 +0000

    This commit was generated by cvs2svn to compensate for changes in r246, which included commits to RCS files with non-trunk default branches.

---
+++ fudge/wysiwyg/plugins/popup_editor/fullscreen.php
@@ -0,0 +1,63 @@
+<?php
+include_once('../../wysiwyg.inc');
+?>
+
+<html>
+	<head>
+		<title>Popup Editor</title>
+		<script type="text/javascript">
+			var parent_object = opener.editor_<?php echo $_REQUEST['editor_name']?>._object;
+
+			function _CloseOnEsc() {
+				if (event.keyCode == 27) { window.close(); return; }
+			}
+
+			function update_parent() {
+				parent_object.setHTML(editor_popup.getHTML());
+			}
+
+			function resize_editor() {
+				var newHeight;
+				if (document.all) {
+					// IE
+					newHeight = document.body.offsetHeight - editor_popup._toolbar.offsetHeight;
+					if (newHeight < 0) { newHeight = 0; }
+				} else {
+					// Gecko
+					newHeight = window.innerHeight - editor_popup._toolbar.offsetHeight;
+				}
+				editor_popup._textArea.style.height = editor_popup._iframe.style.height = newHeight + "px";
+			}
+
+			function init() {
+				resize_editor();
+				setTimeout(function() {
+					editor_popup.setMode(parent_object._mode);
+					editor_popup.setHTML(parent_object.getHTML());
+
+					// continuously update parent editor window
+					setInterval(update_parent, 1000);
+
+					// setup event handlers
+					document.body.onkeypress = _CloseOnEsc;
+					window.onresize = resize_editor;
+				}, 333); // give it some time to meet the new frame
+			}
+		</script>
+	</head>
+	<body bgcolor="#CCCCCC" scroll="no" marginwidth="0" marginheight="0" leftmargin="0" topmargin="0" onload="init()">
+		<form style="margin: 0px; border: 1px solid; border-color: threedshadow threedhighlight threedhighlight threedshadow;">
+			<?php
+			$wysiwyg = new wysiwyg('popup','../../');
+			$wysiwyg->set_stylesheet('http://beta.squiz.net/nswrl/__data/site/design/1/images/styles.css');
+			$wysiwyg->set_width('100%');
+			$wysiwyg->set_height('100%');
+			foreach (explode('|',$_REQUEST['editor_plugins']) as $name) {
+				if (trim($name) == '') continue;
+				$wysiwyg->add_plugin($name);
+			}
+			$wysiwyg->paint();
+			?>
+		</form>
+	</body>
+</html>
