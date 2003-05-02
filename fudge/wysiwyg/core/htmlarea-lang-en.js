30c9c38aafee3c04433364e73612c948623b3fb2
Greg Sherwood <gsherwood@squiz.net>
2003-05-02 00:53:24 +0000

    core WYSIWYG js files

---
+++ fudge/wysiwyg/core/htmlarea-lang-en.js
@@ -0,0 +1,35 @@
+// I18N constants
+
+HTMLArea.I18N = {
+	tooltips: {
+		bold:           "Bold (Ctrl + B)",
+		italic:         "Italic (Ctrl + I)",
+		underline:      "Underline (Ctrl + U)",
+		strikethrough:  "Strikethrough (Ctrl + S)",
+		subscript:      "Subscript",
+		superscript:    "Superscript",
+		justifyleft:    "Justify Left (Ctrl + J)",
+		justifycenter:  "Justify Center (Ctrl + E)",
+		justifyright:   "Justify Right (Ctrl + R)",
+		justifyfull:    "Justify Full (Ctrl + J)",
+		orderedlist:    "Ordered List",
+		unorderedlist:  "Bulleted List",
+		outdent:        "Decrease Indent",
+		indent:         "Increase Indent",
+		forecolor:      "Font Color",
+		backcolor:      "Background Color",
+		removeformat:   "Remove Formatting",
+		selectall:      "Select All (Ctrl + A)",
+		undo:           "Undo (Ctrl + Z)",
+		redo:           "Redo (Ctrl + Y)",
+		horizontalrule: "Horizontal Rule",
+		createlink:     "Insert Web Link",
+		insertimage:    "Insert Image",
+		inserttable:    "Insert Table",
+		htmlmode:       "Toggle HTML Source",
+		popupeditor:    "Enlarge Editor",
+		about:          "About this editor",
+		help:           "Help using editor",
+		textindicator:  "Current style"
+	}
+};
