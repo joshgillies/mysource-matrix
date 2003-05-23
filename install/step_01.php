14d104adbb70220504b65f0fd9e924803e60e7f8
Blair Robertson <brobertson@squiz.net>
2003-05-23 02:14:31 +0000

    Added fudge path to step 1 of install

--- install/step_01.php
+++ install/step_01.php
@@ -4,13 +4,15 @@ define('SQ_SYSTEM_ROOT',   dirname(dirname(realpath(__FILE__))));
 define('SQ_INCLUDE_PATH',  SQ_SYSTEM_ROOT.'/core/include');
 define('SQ_LIB_PATH',      SQ_SYSTEM_ROOT.'/core/lib');
 define('SQ_DATA_PATH',     SQ_SYSTEM_ROOT.'/data');
+define('SQ_FUDGE_PATH',    SQ_SYSTEM_ROOT.'/fudge');
 
 require_once SQ_INCLUDE_PATH.'/resolve_object.inc';
 require_once SQ_INCLUDE_PATH.'/config.inc';
 
-define('SQ_CONF_DB_DSN',           'mysql://root@localhost/blair_resolve');
+#define('SQ_CONF_DB_DSN', 'mysql://root@localhost/blair_resolve');
+define('SQ_CONF_DB_DSN', 'pgsql://brobertson@unix+localhost/blair_resolve');
 define('SQ_CONF_SYSTEM_ROOT_URLS', 'http://beta.squiz.net/blair');
-#define('SQ_CONF_PEAR_PATH', '/usr/local/pear/share/pear');
+define('SQ_CONF_PEAR_PATH', SQ_SYSTEM_ROOT.'/php_includes');
 
 // Need to chmod cache and data directories
 
