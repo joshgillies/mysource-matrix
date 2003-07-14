957a2ae776c2c64242a4842817d693a909ff99ad
Blair Robertson <brobertson@squiz.net>
2003-07-14 02:21:53 +0000

    Added E_ALL to top of file

--- install/step_01.php
+++ install/step_01.php
@@ -9,6 +9,7 @@
 * @package Resolve
 */
 
+error_reporting(E_ALL);
 define('SQ_SYSTEM_ROOT',   dirname(dirname(realpath(__FILE__))));
 define('SQ_INCLUDE_PATH',  SQ_SYSTEM_ROOT.'/core/include');
 define('SQ_LIB_PATH',      SQ_SYSTEM_ROOT.'/core/lib');
