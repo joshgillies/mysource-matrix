0f02bc1bcf9146ecad885b4b2a3389704105f010
Greg Sherwood <gsherwood@squiz.net>
2003-07-22 00:45:02 +0000

    Moved default email to conf setting and added tech email as well. Also got emails sent with internal messages to come from webmaster@HTTP_HOST if no default email is set in the conf

--- install/step_01.php
+++ install/step_01.php
@@ -19,12 +19,14 @@ define('SQ_FUDGE_PATH',    SQ_SYSTEM_ROOT.'/fudge');
 require_once SQ_INCLUDE_PATH.'/resolve_object.inc';
 require_once SQ_INCLUDE_PATH.'/config.inc';
 
-#define('SQ_CONF_DB_DSN', 'mysql://root@localhost/blair_resolve');
-define('SQ_CONF_DB_DSN', 'pgsql://brobertson@unix+localhost/blair_resolve');
-define('SQ_CONF_SYSTEM_ROOT_URLS', 'http://beta.squiz.net/blair');
+define('SQ_CONF_DB_DSN', 'mysql://root@localhost/greg_matrix');
+#define('SQ_CONF_DB_DSN', 'pgsql://brobertson@unix+localhost/blair_resolve');
+define('SQ_CONF_SYSTEM_ROOT_URLS', 'http://beta.squiz.net/greg');
 define('SQ_CONF_PEAR_PATH', SQ_SYSTEM_ROOT.'/php_includes');
 define('SQ_CONF_ASSET_TREE_BASE', 64);
 define('SQ_CONF_ASSET_TREE_SIZE', 4);
+define('SQ_CONF_DEFAULT_EMAIL', 'gsherwood@squiz.net');
+define('SQ_CONF_TECH_EMAIL',    'techos@squiz.net');
 
 
 // Need to chmod cache and data directories
