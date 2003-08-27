0fc17ce9503ce92bbfec27a6799baeebba433d92
Greg Sherwood <gsherwood@squiz.net>
2003-08-27 01:42:47 +0000

    Added new status bar to the WYSIWYG - developed by interactiveTools. This lets you see which tags the cursor is nested in and allows selection of large chunks of html (try it out). Also displays warning message when in source view

--- fudge/wysiwyg/core/htmlarea-lang-en.js
+++ fudge/wysiwyg/core/htmlarea-lang-en.js
@@ -1,27 +1,40 @@
 // I18N constants
 
+// LANG: "en", ENCODING: UTF-8 | ISO-8859-1
+// Author: Mihai Bazon, <mishoo@infoiasi.ro>
+
+// FOR TRANSLATORS:
+//
+//   1. PLEASE PUT YOUR CONTACT INFO IN THE ABOVE LINE
+//      (at least a valid email address)
+//
+//   2. PLEASE TRY TO USE UTF-8 FOR ENCODING;
+//      (if this is not possible, please include a comment
+//       that states what encoding is necessary.)
+
 HTMLArea.I18N = {
+
+	// the following should be the filename without .js extension
+	// it will be used for automatically load plugin language.
+	lang: "en",
+
 	tooltips: {
-		bold:           "Bold (Ctrl + B)",
-		italic:         "Italic (Ctrl + I)",
-		underline:      "Underline (Ctrl + U)",
-		strikethrough:  "Strikethrough (Ctrl + S)",
+		bold:           "Bold",
+		italic:         "Italic",
+		underline:      "Underline",
+		strikethrough:  "Strikethrough",
 		subscript:      "Subscript",
 		superscript:    "Superscript",
-		justifyleft:    "Justify Left (Ctrl + J)",
-		justifycenter:  "Justify Center (Ctrl + E)",
-		justifyright:   "Justify Right (Ctrl + R)",
-		justifyfull:    "Justify Full (Ctrl + J)",
+		justifyleft:    "Justify Left",
+		justifycenter:  "Justify Center",
+		justifyright:   "Justify Right",
+		justifyfull:    "Justify Full",
 		orderedlist:    "Ordered List",
 		unorderedlist:  "Bulleted List",
 		outdent:        "Decrease Indent",
 		indent:         "Increase Indent",
 		forecolor:      "Font Color",
-		backcolor:      "Background Color",
-		removeformat:   "Remove Formatting",
-		selectall:      "Select All (Ctrl + A)",
-		undo:           "Undo (Ctrl + Z)",
-		redo:           "Redo (Ctrl + Y)",
+		hilitecolor:    "Background Color",
 		horizontalrule: "Horizontal Rule",
 		createlink:     "Insert Web Link",
 		insertimage:    "Insert Image",
@@ -29,7 +42,22 @@ HTMLArea.I18N = {
 		htmlmode:       "Toggle HTML Source",
 		popupeditor:    "Enlarge Editor",
 		about:          "About this editor",
-		help:           "Help using editor",
-		textindicator:  "Current style"
+		showhelp:       "Help using editor",
+		textindicator:  "Current style",
+		undo:           "Undoes your last action",
+		redo:           "Redoes your last action",
+		cut:            "Cut selection",
+		copy:           "Copy selection",
+		paste:          "Paste from clipboard"
+	},
+
+	buttons: {
+		"ok":           "OK",
+		"cancel":       "Cancel"
+	},
+
+	msg: {
+		"Path":         "Path",
+		"TEXT_MODE":    "You are in TEXT MODE.  Use the [<>] button to switch back to WYSIWIG."
 	}
 };
