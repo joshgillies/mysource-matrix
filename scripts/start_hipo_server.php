c6cb0c5ce986285c4b89cd8ad8ce82bc105f5b0b
Blair Robertson <brobertson@squiz.net>
2003-09-29 04:30:11 +0000

    Added log file to server constructor

--- scripts/start_hipo_server.php
+++ scripts/start_hipo_server.php
@@ -2,7 +2,7 @@
 /**
 * Copyright (c) 2003 - Squiz Pty Ltd
 *
-* $Id: start_hipo_server.php,v 1.4 2003/09/26 05:26:35 brobertson Exp $
+* $Id: start_hipo_server.php,v 1.5 2003/09/29 04:30:11 brobertson Exp $
 * $Name: not supported by cvs2svn $
 */
 
@@ -29,7 +29,7 @@ ini_set('include_path', $inc_dir);
 ini_set('error_log', '');
 
 require_once dirname(__FILE__).'/../core/hipo/server/hipo_server.inc';
-$server	= new HIPO_Server('localhost', 9090, dirname(__FILE__).'/server.conf');
+$server	= new HIPO_Server('localhost', 9090, dirname(__FILE__).'/server.conf', dirname(__FILE__).'/server.log');
 $server->start();
 
 ?>
