39957b9e63adf6dc6f450c163e40827737ffa628
Blair Robertson <brobertson@squiz.net>
2003-10-16 03:13:46 +0000

    OK changes to flashToJsCall() to make it work for mox/firebird, it appears that we need to get the actuall execution on a different thread from the one that the flash is calling, so what we do is set a timeout for 100 milliseconds and call the fn from there - this solves the issue of not getting pop-ups from the flash

--- core/lib/asset_map/flashExternalCall.js
+++ core/lib/asset_map/flashExternalCall.js
@@ -1,7 +1,7 @@
 /**
 * Copyright (c) 2003 - Squiz Pty Ltd
 *
-* $Id: flashExternalCall.js,v 1.10 2003/10/13 01:20:02 brobertson Exp $
+* $Id: flashExternalCall.js,v 1.11 2003/10/16 03:13:46 brobertson Exp $
 * $Name: not supported by cvs2svn $
 */
 
@@ -51,6 +51,7 @@ function flashToJsCall(arg)
 {
 
 	var str = new String(arg);
+//prompt("", "flashToJsCall args : " + str);
 	var pieces = str.match(/^([a-z_]+):\/\/(.*)$/);
 
 	switch(pieces[1]) {
@@ -75,7 +76,15 @@ function flashToJsCall(arg)
 			if (FLASH_TO_JS_CALL !== null && pieces[2] == "true") {
 				//alert("onExternalCall : " + FLASH_TO_JS_CALL.cmd);
 				//for(var i in FLASH_TO_JS_CALL.params) alert("params -> " + i + " : " + FLASH_TO_JS_CALL.params[i]);
-				FLASH_TO_JS_CALL_BACK_FNS[FLASH_TO_JS_CALL.cmd](FLASH_TO_JS_CALL.params);
+				//alert(FLASH_TO_JS_CALL_BACK_FNS[FLASH_TO_JS_CALL.cmd]);
+
+				if (navigator.userAgent.match(/MSIE/)) {
+					FLASH_TO_JS_CALL_BACK_FNS[FLASH_TO_JS_CALL.cmd](FLASH_TO_JS_CALL.params);
+				} else {
+					// because of some wierd thing with Moz/Firebird and flash we need to get this on a separate thread
+					// than this call from the flash
+					setTimeout(FLASH_TO_JS_CALL_BACK_FNS[FLASH_TO_JS_CALL.cmd], 100, FLASH_TO_JS_CALL.params);
+				}
 				// reset the storage units
 				FLASH_TO_JS_CALL = null;
 			}
