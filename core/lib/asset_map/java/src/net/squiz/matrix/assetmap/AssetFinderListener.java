adbc750e48ea01cc146fbb7a250257dfb1c28804
Marc McIntyre <mmcintyre@squiz.net>
2004-01-13 00:42:04 +0000

    A listener interface that should be implemented by any class that wants to listen for asset finder events

---
+++ core/lib/asset_map/java/src/net/squiz/matrix/assetmap/AssetFinderListener.java
@@ -0,0 +1,50 @@
+/**
+* +--------------------------------------------------------------------+
+* | Squiz.net Open Source Licence                                      |
+* +--------------------------------------------------------------------+
+* | Copyright (c), 2003 Squiz Pty Ltd (ABN 77 084 670 600).            |
+* +--------------------------------------------------------------------+
+* | This source file may be used subject to, and only in accordance    |
+* | with, the Squiz Open Source Licence Agreement found at             |
+* | http://www.squiz.net/licence.                                      |
+* | Make sure you have read and accept the terms of that licence,      |
+* | including its limitations of liability and disclaimers, before     |
+* | using this software in any way. Your use of this software is       |
+* | deemed to constitute agreement to be bound by that licence. If you |
+* | modify, adapt or enhance this software, you agree to assign your   |
+* | intellectual property rights in the modification, adaptation and   |
+* | enhancement to Squiz Pty Ltd for use and distribution under that   |
+* | licence.                                                           |
+* +--------------------------------------------------------------------+
+*
+* $Id: AssetFinderListener.java,v 1.1 2004/01/13 00:42:04 mmcintyre Exp $
+* $Name: not supported by cvs2svn $
+*/
+
+package net.squiz.matrix.assetmap;
+
+import java.util.EventListener;
+
+
+/**
+* Interface for objects that want to listen for Asset Finder Events which get fired from
+* Javascript when the user invokes an asset finder state (on/off)
+*
+* @author Marc McIntyre <mmcintyre@squiz.net>
+*
+* @see JsEventManager
+*/
+public interface AssetFinderListener extends EventListener {
+	
+	/**
+	* An event that gets invoked when the user wants to choose an asset to use as
+	* a reference to a component in the matrix system
+	*/
+	void assetFinderStarted(JsEvent e);
+
+	/**
+	* An event that gets invoked when the user cancels the asset finder mode
+	*/
+	void assetFinderStopped(JsEvent e);
+
+}//end interface
