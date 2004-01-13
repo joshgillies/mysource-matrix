ad1006636a84f4cbbff03abf1e43304065c78b81
Marc McIntyre <mmcintyre@squiz.net>
2004-01-13 00:52:25 +0000

    The class that handles socket (URL) requests to the matrix system

---
+++ core/lib/asset_map/java/src/net/squiz/matrix/assetmap/MySource.java
@@ -0,0 +1,260 @@
+/**
+* +--------------------------------------------------------------------+
+* | Squiz.net Open Source Licence                                      |
+* +--------------------------------------------------------------------+
+* | Copyright (c), 2003 Squiz Pty Ltd (ABN 77 084 670 600).            |
+* +--------------------------------------------------------------------+
+* | This source file may be used subject to, and only in accordance    |
+* | with, the Squiz Open Source Licence Agreement found at             |
+* | http://www.squiz.net/licence.                                      |
+* | Make sure you have read and accept the terms of that licence,      |
+* | including its limitations of liability and disclaimers, before     |
+* | using this software in any way. Your use of this software is       |
+* | deemed to constitute agreement to be bound by that licence. If you |
+* | modify, adapt or enhance this software, you agree to assign your   |
+* | intellectual property rights in the modification, adaptation and   |
+* | enhancement to Squiz Pty Ltd for use and distribution under that   |
+* | licence.                                                           |
+* +--------------------------------------------------------------------+
+*
+* $Id: MySource.java,v 1.1 2004/01/13 00:52:25 mmcintyre Exp $
+* $Name: not supported by cvs2svn $
+*/
+
+package net.squiz.matrix.assetmap;
+
+import net.squiz.matrix.xml.XML;
+
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.MalformedURLException;
+import javax.xml.parsers.DocumentBuilder;
+import org.w3c.dom.*;
+
+import java.util.HashMap;
+import java.util.Map;
+import javax.xml.transform.stream.StreamSource;
+
+import org.xml.sax.SAXException;
+import java.io.ByteArrayOutputStream;
+import java.util.StringTokenizer;
+import java.io.PrintWriter;
+import java.io.IOException;
+
+import java.security.Permission;
+import java.security.AccessControlException;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+
+
+/**
+* An interface to the MySource system. There is only one interface, 
+* accessible through the static method <code>getInstance()</code>.
+*
+* <p>
+* Clients of this class call <code>doRequest</code> to send messages
+* to MySource, which is controlled on that side by 
+* <code>core/lib/asset_map/asset_map.inc</code>.
+* </p>
+* 
+* <p>
+* This would be where we broadcast our pirate signal and hack into 
+* the Matrix...
+* </p>
+*
+* @author Marc McIntyre <mmcintyre@squiz.net>
+*/
+public class MySource {
+	
+	/**
+	* The unique instance of the MySource object.
+	*/
+	private static MySource instance = null;
+
+	/**
+	* base URL for the Matrix system
+	*/
+	private URL baseURL;
+	
+	
+	/**
+	* execution URL - URL for talking to the asset map
+	*/
+	private URL execURL;
+
+	/**
+	* A hashmap of the icons that are general to the asset map
+	*/
+	private HashMap icons = new HashMap();
+
+
+	/** 
+	* Gets the unique instance of the MySource object. 
+	* 
+	* @return the <code>MySource</code> singleton instance
+	*/
+	public static MySource getInstance() {
+		if (MySource.instance == null) {
+			AssetMap am = AssetMap.getApplet();
+
+			URL base = am.getBaseURL();
+			String basePath = base.getProtocol() + "://" + base.getHost() + MySource.getPath(base.getPath());
+			String execPath = basePath + "/_admin/?SQ_BACKEND_PAGE=asset_map_request";
+			
+			basePath = "http://beta.squiz.net/marc_matrix";
+			execPath = "http://beta.squiz.net/marc_matrix/_admin/?SQ_BACKEND_PAGE=asset_map_request";
+
+
+			MySource.instance = new MySource(basePath, execPath);
+		}
+		return MySource.instance;
+
+	}//end getInstance()
+
+
+	public static String getPath(String path) {
+
+		int length = 5;
+		String [] specialPaths = new String[length];
+		specialPaths[0] = "/_admin";
+		specialPaths[1] = "/_edit";
+		specialPaths[2] = "/__lib";
+		specialPaths[3] = "/__fudge";
+		specialPaths[4] = "/__data";
+	
+		String realPath = null;
+
+		for (int i = 0; i < length; i++) {
+			int pos = path.indexOf(specialPaths[i]);
+			// if we have something here...
+			if (pos != -1) {
+				realPath = path.substring(0, pos);
+			}
+		}
+
+		return realPath;
+	}
+
+	/**
+	* Constructor.
+	*
+	* @param baseURLString The base URL for the Matrix system
+	* @param execURLString The URL for the PHP asset map object of the Matrix
+	*/
+	private MySource(String baseURLString, String execURLString) {
+		try {
+			baseURL = new URL(baseURLString);
+			execURL = new URL(execURLString);
+		} catch (MalformedURLException mue) {
+			System.err.println ("Could not set url : " + mue.getMessage());
+			baseURL = null;
+			execURL = null;
+		}
+
+	}//end constructor
+
+
+	/** 
+	* Returns the base URL for the Matrix system.
+	* 
+	* @return the base URL for the Matrix system, or <code>null</code> if we don't have one.
+	*/
+	public URL getBaseURL() {
+		if (baseURL != null) {
+			try {
+				return new URL(baseURL.toString());
+			} catch (MalformedURLException mue) {
+				return null;
+			}
+		}
+		return null;
+
+	}//end getBaseURL()
+
+
+	/**
+	* Returns an <code>javax.swing.Icon</code> for a web path
+	*
+	* The Web path should be relative to <code>core/lib/web</code>
+	*/
+	public Icon getIcon(String webPath) {
+		
+		// don't keep hitting the server for each icon every time it is needed
+		if (!(icons.containsKey(webPath))) {
+			URL url = null;
+			try {
+				url = new URL(baseURL + "__lib/images/icons" + webPath);
+			} catch (MalformedURLException mfue) {
+				System.err.println("There does not seem to be an icon for web path " + webPath + ": " + mfue.getMessage());
+			}
+			icons.put(webPath, new ImageIcon(url));
+		}
+		return (Icon) icons.get(webPath);
+
+	}//end getIcon()
+
+
+	/**
+	* Performs an XML request to <code>asset_map.inc</code>.
+	* 
+	* @param request The String of the XML request
+	* 
+	* @return	the XML DOM <code>Document</code> response from the Matrix
+	* @throws IOException	if connection can't be opened, XML parse error, or an error given by the Matrix
+	*/
+	public Document doRequest(String request) throws IOException {
+		DG.speedCheck();
+
+		if (execURL == null) {
+			throw new IOException("ExecURL is null");
+		}
+
+		URLConnection conn = null;
+		try {
+			conn = execURL.openConnection();
+			conn.setUseCaches(false);
+			conn.setDoOutput(true);
+			ByteArrayOutputStream byteStream = new ByteArrayOutputStream(512); // Grows if necessary
+			PrintWriter out = new PrintWriter(byteStream, true);
+			String postData = request;
+			out.print(postData);
+			out.flush();
+
+			conn.setRequestProperty("Content-Length", String.valueOf(byteStream.size()));
+			conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+
+			byteStream.writeTo(conn.getOutputStream());
+		} catch (IOException ioe) {
+			throw new IOException("error while getting request connection : " + ioe.getMessage());
+		} catch (AccessControlException ace) {
+			Permission perm = ace.getPermission();
+			throw new AccessControlException("Permission Exception while connecting to '" + perm.getName() + "' " + perm.toString() + ". The Following Action could not Performed: " + perm.getActions());
+		}
+
+		Document document = null;
+		DocumentBuilder builder = null;
+
+		try {
+			builder = XML.getParser();
+		} catch (SAXException se) {
+			throw new IOException("Could not do the mysource request: " + se.getMessage());
+		}
+
+		
+		try {
+			document = builder.parse(conn.getInputStream());
+		} catch (SAXException se) {
+			// we will get into here if no XML is returned from conn (the DB daemon hasn't started?)
+			throw new IOException("error while parsing : \n" + se.getMessage());
+		} 
+
+		if (document.getDocumentElement().getTagName().equals("error")) {
+			Element errorElement = (Element)document.getDocumentElement();
+			throw new IOException("error while getting response - error returned: " + errorElement.getFirstChild().getNodeValue());
+		}
+		DG.speedCheck();
+		return document;
+
+	}//end doRequest()
+
+}//end class
