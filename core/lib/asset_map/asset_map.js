c4e785b39bede34238cf55b060ec4972b0a05628
Marc McIntyre <mmcintyre@squiz.net>
2004-04-05 05:06:42 +0000

    removed alot of flash specific functions. The asset map object that is used is determined by the browser

--- core/lib/asset_map/asset_map.js
+++ core/lib/asset_map/asset_map.js
@@ -17,7 +17,7 @@
 * | licence.                                                           |
 * +--------------------------------------------------------------------+
 *
-* $Id: asset_map.js,v 1.3 2004/04/01 07:04:03 mmcintyre Exp $
+* $Id: asset_map.js,v 1.4 2004/04/05 05:06:42 mmcintyre Exp $
 * $Name: not supported by cvs2svn $
 */
 
@@ -25,7 +25,6 @@
 /*--         START GLOBALS            --*/
 
 
-var ASSET_MAP_FLASH_CHECKED = false;
 var FLASH_TO_JS_CALL_BACK_FNS = {};
 var IE_FLASH_VERSION  = 6;
 var MOZ_FLASH_VERSION = 7;
@@ -57,15 +56,7 @@ function init_asset_map() {
 	if (matches = navigator.userAgent.match(/MSIE ([0-9.]+)/)) {
 		if (matches[1] < '6.0') {
 			alert('You need to use Internet Explorer 6.0 or above for the communication between the Asset Map and the Javascript');
-		} else {
-			document.write('<SCR' + 'IPT LANGUAGE=VBScript\> \n'); //FS hide this from IE4.5 Mac by splitting the tag
-			document.write('on error resume next \n');
-			document.write('ASSET_MAP_FLASH_CHECKED = ( IsObject(CreateObject("ShockwaveFlash.ShockwaveFlash.' + IE_FLASH_VERSION + '")))\n');
-			document.write('</SCR' + 'IPT\> \n');
-			if (!ASSET_MAP_FLASH_CHECKED) {
-				alert('You need to use to have version ' + IE_FLASH_VERSION + ' of Flash installed for the communication between the Asset Map and the Javascript');
-			}
-		}
+		} 
 
 	} else {
 
@@ -78,47 +69,17 @@ function init_asset_map() {
 
 			} else if (matches[1] == 'Mozilla' && matches[2] < '1.4') {
 				alert('You need to use Mozilla 1.4 or above for the communication between the Asset Map and the Javascript');
-
-			} else {
-
-				var plugin = (navigator.mimeTypes && navigator.mimeTypes["application/x-shockwave-flash"]) ? navigator.mimeTypes["application/x-shockwave-flash"].enabledPlugin : 0;
-				if (plugin) {
-					var words = navigator.plugins["Shockwave Flash"].description.split(" ");
-					for (var i = 0; i < words.length; ++i)
-					{
-						if (isNaN(parseInt(words[i]))) continue;
-						var MM_PluginVersion = words[i]; 
-					}
-					ASSET_MAP_FLASH_CHECKED = (MM_PluginVersion >= MOZ_FLASH_VERSION);
-				}
-
-				if (!ASSET_MAP_FLASH_CHECKED) {
-					alert('You need to use to have version ' + MOZ_FLASH_VERSION + ' of Flash installed for the communication between the Asset Map and the Javascript');
-				}
-
-			}// end if
+			}
 
 		// we don't know about this browser, ah well may as well give it a go ...
 		} else {
 			alert('You are using an untested browser there is no guarantee that the communication between the Asset Map and the Javascript will be successful');
-			ASSET_MAP_FLASH_CHECKED = true;
 
 		}// end if
 
 	}// end if
 }//end init_asset_map()
 
-
-/**
-* Handle all the the FSCommand messages in a Flash movie
-*/
-function asset_map_DoFSCommand(command, args)
-{
-	//var asset_mapObj = flash_InternetExplorer ? asset_map : document.asset_map;
-	if (command == "flashToJsCall") {
-		flashToJsCall(args);
-	}
-}//end asset_map_DoFSCommand()
 
 
 /*
@@ -134,9 +95,17 @@ function asset_map_DoFSCommand(command, args)
 //}//end reload_assets()
 
 
+function getJavaAppletObject() {
+	if (is_ie) {
+		return document.getElementById('sq_asset_map');
+	} else {
+		return document.embeds[0];
+	}
+}
+
 
 function reload_assets(assetids) {
-	var asset_mapObj = document.getElementById('sq_asset_map');
+	var asset_mapObj = getJavaAppletObject();
 	params = new Array();
 	params["assetids"] = assetids;
 	jsToJavaCall(asset_mapObj, "reload_assets", "assetsReloaded", params);
@@ -148,13 +117,11 @@ function reload_assets(assetids) {
 */
 function reload_asset(assetid)
 {
-	var asset_mapObj = document.getElementById("sq_asset_map");
+	var asset_mapObj = getJavaAppletObject();
 	params = new Array();
 	params["assetids"] = assetid + ",";
 	jsToJavaCall(asset_mapObj, "reload_assets", "assetsReloaded", params);
-	//alert("Reload Assetid : " + assetid);
-	//jsToFlashCall(asset_mapObj, 'reload_asset', {assetid: assetid});
-
+	
 }//end reload_asset()
 
 
@@ -191,17 +158,6 @@ function add_messages(xml)
 	jsToFlashCall(asset_mapObj, 'add_message', {msgs_xml: xml});
 
 }//end add_messages()
-
-
-/* 
-* Reload the passed assetid in the flash
-*/
-function asset_map_popup(params)
-{
-	var popup_win = window.open(params.url, 'sq_asset_map_popup', 'toolbar=0,menubar=0,location=0,status=0,scrollbars=1,resizable=1,width=650,height=400');
-
-}//end asset_map_popup()
-registerFlashToJsCall('asset_map_popup', asset_map_popup);
 
 
 
@@ -250,15 +206,8 @@ function asset_finder_change_btn_press(name, safe_name, type_codes, done_fn)
 		ASSET_FINDER_FIELD_NAME = name;
 		ASSET_FINDER_FIELD_SAFE_NAME = safe_name;
 		
-		// simple temporary hack to find if we are using the java or the flash
-		if (null != (document.getElementById('asset_map'))) {
-			flash_asset_finder_start(type_codes);
-		} else if (null != (document.getElementById('sq_asset_map'))) {
-			asset_finder_start('asset_finder_done', type_codes);
-		} else {
-			alert("Could not find the asset map");
-			return false;
-		}
+		asset_finder_start('asset_finder_done', type_codes);
+		
 
 		ASSET_FINDER_OBJ.set_button_value(ASSET_FINDER_FIELD_SAFE_NAME + '_change_btn', 'Cancel');
 
@@ -286,22 +235,12 @@ function asset_finder_done(params, label, url)
 {
 	if (ASSET_FINDER_FIELD_NAME == null) return;
 
-	// we are in flash
-	if (params.assetid) {
-		// if we get a -1 they cancelled, do nothing
-		if (params.assetid != -1) {
-			ASSET_FINDER_OBJ.set_hidden_field(ASSET_FINDER_FIELD_NAME + '[assetid]', params.assetid);
-			ASSET_FINDER_OBJ.set_hidden_field(ASSET_FINDER_FIELD_NAME + '[url]', params.url);
-			ASSET_FINDER_OBJ.set_text_field(ASSET_FINDER_FIELD_SAFE_NAME + '_label', (params.assetid == 0) ? '' : params.label + ' (Id : #' + params.assetid + ')');
-		}
-	} else {
-		var assetid = params;
-		// we are in java, params is actually the assetid
-		if (assetid != -1) {
-			ASSET_FINDER_OBJ.set_hidden_field(ASSET_FINDER_FIELD_NAME + '[assetid]',assetid);
-			ASSET_FINDER_OBJ.set_hidden_field(ASSET_FINDER_FIELD_NAME + '[url]', url);
-			ASSET_FINDER_OBJ.set_text_field(ASSET_FINDER_FIELD_SAFE_NAME + '_label', (assetid == 0) ? '' : label + ' (Id : #' + assetid + ')');
-		}
+	var assetid = params;
+	// we are in java, params is actually the assetid
+	if (assetid != -1) {
+		ASSET_FINDER_OBJ.set_hidden_field(ASSET_FINDER_FIELD_NAME + '[assetid]',assetid);
+		ASSET_FINDER_OBJ.set_hidden_field(ASSET_FINDER_FIELD_NAME + '[url]', url);
+		ASSET_FINDER_OBJ.set_text_field(ASSET_FINDER_FIELD_SAFE_NAME + '_label', (assetid == 0) ? '' : label + ' (Id : #' + assetid + ')');
 	}
 
 	ASSET_FINDER_OBJ.set_button_value(ASSET_FINDER_FIELD_SAFE_NAME + '_change_btn', 'Change');
@@ -323,7 +262,7 @@ function asset_finder_done(params, label, url)
 var ASSET_FINDER_CALL_BACK = null;
 function asset_finder_start(fn, type_codes)
 {
-	var asset_mapObj = document.getElementById('sq_asset_map');
+	var asset_mapObj = getJavaAppletObject();
 	var params = new Array();
 	params["callback_fn"] = fn;
 	params["type_codes"] = type_codes;
@@ -333,43 +272,17 @@ function asset_finder_start(fn, type_codes)
 }//end asset_finder_start()
 
 
-/*
-* Initialises the asset finder in the flash
-*
-* @param Function	fn			the call-back function to with the results
-* @param string		type_codes	the type codes seperated by a pipe ( | )
-*
-*/
-function flash_asset_finder_start(type_codes)
-{
-	var asset_mapObj = document.getElementById('asset_map');
-
-	registerFlashToJsCall('asset_finder_done', asset_finder_done)
-	jsToFlashCall(asset_mapObj, 'asset_finder', {action: 'start', type_codes: type_codes});
-
-}//end flash_asset_finder_start()
-
-
 /**
 * Alerts the asset map that asset finder mode has been canceled
 *
 * @access public
 */
 function asset_finder_cancel() {
-
-	// simple temporary hack to find if we are using the java or the flash
-	if (null != (document.getElementById('asset_map'))) {
-		var asset_mapObj = document.getElementById('asset_map');
-		jsToFlashCall(asset_mapObj, 'asset_finder', {action: 'cancel'});
-	} else if (null != (document.getElementById('sq_asset_map'))) {
-		var asset_mapObj = document.getElementById('sq_asset_map');
-		params = new Array();
-		jsToJavaCall(asset_mapObj, 'asset_finder', 'assetFinderStopped', params);
-	} else {
-		alert("Could not find the asset map");
-		return false;
-	}
-
+	
+	var asset_mapObj = getJavaAppletObject();
+	params = new Array();
+	jsToJavaCall(asset_mapObj, 'asset_finder', 'assetFinderStopped', params);
+	
 }//end asset_finder_cancel()
 
 
@@ -422,100 +335,6 @@ ASSET_FINDER_OTHER_ONUNLOAD = (window.onunload) ? window.onunload : new Function
 window.onunload = asset_finder_onunload;
 
 
-  ////////////////////////////////
- //  Asset Map External Calls  //
-////////////////////////////////
-
-
-/**
-* This function works in partnership with the ExternalCall flash class.
-* What it allows is the execution of commands inside the flash that
-* you are unable to do normally.
-*
-*/
-function jsToFlashCall(swObj, cmd, params)
-{
-	if (!ASSET_MAP_FLASH_CHECKED) return;
-
-//	alert('js to flash calling with ' + swObj + "/" + cmd + "/" + params);
-	swObj.SetVariable('_root.external_call.cmd', cmd);
-
-	for(i in params) {
-		var name  = escape(i);
-		var value = escape(params[i]);
-		swObj.SetVariable('_root.external_call.add_param', name + '=' + value);
-	}// end for
-
-	swObj.SetVariable('_root.external_call.exec', 'true');
-
-}// end jsToFlashCall()
-
-/**
-* This function works in partnership with the ExternalCall flash class.
-* What it allows is the execution of commands inside the flash that
-* you are unable to do normally.
-*
-*/
-function registerFlashToJsCall(cmd, fn)
-{
-	FLASH_TO_JS_CALL_BACK_FNS[cmd] = fn;
-
-}// end flashToJsCall()
-
-
-/**
-* This function works in partnership with the ExternalCall flash class.
-* What it allows is the execution of commands inside the flash that
-* you are unable to do normally.
-*
-*/
-var FLASH_TO_JS_CALL = null;
-function flashToJsCall(arg)
-{
-
-	var str = new String(arg);
-	var pieces = str.match(/^([a-z_]+):\/\/(.*)$/);
-
-	switch(pieces[1]) {
-		case "cmd" :
-			if (FLASH_TO_JS_CALL !== null) return;
-			var cmd = pieces[2];
-			if (FLASH_TO_JS_CALL_BACK_FNS[cmd] == undefined) {
-				alert('Command "' + cmd + '" has not been registered, unable to perform flashToJsCall');
-				return;
-			}
-			FLASH_TO_JS_CALL = {cmd: cmd, params: {}}
-			break;
-		case "add_param" :
-			if (FLASH_TO_JS_CALL !== null) {
-				var tmp = pieces[2].split("=", 2);
-				var name  = unescape(tmp[0]);
-				var value = unescape(tmp[1]);
-				FLASH_TO_JS_CALL.params[name] = value;
-			}
-			break;
-		case "exec" :
-			if (FLASH_TO_JS_CALL !== null && pieces[2] == "true") {
-				//alert("onExternalCall : " + FLASH_TO_JS_CALL.cmd);
-				//for(var i in FLASH_TO_JS_CALL.params) alert("params -> " + i + " : " + FLASH_TO_JS_CALL.params[i]);
-				//alert(FLASH_TO_JS_CALL_BACK_FNS[FLASH_TO_JS_CALL.cmd]);
-
-				if (navigator.userAgent.match(/MSIE/)) {
-					FLASH_TO_JS_CALL_BACK_FNS[FLASH_TO_JS_CALL.cmd](FLASH_TO_JS_CALL.params);
-				} else {
-					// because of some wierd thing with Moz/Firebird and flash we need to get this on a separate thread
-					// than this call from the flash
-					setTimeout(FLASH_TO_JS_CALL_BACK_FNS[FLASH_TO_JS_CALL.cmd], 100, FLASH_TO_JS_CALL.params);
-				}
-				// reset the storage units
-				FLASH_TO_JS_CALL = null;
-			}
-
-		break;
-	}// end switch
-}// end flashToJsCall()
-
-
 /**
 * Makes an extenal call to java
 *
@@ -526,10 +345,11 @@ function flashToJsCall(arg)
 */
 function jsToJavaCall(asset_mapObj, type, command, params)
 {
-	//alert(params);
-	// it just doesn't get much easier than this
-
 	params = var_serialise(params);
 	asset_mapObj.jsToJavaCall(type, command, params);
 
 }//end jsToJavaCall();
+
+
+
+
