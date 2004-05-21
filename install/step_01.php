35ec4e15eacb4bed5a9c64be222db805e1a5ce3a
Greg Sherwood <gsherwood@squiz.net>
2004-05-21 04:53:55 +0000

    Removed blairs info as the default for the config

--- install/step_01.php
+++ install/step_01.php
@@ -18,7 +18,7 @@
 * | licence.                                                           |
 * +--------------------------------------------------------------------+
 *
-* $Id: step_01.php,v 1.26 2004/05/19 04:05:18 lwright Exp $
+* $Id: step_01.php,v 1.27 2004/05/21 04:53:55 gsherwood Exp $
 * $Name: not supported by cvs2svn $
 */
 
@@ -45,16 +45,17 @@ $GLOBALS['SQ_INSTALL'] = true;
 require_once SQ_INCLUDE_PATH.'/mysource_object.inc';
 require_once SQ_INCLUDE_PATH.'/system_config.inc';
 
-#define('SQ_CONF_DB_DSN', 'mysql://root@localhost/dom_resolve');
-define('SQ_CONF_DB_DSN', 'pgsql://brobertson@unix+localhost/blair_resolve');
-define('SQ_CONF_DB2_DSN', 'pgsql://brobertson_secondary@unix+localhost/blair_resolve');
-define('SQ_CONF_SYSTEM_ROOT_URLS', 'woteva.uk.squiz.net/blair_resolve');
+#define('SQ_CONF_DB_DSN',  'mysql://username:password@localhost/database_name');
+#define('SQ_CONF_DB2_DSN', 'mysql://username_2:password@localhost/database_name');
+#define('SQ_CONF_DB_DSN',  'pgsql://username:password@unix()/database_name');
+#define('SQ_CONF_DB2_DSN', 'pgsql://username_2:password@unix()/database_name');
+define('SQ_CONF_SYSTEM_ROOT_URLS', 'www.example.com');
 define('SQ_CONF_PEAR_PATH', SQ_SYSTEM_ROOT.'/php_includes');
 define('SQ_CONF_ASSET_TREE_BASE', 36);
 define('SQ_CONF_ASSET_TREE_SIZE', 4);
 define('SQ_CONF_ROLLBACK_ENABLED', '0');
-define('SQ_CONF_DEFAULT_EMAIL', 'brobertson@squiz.net');
-define('SQ_CONF_TECH_EMAIL',    'brobertson@squiz.net');
+define('SQ_CONF_DEFAULT_EMAIL', 'support@squiz.net');
+define('SQ_CONF_TECH_EMAIL',    'matrix@squiz.net');
 
 trigger_error('Need to chmod cache and data directories', E_USER_NOTICE);
 
