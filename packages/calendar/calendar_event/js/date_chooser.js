0314e0f907385757e79a13ffad0af4334d759e6d
Tom Barrett <tbarrett@squiz.net>
2004-08-03 02:44:38 +0000

    Improvements to dynamic event input interface

--- packages/calendar/calendar_event/js/date_chooser.js
+++ packages/calendar/calendar_event/js/date_chooser.js
@@ -1,8 +1,9 @@
 function make2digits(num) {
-	if (num < 10)
+	if (num < 10) {
 		return "0"+num;
-	else
+	} else {
 		return num;
+	}
 		
 }//end make2digits()
 
@@ -52,7 +53,7 @@ var dateComponents = Array('_year','_month','_day');
 
 function enableDateField(name) {
 	for (i=0; i<dateComponents.length; i++) {
-		if ((elt = document.getElementById(name+dateComponents[i])) != null) {
+		if ((elt = document.getElementById(name+dateComponents[i])) !== null) {
 			elt.disabled = 0;
 		}
 	}
@@ -62,7 +63,7 @@ function enableDateField(name) {
 
 function disableDateField(name) {
 	for (i=0; i<dateComponents.length; i++) {
-		if ((elt = document.getElementById(name+dateComponents[i])) != null) {
+		if ((elt = document.getElementById(name+dateComponents[i])) !== null) {
 			elt.disabled = 1;
 		}
 	}
@@ -71,8 +72,9 @@ function disableDateField(name) {
 
 
 function enableTimeField(name) {
-	if (document.getElementById(name+'_hours') == null)
+	if (document.getElementById(name+'_hours') === null) {
 		alert("hours not found for " + name);
+	}
 	document.getElementById(name+'_hours').disabled=0;
 	document.getElementById(name+'_minutes').disabled=0;
 	document.getElementById(name+'_is_pm').disabled=0;
@@ -91,36 +93,41 @@ function disableTimeField(name) {
 
 
 function checkBox(id) {
-	if ((elt = document.getElementById(id)) != null)
+	if ((elt = document.getElementById(id)) !== null) {
 		elt.checked=1;
-	else
+	} else {
 		alert("Javascript error:  could not find box " + id + " to check");
-		
+	}		
 }//end checkBox()
 
 
 function uncheckBox(id) {
-	if ((elt = document.getElementById(id)) != null)
+	if ((elt = document.getElementById(id)) !== null) {
 		elt.checked=0;
-	else
+	} else {
 		alert("Javascript error:  could not find box " + id + " to uncheck");
-		
+	}
 }//end uncheckBox()
 
 
 function isChecked(id) {
-	if ((elt = document.getElementById(id)) != null)
+	if ((elt = document.getElementById(id)) !== null) {
 		return elt.checked;
-	else
+	} else {
 		alert("Javascript error:  could not find box " + id + " so can't look at its status");
-	return 0;
-	
+		return 0;
+	}	
 }//end isChecked()
 
 
-function validateDay(elt) {
+function dayOK(elt) {
 	dayValue = parseInt(elt.value);
-	if ((dayValue == null) || (dayValue <= 0) || (dayValue > 31)) {
+	return ((dayValue !== null) && (dayValue > 0) && (dayValue <= 31));
+}//end dayOK()
+
+
+function validateDay(elt) {
+	if (!dayOK(elt)) {
 		alert("You entered an invalid day of the month");
 		elt.focus();
 		return false;
@@ -130,46 +137,20 @@ function validateDay(elt) {
 }//end validateDay()
 
 
-function validateMinutes(elt) {
-	minutesValue = parseInt(elt.value);
-	if ((minutesValue == null) || (minutesValue < 0) || (minutesValue > 59)) {
-		alert("You entered an invalid time");
-		elt.focus();
-		return false;
-	} else {
-		return true;
-	}
-	
-}//end validateMinutes()
-
-
-function validateHours(elt) {
-	hoursValue = parseInt(elt.value);
-	if ((hoursValue < 0) && (hoursValue > 23)) {
-		alert("You entered an invalid time");
-		elt.focus();
-		return false;
-	} else if ((hoursValue > 12) && (hoursValue < 24)) {
-		elt.value = hoursValue - 12;
-		if ((pmElt = document.getElementById(elt.getAttribute('id').substring(0, elt.getAttribute('id').length-5)+'_is_pm')) != null)
-			pmElt.selectedIndex=1;
-		else
-			alert("Couldn't find " + substring(elt.getAttribute('id'), 0, elt.getAttribute('id').length-5)+'_is_pm');
-	}
-	
-	return true;
-	
-}//end validateHours()
-
-
-function validateFutureYear(elt) {
+function yearOK(elt) {
 	yearVal = parseInt(elt.value);
-	if (yearVal == null) {
+	return (yearVal !== null)  && (yearVal > 0) && ((yearVal < 100) || ((yearVal >= 1900) && (yearVal < 3000)));
+}//end yearOK()	
+
+
+function validateYear(elt) {
+	if (!yearOK(elt)) {
 		alert("You entered an invalid year");
 		elt.value = '';
 		elt.focus();
 		return false;
 	} else {
+		yearVal = parseInt(elt.value);
 		if (yearVal < 100) {
 			yearVal = yearVal + 2000;
 			elt.value = yearVal + 2000;
@@ -180,13 +161,56 @@ function validateFutureYear(elt) {
 }//end validateFutureYear()
 
 
+function minutesOK(elt) {
+	minutesValue = parseInt(elt.value);
+	return !((minutesValue === null) || (minutesValue < 0) || (minutesValue > 59));
+}//end minutesOK()
+	
+
+function validateMinutes(elt) {
+	if (!minutesOK(elt)) {
+		alert("You entered an invalid time");
+		elt.focus();
+		return false;
+	} else {
+		return true;
+	}
+	
+}//end validateMinutes()
+
+
+function hoursOK(elt) {
+	hoursValue = parseInt(elt.value);
+	return (hoursValue >= 0) && (hoursValue <= 23);
+}//end hoursOK()
+
+
+function validateHours(elt) {
+	hoursValue = parseInt(elt.value);
+	if (!hoursOK(elt)) {
+		alert("You entered an invalid time");
+		elt.focus();
+		return false;
+	} else if ((hoursValue > 12) && (hoursValue < 24)) {
+		elt.value = hoursValue - 12;
+		if ((pmElt = document.getElementById(elt.getAttribute('id').substring(0, elt.getAttribute('id').length-5)+'is_pm')) !== null) {
+			pmElt.selectedIndex=1;
+		} else {
+			alert("Couldn't find " + elt.getAttribute('id').substring(0, elt.getAttribute('id').length-5)+'is_pm'); 
+		}
+	}
+	return true;
+	
+}//end validateHours()
+
+
 function getDateFromField(name) {
 	d = new Date(document.getElementById(name+'_year').value, document.getElementById(name+'_month').value, document.getElementById(name+'_day').value);
 	hoursVal = parseInt(document.getElementById(name+'_hours').value);
 	if ((document.getElementById(name+'_is_pm').selectedIndex == 1) && (hoursVal < 12)) {
 		hoursVal += 12;
 	}
-	if ((document.getElementById(name+'_is_pm').selectedIndex == 0) && (hoursVal == 12)) {
+	if ((document.getElementById(name+'_is_pm').selectedIndex === 0) && (hoursVal == 12)) {
 		hoursVal = 0;
 	}
 	d.setHours(hoursVal);
@@ -200,12 +224,13 @@ function setDateField(fieldName, dateVal) {
 	document.getElementById(fieldName+'_year').value = dateVal.getFullYear();
 	document.getElementById(fieldName+'_month').value = dateVal.getMonth();
 	document.getElementById(fieldName+'_day').value = dateVal.getDate();
-	if ((hourElt = document.getElementById(fieldName+'_hours')) != null) {
+	if ((hourElt = document.getElementById(fieldName+'_hours')) !== null) {
 		document.getElementById(fieldName+'_is_pm').selectedIndex = ((dateVal.getHours() >= 12) ? 1 : 0);
-		if (dateVal.getHours() % 12 == 0)
+		if (dateVal.getHours() % 12 === 0) {
 			hourElt.value = 12;
-		else
+		} else {
 			hourElt.value = make2digits(dateVal.getHours()) % 12;
+		}
 		document.getElementById(fieldName+'_minutes').value = dateVal.getMinutes();
 	}
 	
@@ -216,8 +241,9 @@ function updateEndDate(name) {
 	endD = getDateFromField(name+'_end');
 	startD = getDateFromField(name+'_start');
 	//alert("Got end and start dates for updating end date");
-	if (endD < startD)
+	if (endD < startD) {
 		setDateField(name+'_end', startD);
+	}
 	
 }//end updateEndDate()
 
@@ -226,7 +252,8 @@ function updateStartDate(name) {
 	endD = getDateFromField(name+'_end');
 	startD = getDateFromField(name+'_start');
 	//alert("Got end and start dates for updating start date");
-	if (endD < startD)
+	if (endD < startD) {
 	  setDateField(name+'_start', endD);
+	}
 	  
 }//end updateStartDate()
