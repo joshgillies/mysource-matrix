d6e302ba6be59e997968f33c226bf1816d1bba94
Marc McIntyre <mmcintyre@squiz.net>
2004-11-02 02:51:46 +0000

    Communication between the asset map and javascript is now one way to overcome the freezing in IE on windows. The asset map now polls the javascript every two seconds looking for assetids to refresh

--- core/lib/asset_map/java/src/net/squiz/matrix/complexassetmap/ComplexAssetMap.java
+++ core/lib/asset_map/java/src/net/squiz/matrix/complexassetmap/ComplexAssetMap.java
@@ -17,7 +17,7 @@
 * | licence.                                                           |
 * +--------------------------------------------------------------------+
 *
-* $Id: ComplexAssetMap.java,v 1.3 2004/09/24 04:26:25 mmcintyre Exp $
+* $Id: ComplexAssetMap.java,v 1.4 2004/11/02 02:51:46 mmcintyre Exp $
 * $Name: not supported by cvs2svn $
 */
 
@@ -28,7 +28,9 @@ import net.squiz.matrix.assetmap.*;
 import javax.swing.*;
 import javax.swing.tree.*;
 import java.awt.*;
+import java.awt.event.*;
 import javax.swing.UIManager;
+import netscape.javascript.*;
 
 //import com.sun.java.swing.plaf.windows;
 
@@ -44,34 +46,53 @@ public class ComplexAssetMap extends AssetMap /*implements KeyListener*/
 
 	/** The font used on tabs*/
 	public static final Font TAB_FONT = new Font("Tab Font", Font.PLAIN, 10);
-	
+
+	public static final int POLLING_DELAY = 2000;
+
 	/**
 	 * Constructor
 	 */
 	public ComplexAssetMap() {
-		
+
 		// you must set the following properties before
 		// creating the tabbed pane object
 		UIManager.put("TabbedPane.selected", AssetMapMenuPanel.BG_COLOUR);
 		UIManager.put("TabbedPane.background", new Color(0x725B7D));
 		UIManager.put("TabbedPane.foreground", Color.white);
-		
+
 		tp = new JTabbedPane();
 	}
 
 	/**
-	 * Initialises the tree. 
+	 * Initialises the tree.
 	 */
 	public void initTree() {
 		tree = new ComplexAssetTree(new DefaultTreeModel(
 				AssetManager.INSTANCE.getRootNode()));
 	//	tree.addKeyListener(this);
 		tree.initialise();
+
+		ActionListener taskPerformer = new ActionListener() {
+			public void actionPerformed(ActionEvent evt) {
+
+				if (window == null)
+					JSObject.getWindow(ComplexAssetMap.this);
+
+				String assetids = (String) window.getMember("SQ_REFRESH_ASSETIDS");
+				if (!assetids.equals("")) {
+					AssetManager.INSTANCE.assetsReloaded(assetids);
+					window.eval("SQ_REFRESH_ASSETIDS = '';");
+				}
+			}
+		};
+
+		Timer t = new Timer(POLLING_DELAY, taskPerformer);
+		t.start();
 	}
-	
+
 	/**
 	 * Returns the Asset Tree
-	 * 
+	 *
 	 * @return the Asset Tree
 	 */
 	public JTree getTree() {
@@ -82,51 +103,51 @@ public class ComplexAssetMap extends AssetMap /*implements KeyListener*/
 	 * Initialises the applet
 	 */
 	public void start() {
-		
+
 		Icon mySpaceIcon = MatrixToolkit.getAssetMapIcon("myspace.png");
 		Icon treeIcon = MatrixToolkit.getAssetMapIcon("tree.png");
-		
+
 		JPanel treePanel = new JPanel(new BorderLayout());
 		treePanel.add(new AssetMapMenuPanel((ComplexAssetTree) tree), BorderLayout.NORTH);
 		treePanel.add(new JScrollPane(tree));
-		
+
 		StatusKey key = new StatusKey();
 		key.setSize(500, 500);
-		
+
 		JSplitPane splitPane = new JSplitPane(JSplitPane.VERTICAL_SPLIT, treePanel, key);
 		splitPane.setResizeWeight(1.0);
 		splitPane.setDividerLocation(Integer.MAX_VALUE);
 		splitPane.setOneTouchExpandable(true);
-	
+
 		tp.setFont(TAB_FONT);
 		tp.addTab("Asset Map", treeIcon, splitPane);
 
 		String workspaceId = AssetManager.INSTANCE.getWorkspaceId();
 
 		if (!workspaceId.equals("0")) {
-			
-			String inboxURL = MySource.INSTANCE.getBaseURL() + 
-				AssetMap.getApplet().getParameter("BACKEND_SUFFIX") + 
+
+			String inboxURL = MySource.INSTANCE.getBaseURL() +
+				AssetMap.getApplet().getParameter("BACKEND_SUFFIX") +
 				MatrixToolkit.rawUrlDecode(getParameter("INBOX_URL"));
-			
-			String detailsURL = MySource.INSTANCE.getBaseURL() + 
-				AssetMap.getApplet().getParameter("BACKEND_SUFFIX") + 
+
+			String detailsURL = MySource.INSTANCE.getBaseURL() +
+				AssetMap.getApplet().getParameter("BACKEND_SUFFIX") +
 				MatrixToolkit.rawUrlDecode(getParameter("DETAILS_URL"));
 
 			int newMessages = Integer.parseInt(getParameter("NEW_MSGS"));
-			
+
 			tp.addTab("My Space", mySpaceIcon, new MySpace(
-					inboxURL, 
-					detailsURL, 
-					workspaceId, 
+					inboxURL,
+					detailsURL,
+					workspaceId,
 					newMessages));
-		
+
 		}
 
 		getContentPane().setLayout(new BorderLayout());
 		getContentPane().add(tp);
 	}
-	
+
 	/*private int keysPressed = 0;
 	private String trigger = "invaders";
 	public void keyPressed(KeyEvent e) {
@@ -139,7 +160,7 @@ public class ComplexAssetMap extends AssetMap /*implements KeyListener*/
 			keysPressed = 0;
 		}
 	}
-	
+
 	public void keyReleased(KeyEvent e) {}
 	public void keyTyped(KeyEvent e) {}*/
 }
