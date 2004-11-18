9d265c1955b6647ec51fa9544a674fb22a09441e
Luke Wright <lwright@squiz.com.au>
2004-11-18 00:57:42 +0000

    Removed identifying part of the comment affected

--- core/web/index.php
+++ core/web/index.php
@@ -18,7 +18,7 @@
 * | licence.                                                           |
 * +--------------------------------------------------------------------+
 *
-* $Id: index.php,v 1.17 2004/11/16 03:51:22 lwright Exp $
+* $Id: index.php,v 1.18 2004/11/18 00:57:42 lwright Exp $
 * $Name: not supported by cvs2svn $
 */
 
@@ -32,7 +32,7 @@
 * @package MySource_Matrix
 */
 
-ini_set('memory_limit', '8M');
+ini_set('memory_limit', '32M');
 require_once dirname(dirname(__FILE__)).'/include/init.inc';
 $GLOBALS['SQ_SYSTEM']->start();
 
