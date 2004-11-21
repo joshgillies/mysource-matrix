da877010bf5c77e73df2453a55aae09ac547969b
Greg Sherwood <gsherwood@squiz.net>
2004-11-21 22:52:11 +0000

    Removed some old flash asset map based code and did a bit of a cleanup

--- core/lib/asset_map/asset_map.js
+++ core/lib/asset_map/asset_map.js
@@ -17,35 +17,12 @@
 * | licence.                                                           |
 * +--------------------------------------------------------------------+
 *
-* $Id: asset_map.js,v 1.9 2004/11/02 03:18:41 mmcintyre Exp $
+* $Id: asset_map.js,v 1.10 2004/11/21 22:52:11 gsherwood Exp $
 * $Name: not supported by cvs2svn $
 */
 
 
-/*--         START GLOBALS            --*/
-
-
-var FLASH_TO_JS_CALL_BACK_FNS = {};
-var IE_FLASH_VERSION  = 6;
-var MOZ_FLASH_VERSION = 7;
-var flash_InternetExplorer = (navigator.appName.indexOf("Microsoft") != -1);
-
 var SQ_REFRESH_ASSETIDS = "";
-
-
-// Hook for Internet Explorer
-if (navigator.appName && navigator.appName.indexOf("Microsoft") != -1 &&
-	navigator.userAgent.indexOf("Windows") != -1 && navigator.userAgent.indexOf("Windows 3.1") == -1) {
-		document.write('<SCRIPT LANGUAGE="VBScript"\> \n');
-		document.write('on error resume next \n');
-		document.write('Sub asset_map_FSCommand(ByVal command, ByVal args)\n');
-		document.write('  call asset_map_DoFSCommand(command, args)\n');
-		document.write('end sub\n');
-		document.write('</SCRIPT\> \n');
-}
-
-
-/*--         END GLOBALS             --*/
 
 
 /**
@@ -54,8 +31,10 @@ if (navigator.appName && navigator.appName.indexOf("Microsoft") != -1 &&
 * Checks the browser flavour and version and make some desicions based on that information
 * about whether communication should be stable between the javascript and the asset map
 */
-function init_asset_map() {
+function init_asset_map()
+{
 	if (matches = navigator.userAgent.match(/MSIE ([0-9.]+)/)) {
+
 		if (matches[1] < '6.0') {
 			alert('You need to use Internet Explorer 6.0 or above for the communication between the Asset Map and the Javascript');
 		}
@@ -78,34 +57,23 @@ function init_asset_map() {
 		// we don't know about this browser, ah well may as well give it a go ...
 		} else {
 			alert('You are using an untested browser there is no guarantee that the communication between the Asset Map and the Javascript will be successful');
+		}
 
-		}// end if
+	}//end if
 
-	}// end if
 }//end init_asset_map()
 
 
 /**
-* Opens a new window with a HIPO ob in it
+* Opens a new window with a HIPO job in it
 *
 */
 function open_hipo(url)
 {
 	window.open(url, 'hipo_job', 'width=650,height=400,scrollbars=1,toolbar=0,menubar=0,location=0,resizable=1');
-}
 
+}//end open_hipo()
 
-/*
-* Reload the assetids represented in the flash
-*/
-//function reload_assets(assetids_xml)
-//{
-//	var asset_mapObj = document.asset_map;
-//	jsToFlashCall(asset_mapObj, 'reload_assets', {assetids_xml: assetids_xml});
-
-
-
-//}//end reload_assets()
 
 /**
 * Returns the java applet object
@@ -115,20 +83,27 @@ function open_hipo(url)
 function get_java_applet_object()
 {
 	return document.sq_asset_map;
-}
+
+}//end get_java_applet_object()
 
 
+/**
+* Add the passed list of assetids to the array of IDs to refresh
+*
+*/
 function reload_assets(assetids)
 {
 	if (SQ_REFRESH_ASSETIDS != "") {
 		SQ_REFRESH_ASSETIDS += ",";
 	}
 	SQ_REFRESH_ASSETIDS += assetids;
-}
+
+}//end reload_assets()
 
 
 /*
-* Reload the passed assetid in the flash
+* Add the passed assetid to the array of IDs to refresh
+*
 */
 function reload_asset(assetid)
 {
@@ -140,47 +115,27 @@ function reload_asset(assetid)
 }//end reload_asset()
 
 
-
-/*
-* Reload the passed assetid in the flash
-*/
-function refresh_internal_messages()
-{
-	var asset_mapObj = document.asset_map;
-//	jsToFlashCall(asset_mapObj, 'refresh_mail', {});
-
-}//end refresh_internal_messages()
-
-
 /**
-* highlight the link path
+* Makes an extenal call to java
+*
+* @param object		$asset_mapObj	the java applet
+* @param string		$type			the type of request
+* @param String		$command		the command
+* @param Array		$params			the params to pass to java
 */
-function select_path(link_path)
+function jsToJavaCall(asset_mapObj, type, command, params)
 {
-	var asset_mapObj = document.asset_map;
-//	jsToFlashCall(asset_mapObj, 'select_path', {link_path: link_path});
+	params = var_serialise(params);
+	asset_mapObj.jsToJavaCall(type, command, params);
 
-}//end select_path()
+}//end jsToJavaCall();
 
-
-/**
-* Reload the passed assetid in the flash
-*/
-function add_messages(xml)
-{
-	var asset_mapObj = document.asset_map;
-	//alert("Add Message : " + xml);
-//	jsToFlashCall(asset_mapObj, 'add_message', {msgs_xml: xml});
-
-}//end add_messages()
 
 
 
   //////////////////////////////////
  //   Asset Finder Functions     //
 //////////////////////////////////
-
-
 
 var ASSET_FINDER_FIELD_NAME = null;
 var ASSET_FINDER_FIELD_SAFE_NAME = null;
@@ -192,7 +147,8 @@ var ASSET_FINDER_OBJ = null;
 *
 * @param finder the finder that initiated the asset finder
 */
-function set_finder(finder) {
+function set_finder(finder)
+{
 	ASSET_FINDER_OBJ = finder;
 
 }//end set_finder()
@@ -233,7 +189,7 @@ function asset_finder_change_btn_press(name, safe_name, type_codes, done_fn)
 		ASSET_FINDER_FIELD_SAFE_NAME = null;
 	}
 
-}// end asset_finder_change_btn_press()
+}//end asset_finder_change_btn_press()
 
 
 /**
@@ -318,7 +274,7 @@ function asset_finder_clear_btn_press(name, safe_name)
 	ASSET_FINDER_OBJ.set_hidden_field(name + '[url]', '');
 	ASSET_FINDER_OBJ.set_text_field(safe_name + '_label', '');
 
-}// end asset_finder_clear_btn_press()
+}//end asset_finder_clear_btn_press()
 
 
 /**
@@ -334,7 +290,7 @@ function asset_finder_reset_btn_press(name, safe_name, assetid, label)
 	ASSET_FINDER_OBJ.set_hidden_field(name + '[assetid]', assetid);
 	ASSET_FINDER_OBJ.set_text_field(safe_name + '_label', label);
 
-}// end asset_finder_reset_btn_press()
+}//end asset_finder_reset_btn_press()
 
 
 /**
@@ -348,26 +304,10 @@ function asset_finder_onunload()
 	if (ASSET_FINDER_FIELD_NAME != null) {
 		asset_finder_cancel();
 	}
-}// end asset_finder_onunload()
+
+}//end asset_finder_onunload()
+
+
 ASSET_FINDER_OTHER_ONUNLOAD = (window.onunload) ? window.onunload : new Function;
 window.onunload = asset_finder_onunload;
-
-
-/**
-* Makes an extenal call to java
-*
-* @param object		$asset_mapObj	the java applet
-* @param string		$type			the type of request
-* @param String		$command		the command
-* @param Array		$params			the params to pass to java
-*/
-function jsToJavaCall(asset_mapObj, type, command, params)
-{
-	params = var_serialise(params);
-	asset_mapObj.jsToJavaCall(type, command, params);
-
-}//end jsToJavaCall();
-
-
-
 
