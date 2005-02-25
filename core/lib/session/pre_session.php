f7f7d905f88dc050ad92f1beeca27fa5f43261cf
Greg Sherwood <gsherwood@squiz.net>
2005-02-25 04:45:51 +0000

    Matrix now has support for maintaining sessions across domains. To do this, matrix sessions are now set per URL rather than just per domain. There is also a new SITE_NETWORK asset to allow you to create groups of sites. Basically, you create a SITE NETWORK and then link a bunch of sites under this network. You select which site is the primary one (for some session replication magic). Now users will have their sessions maintained when moving between the sites within this network. YOU MUST REPARSE ALL YOUR DESIGNS FOR THIS FUCNTIONALITY TO WORK - INCLUDING THE SYSTEM LOGIN DESIGN

---
+++ core/lib/session/pre_session.php
@@ -0,0 +1,41 @@
+<?php
+
+if (!isset($_SESSION['PRIMARY_SESSIONID'])) {
+	reload_browser(true, $SQ_SITE_NETWORK);
+} else {
+	$pri_session = $SQ_SITE_NETWORK->unserialiseSessionFile(SQ_CACHE_PATH.'/sess_'.$_SESSION['PRIMARY_SESSIONID']);
+	$pri_timestamp = (isset($pri_session['SQ_SESSION_TIMESTAMP'])) ? $pri_session['SQ_SESSION_TIMESTAMP'] : -1;
+	$sec_timestamp = (isset($_SESSION['SQ_SESSION_TIMESTAMP'])) ? $_SESSION['SQ_SESSION_TIMESTAMP'] : -1;
+
+	if ($pri_timestamp > $sec_timestamp) {
+		$SQ_SITE_NETWORK->syncSessionFile($_SESSION['PRIMARY_SESSIONID']);
+		reload_browser(false, $SQ_SITE_NETWORK);
+	}
+}
+
+function reload_browser($do_js_request=false, $site_network)
+{
+	$primary_url = $site_network->getPrimaryURL();
+	?>
+		<html>
+			<head>
+				<script type="text/javascript" src="<?php echo sq_web_path('lib'); ?>/js/JsHttpConnector.js"></script>
+				<script type="text/javascript" src="<?php echo $primary_url; ?>/__lib/session/session.php?in_primary=<?php echo (sq_web_path('root_url') == $primary_url) ? 1 : 0; ?>&site_network=<?php echo $site_network->id; ?>"></script>
+				<script type="text/javascript">
+					<?php
+						if ($do_js_request) {
+							echo 'start_session_handler("'.sq_web_path('lib').'/session/session.php?site_network='.$site_network->id.'");'."\n";
+						}
+					?>
+					document.location.href = document.location.href;
+				</script>
+			</head>
+			<body>
+			</body>
+		</html>
+	<?php
+	exit();
+
+}//end reload_browser()
+
+?>
---
+++ core/lib/session/session.php
@@ -0,0 +1,47 @@
+<?php
+
+// include init.in, which will call $GLOBALS['SQ_SYSTEM']->init() and start a session
+require_once '../../include/init.inc';
+
+$site_network_id = array_get_index($_GET, 'site_network', '0');
+$site_network = null;
+$primary_url = '';
+
+if ($site_network_id) {
+	$site_network = &$GLOBALS['SQ_SYSTEM']->am->getAsset($site_network_id);
+	if (!is_null($site_network)) $primary_url = $site_network->getPrimaryURL();
+}
+
+
+if ($primary_url == sq_web_path('root_url')) {
+
+	// if the actual script execution is happening in the primary
+	// url, we want to update the time to persuade the overriding
+	// of the secondary session files
+	if (isset($_GET['in_primary']) && $_GET['in_primary']) {
+		$_SESSION['SQ_SESSION_TIMESTAMP'] = time();
+		exit();
+	}
+
+	echo 'var SESSIONID = "'.session_id().'";';
+	?>
+	function start_session_handler(url) {
+		JsHttpConnector.submitRequest(url + '&sessionid=' + SESSIONID);
+	}
+	<?php
+
+} else {
+
+	if (!isset($_GET['sessionid'])) {
+		// something is definately wrong
+		trigger_error('Missing primary sessionid', E_USER_ERROR);
+	}
+	if (is_null($site_network)) {
+		trigger_error('Missing site network', E_USER_ERROR);
+	}
+
+	$site_network->syncSessionFile($_REQUEST['sessionid']);
+
+}//end if
+
+?>
