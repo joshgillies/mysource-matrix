c078755ba05581d7332a6e71f71bad9018236e34
Marc McIntyre <mmcintyre@squiz.net>
2005-02-28 00:59:33 +0000

    Fixed a bug where the spinner would dissapear when repaint() was called. Commenting changes.

--- core/lib/asset_map/java/src/net/squiz/matrix/ui/Spinner.java
+++ core/lib/asset_map/java/src/net/squiz/matrix/ui/Spinner.java
@@ -1,3 +1,25 @@
+/**
+ * +--------------------------------------------------------------------+
+ * | Squiz.net Open Source Licence                                      |
+ * +--------------------------------------------------------------------+
+ * | Copyright (c), 2003 Squiz Pty Ltd (ABN 77 084 670 600).            |
+ * +--------------------------------------------------------------------+
+ * | This source file may be used subject to, and only in accordance    |
+ * | with, the Squiz Open Source Licence Agreement found at             |
+ * | http://www.squiz.net/licence.                                      |
+ * | Make sure you have read and accept the terms of that licence,      |
+ * | including its limitations of liability and disclaimers, before     |
+ * | using this software in any way. Your use of this software is       |
+ * | deemed to constitute agreement to be bound by that licence. If you |
+ * | modify, adapt or enhance this software, you agree to assign your   |
+ * | intellectual property rights in the modification, adaptation and   |
+ * | enhancement to Squiz Pty Ltd for use and distribution under that   |
+ * | licence.                                                           |
+ * +--------------------------------------------------------------------+
+ *
+ * $Id: Spinner.java,v 1.2 2005/02/28 00:59:33 mmcintyre Exp $
+ * $Name: not supported by cvs2svn $
+ */
 
 package net.squiz.matrix.ui;
 
@@ -8,10 +30,21 @@ import net.squiz.matrix.core.*;
 import java.util.*;
 import java.awt.image.*;
 
-public class Spinner extends JComponent implements Runnable{
+/**
+ * A spinner primvides an animation to indicate that an operation in the system
+ * is currently progress. When not spinner, the spinner is in a stopped state
+ * and shows a stopped icon to indicate that no operation is in progress. 
+ * The spinner image itself is a sequence of frames; the first being the stopped
+ * image and those preceeding it being the animation steps of the spinner.
+ * @author Marc McIntyre <mmcintyre@squiz.net>
+ */
+public class Spinner extends JComponent implements Runnable {
 
+	/** The spinner icon */
 	public static final String SPINNER_ICON = "spinner.png";
+	/** The delay between each animation operation */
 	public static final int delay = 40;
+	/** The offset which each aniation frame occurs in the image */
 	public static final int IMG_OFFSET = 15;
 
 	private Dimension size = new Dimension(IMG_OFFSET, IMG_OFFSET);
@@ -19,51 +52,61 @@ public class Spinner extends JComponent implements Runnable{
 	private boolean isStarted = false;
 	private int framePtr = 0;
 	private volatile Thread animator;
-	private static Image[] frames;
-	private static Image stopImage;
+	private Image[] frames;
+	private Image stopImage;
 	
+	/**
+	 * Constructs a spinner object.
+	 * @return the spinner object
+	 */
 	public Spinner() {
-		// we only need to do this once for all spinner instances
-		if (frames == null) {
-			ImageIcon spinner = (ImageIcon) GUIUtilities.getAssetMapIcon(SPINNER_ICON);
-			Image spinnerImg = spinner.getImage();
-			
-			BufferedImage spinnerSrc = new BufferedImage(
-				spinner.getIconWidth(),
-				spinner.getIconHeight(),
-				BufferedImage.TYPE_INT_ARGB_PRE
-			);
-	
-			Graphics2D g2d = (Graphics2D) spinnerSrc.createGraphics();
-			g2d.drawImage(spinnerImg, 0, 0, null);
-			g2d.dispose();
-			
-			int numFrames = (spinner.getIconWidth() / IMG_OFFSET) - 1;
-			int offset = IMG_OFFSET;
-			frames = new Image[numFrames];
-			
-			stopImage = spinnerSrc.getSubimage(0, 0, IMG_OFFSET, IMG_OFFSET);
-			
-			for (int i = 0; i < numFrames; i++) {
-				frames[i] = spinnerSrc.getSubimage(offset, 0, IMG_OFFSET, IMG_OFFSET);
-				offset += IMG_OFFSET;
-			}
+		ImageIcon spinner = (ImageIcon) GUIUtilities.getAssetMapIcon(SPINNER_ICON);
+		Image spinnerImg = spinner.getImage();
+		
+		BufferedImage spinnerSrc = new BufferedImage(
+			spinner.getIconWidth(),
+			spinner.getIconHeight(),
+			BufferedImage.TYPE_INT_ARGB_PRE
+		);
+
+		Graphics2D g2d = (Graphics2D) spinnerSrc.createGraphics();
+		g2d.drawImage(spinnerImg, 0, 0, null);
+		g2d.dispose();
+		
+		// the number of frames of animation is one less than the icon width
+		// as we need to remove the stop spinner state from the frames
+		int numFrames = (spinner.getIconWidth() / IMG_OFFSET) - 1;
+		int offset = IMG_OFFSET;
+		frames = new Image[numFrames];
+		
+		// the stop image is the first image square in the main spinner sequence
+		stopImage = spinnerSrc.getSubimage(0, 0, IMG_OFFSET, IMG_OFFSET);
+		
+		// all other images are IMG_OFFSET from the stop image
+		for (int i = 0; i < numFrames; i++) {
+			frames[i] = spinnerSrc.getSubimage(offset, 0, IMG_OFFSET, IMG_OFFSET);
+			offset += IMG_OFFSET;
 		}
 	}
 	
+	/**
+	 * The run method of this Thread.
+	 */
 	public void run() {
 		Thread.currentThread().setPriority(Thread.MIN_PRIORITY);
 		long tm = System.currentTimeMillis();
 		
 		while (Thread.currentThread() == animator) {
 			Graphics g = getGraphics();
-			if (g != null)
+			if (g != null) {
 				g.drawImage(frames[framePtr], 0, 0, null);
-			if (framePtr == frames.length - 1)
+			}
+			if (framePtr == frames.length - 1) {
 				framePtr = 0;
-			else
+			} else {
 				framePtr++;
-
+			}
+			
 			try {
 				tm += delay;
 				Thread.sleep(Math.max(0, tm - System.currentTimeMillis()));
@@ -74,22 +117,63 @@ public class Spinner extends JComponent implements Runnable{
 		}
 	}
 	
+	/**
+	 * Paints the component.
+	 * there are no components to paint so we explicity paint the stopped
+	 * spinner state when paint is called.
+	 * @param g the graphics that we are paintint to
+	 */
+	public void paint(Graphics g) {
+		super.paint(g);
+		// if we are not spinning we need to paint the stop spinner state
+		// whenever paint gets called
+		if (!isStarted) {
+			g.drawImage(stopImage, 0, 0, null);
+		}
+	}
+	
+	/**
+	 * Returns the maximum size of the spinner object
+	 * @return the maximum size
+	 */
 	public Dimension getMaximumSize() {
 		return size;
 	}
+	
+	/**
+	 * Returns the maximum size of the spinner object
+	 * @return the minimum size
+	 */
 	public Dimension getMinimumSize() {
 		return size;
 	}
+	
+	/**
+	 * Returns the preferred size of the spinner object
+	 * @return the preferred size
+	 */
 	public Dimension getPreferredSize() {
 		return size;
 	}
 	
+	/**
+	 * Starts the animation of the spinner
+	 * @see stop()
+	 * @see isStarted()
+	 */
 	public void start() {
+		if (isStarted)
+			return;
 		isStarted = true;
 		animator = new Thread(this);
 		animator.start();
 	}
 	
+	/**
+	 * Stops the animation of the spinner
+	 * @see start()
+	 * @see isStarted()
+	 */
 	public void stop() {
 		isStarted = false;
 		framePtr = 0;
@@ -99,6 +183,12 @@ public class Spinner extends JComponent implements Runnable{
 			g.drawImage(stopImage, 0, 0, null);
 	}
 	
+	/**
+	 * Returns TRUE is the spinner is current animating
+	 * @return TRUE is the spinner is current animating
+	 * @see start()
+	 * @see stop()
+	 */
 	public boolean isStarted() {
 		return isStarted;
 	}
