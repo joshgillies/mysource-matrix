bd1e277009386b95f781548fec93560a004dc382
Marc McIntyre <mmcintyre@squiz.net>
2005-03-06 22:54:02 +0000

    Commenting changes

--- core/lib/asset_map/java/src/net/squiz/matrix/core/GUIUtilities.java
+++ core/lib/asset_map/java/src/net/squiz/matrix/core/GUIUtilities.java
@@ -1,3 +1,25 @@
+/**
+* +--------------------------------------------------------------------+
+* | Squiz.net Open Source Licence                                      |
+* +--------------------------------------------------------------------+
+* | Copyright (c), 2003 Squiz Pty Ltd (ABN 77 084 670 600).            |
+* +--------------------------------------------------------------------+
+* | This source file may be used subject to, and only in accordance    |
+* | with, the Squiz Open Source Licence Agreement found at             |
+* | http://www.squiz.net/licence.                                      |
+* | Make sure you have read and accept the terms of that licence,      |
+* | including its limitations of liability and disclaimers, before     |
+* | using this software in any way. Your use of this software is       |
+* | deemed to constitute agreement to be bound by that licence. If you |
+* | modify, adapt or enhance this software, you agree to assign your   |
+* | intellectual property rights in the modification, adaptation and   |
+* | enhancement to Squiz Pty Ltd for use and distribution under that   |
+* | licence.                                                           |
+* +--------------------------------------------------------------------+
+*
+* $Id: GUIUtilities.java,v 1.2 2005/03/06 22:54:02 mmcintyre Exp $
+*
+*/
 
 package net.squiz.matrix.core;
 
@@ -139,7 +161,7 @@ public class GUIUtilities {
 	}
 	
 	/**
-	 * Returns true if the right mouse button is clicked. Use this in favour of
+	 * Returns TRUE if the right mouse button is clicked. Use this in favour of
 	 * SwingUtilities.isRightMouseButton() as it detects Mac apple key and mouse click
 	 * @param evt the mouse event
 	 * @return TRUE if the right mouse button is clicked
