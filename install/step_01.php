87fd54f10b62c01bd8aeefcadf04535cc5e2203c
Marcus Nyeholt <mnyeholt@squiz.net>
2005-03-16 04:24:46 +0000

    Added version checking from the web installer to step 1, as well as ability to provide details from the command line for installing

--- install/step_01.php
+++ install/step_01.php
@@ -18,7 +18,7 @@
 * | licence.                                                           |
 * +--------------------------------------------------------------------+
 *
-* $Id: step_01.php,v 1.34 2005/01/20 13:37:16 brobertson Exp $
+* $Id: step_01.php,v 1.35 2005/03/16 04:24:46 mnyeholt Exp $
 *
 */
 
@@ -28,10 +28,14 @@
 * Purpose
 *
 * @author  Blair Robertson <blair@squiz.net>
-* @version $Revision: 1.34 $
+* @version $Revision: 1.35 $
 * @package MySource_Matrix
 * @subpackage install
 */
+
+// First off, run the version check script
+require_once dirname(__FILE__).'/versioncheck.php';
+
 ini_set('memory_limit', -1);
 error_reporting(E_ALL);
 define('SQ_SYSTEM_ROOT',   dirname(dirname(realpath(__FILE__))));
@@ -45,15 +49,31 @@ $GLOBALS['SQ_INSTALL'] = true;
 require_once SQ_INCLUDE_PATH.'/mysource_object.inc';
 require_once SQ_INCLUDE_PATH.'/system_config.inc';
 
+echo "\n";
+echo "\n";
+echo "Please enter a value for SQ_CONF_DB_DSN [] : ";
+$dsn1 = read_line();
+echo "Please enter a value for SQ_CONF_DB2_DSN [] : ";
+$dsn2 = read_line();
+echo "Please enter a value for SQ_CONF_SYSTEM_ROOT_URLS [www.example.com] : ";
+$root_url = read_line('www.example.com');
+echo "Please enter a value for SQ_CONF_DEFAULT_EMAIL [] : ";
+$mail1 = read_line();
+echo "Please enter a value for SQ_CONF_TECH_EMAIL [] : ";
+$mail2 = read_line();
+
+$dsn1 = 
 #define('SQ_CONF_DB_DSN',  'pgsql://username:password@unix()/database_name');
 #define('SQ_CONF_DB2_DSN', 'pgsql://username_2:password@unix()/database_name');
-define('SQ_CONF_SYSTEM_ROOT_URLS', 'www.example.com');
+define('SQ_CONF_DB_DSN', $dsn1);
+define('SQ_CONF_DB2_DSN', $dsn2);
+define('SQ_CONF_SYSTEM_ROOT_URLS', $root_url);
 define('SQ_CONF_PEAR_PATH', SQ_SYSTEM_ROOT.'/php_includes');
 define('SQ_CONF_ASSET_TREE_BASE', 36);
 define('SQ_CONF_ASSET_TREE_SIZE', 4);
 define('SQ_CONF_ROLLBACK_ENABLED', '0');
-define('SQ_CONF_DEFAULT_EMAIL', 'matrix-team@squiz.net');
-define('SQ_CONF_TECH_EMAIL',    'matrix-team@squiz.net');
+define('SQ_CONF_DEFAULT_EMAIL', $mail1);
+define('SQ_CONF_TECH_EMAIL',    $mail2);
 
 $cfg = new System_Config();
 $cfg->save(Array(), true);
@@ -62,4 +82,18 @@ $GLOBALS['SQ_INSTALL'] = false;
 // reminder for chmod
 echo 'Remember to give your system\'s Apache user write access to'."\n";
 echo 'the cache and data directories of your Matrix install...'."\n";
+
+/**
+* Reads a line from stdin
+*
+* @return string		The read in string
+*/
+function read_line($default='')
+{
+	$input = fgets(STDIN);
+	$input = trim($input);
+	if (!strlen($input)) $input = $default;
+	return $input;
+}// end read_line()
+
 ?>
