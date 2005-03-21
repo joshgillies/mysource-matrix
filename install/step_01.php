19af5b2a8214031b983457eee6e27083b7eb0297
Greg Sherwood <gsherwood@squiz.net>
2005-03-21 06:24:50 +0000

    Removed some unrequired CONF vars

--- install/step_01.php
+++ install/step_01.php
@@ -18,7 +18,7 @@
 * | licence.                                                           |
 * +--------------------------------------------------------------------+
 *
-* $Id: step_01.php,v 1.36 2005/03/20 22:50:02 mnyeholt Exp $
+* $Id: step_01.php,v 1.37 2005/03/21 06:24:50 gsherwood Exp $
 *
 */
 
@@ -28,69 +28,33 @@
 * Purpose
 *
 * @author  Blair Robertson <blair@squiz.net>
-* @version $Revision: 1.36 $
+* @version $Revision: 1.37 $
 * @package MySource_Matrix
 * @subpackage install
 */
-
 ini_set('memory_limit', -1);
 error_reporting(E_ALL);
-define('SQ_SYSTEM_ROOT',   dirname(dirname(realpath(__FILE__))));
-define('SQ_INCLUDE_PATH',  SQ_SYSTEM_ROOT.'/core/include');
-define('SQ_LIB_PATH',      SQ_SYSTEM_ROOT.'/core/lib');
-define('SQ_DATA_PATH',     SQ_SYSTEM_ROOT.'/data');
-define('SQ_FUDGE_PATH',    SQ_SYSTEM_ROOT.'/fudge');
-define('SQ_PHP_CLI', (php_sapi_name() == 'cli'));
-$GLOBALS['SQ_INSTALL'] = true;
+
+define('SQ_SYSTEM_ROOT',  dirname(dirname(realpath(__FILE__))));
+define('SQ_INCLUDE_PATH', SQ_SYSTEM_ROOT.'/core/include');
+define('SQ_LIB_PATH',     SQ_SYSTEM_ROOT.'/core/lib');
+define('SQ_DATA_PATH',    SQ_SYSTEM_ROOT.'/data');
+define('SQ_FUDGE_PATH',   SQ_SYSTEM_ROOT.'/fudge');
+define('SQ_PHP_CLI',      (php_sapi_name() == 'cli'));
 
 require_once SQ_INCLUDE_PATH.'/mysource_object.inc';
 require_once SQ_INCLUDE_PATH.'/system_config.inc';
 
-echo "\n";
-echo "\n";
-echo "Please enter a value for SQ_CONF_DB_DSN [] : ";
-$dsn1 = read_line();
-echo "Please enter a value for SQ_CONF_DB2_DSN [] : ";
-$dsn2 = read_line();
-echo "Please enter a value for SQ_CONF_SYSTEM_ROOT_URLS [www.example.com] : ";
-$root_url = read_line('www.example.com');
-echo "Please enter a value for SQ_CONF_DEFAULT_EMAIL [] : ";
-$mail1 = read_line();
-echo "Please enter a value for SQ_CONF_TECH_EMAIL [] : ";
-$mail2 = read_line();
-
-$dsn1 = 
-#define('SQ_CONF_DB_DSN',  'pgsql://username:password@unix()/database_name');
-#define('SQ_CONF_DB2_DSN', 'pgsql://username_2:password@unix()/database_name');
-define('SQ_CONF_DB_DSN', $dsn1);
-define('SQ_CONF_DB2_DSN', $dsn2);
-define('SQ_CONF_SYSTEM_ROOT_URLS', $root_url);
+// override some of the default config values
 define('SQ_CONF_PEAR_PATH', SQ_SYSTEM_ROOT.'/php_includes');
-define('SQ_CONF_ASSET_TREE_BASE', 36);
-define('SQ_CONF_ASSET_TREE_SIZE', 4);
-define('SQ_CONF_ROLLBACK_ENABLED', '0');
-define('SQ_CONF_DEFAULT_EMAIL', $mail1);
-define('SQ_CONF_TECH_EMAIL',    $mail2);
+define('SQ_CONF_DEFAULT_EMAIL', 'matrix-team@squiz.net');
+define('SQ_CONF_TECH_EMAIL',    'matrix-team@squiz.net');
 
 $cfg = new System_Config();
 $cfg->save(Array(), true);
-$GLOBALS['SQ_INSTALL'] = false;
 
 // reminder for chmod
 echo 'Remember to give your system\'s Apache user write access to'."\n";
 echo 'the cache and data directories of your Matrix install...'."\n";
-
-/**
-* Reads a line from stdin
-*
-* @return string		The read in string
-*/
-function read_line($default='')
-{
-	$input = fgets(STDIN);
-	$input = trim($input);
-	if (!strlen($input)) $input = $default;
-	return $input;
-}// end read_line()
 
 ?>
