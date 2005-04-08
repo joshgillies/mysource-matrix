43618e5a180ef5a87347c5e94ae5cafae1158675
Luke Wright <lwright@squiz.com.au>
2005-04-08 05:55:55 +0000

    Updates to Step 4 to handle processing of localised error string files (note, the errors are not actually in the system yet, but they will be in the near future)

--- install/step_04.php
+++ install/step_04.php
@@ -18,7 +18,7 @@
 * | licence.                                                           |
 * +--------------------------------------------------------------------+
 *
-* $Id: step_04.php,v 1.3 2005/04/07 02:43:09 lwright Exp $
+* $Id: step_04.php,v 1.4 2005/04/08 05:55:55 lwright Exp $
 *
 */
 
@@ -29,7 +29,7 @@
 * Compiles languages on the system
 *
 * @author  Luke Wright <lwright@squiz.net>
-* @version $Revision: 1.3 $
+* @version $Revision: 1.4 $
 * @package MySource_Matrix
 * @subpackage install
 */
@@ -78,6 +78,8 @@ if (!regenerate_configs()) {
 }
 
 $string_locales = Array();
+$error_locales  = Array();
+
 $asset_screen_dir = SQ_DATA_PATH.'/private/asset_types/asset/localised_screens';
 create_directory($asset_screen_dir);
 
@@ -145,7 +147,7 @@ foreach ($asset_types as $asset_type) {
 													 'dir'    => $dir_read,
 													 'screen' => $matches[1],
 													 );
-				} elseif (preg_match('|lang\_strings\.xml|', $entry, $matches)) {
+				} else if (preg_match('|lang\_strings\.xml|', $entry, $matches)) {
 					list($country,$lang,$variant) = $GLOBALS['SQ_SYSTEM']->lm->getLocaleParts($locale_name);
 					if (!in_array($country, $string_locales)) {
 						$string_locales[] = $country;
@@ -159,6 +161,23 @@ foreach ($asset_types as $asset_type) {
 						if (!empty($variant)) {
 							if (!in_array($country.'_'.$lang.'@'.$variant, $string_locales)) {
 								$string_locales[] = $country.'_'.$lang.'@'.$variant;
+							}
+						}
+					}
+				} else if (preg_match('|lang\_errors\.xml|', $entry, $matches)) {
+					list($country,$lang,$variant) = $GLOBALS['SQ_SYSTEM']->lm->getLocaleParts($locale_name);
+					if (!in_array($country, $string_locales)) {
+						$error_locales[] = $country;
+					}
+
+					if (!empty($lang)) {
+						if (!in_array($country.'_'.$lang, $string_locales)) {
+							$error_locales[] = $country.'_'.$lang;
+						}
+
+						if (!empty($variant)) {
+							if (!in_array($country.'_'.$lang.'@'.$variant, $string_locales)) {
+								$error_locales[] = $country.'_'.$lang.'@'.$variant;
 							}
 						}
 					}
@@ -212,9 +231,16 @@ foreach ($asset_types as $asset_type) {
 
 }//foreach asset_type
 
+// compile the strings for each locale where a lang_strings.xml exists
 foreach ($string_locales as $locale) {
 	echo 'Compiling strings for locale '.$locale."\n";
 	build_locale_string_file($locale);
+}
+
+// then, compile errors for each locale (using lang_errors.xml)
+foreach ($error_locales as $locale) {
+	echo 'Compiling localised errors for locale '.$locale."\n";
+	build_locale_error_file($locale);
 }
 
 $GLOBALS['SQ_SYSTEM']->restoreRunLevel();
