6efb5ab3dcb1960d61230acc9b66b99fb099a83b
Marc McIntyre <mmcintyre@squiz.net>
2005-04-13 02:13:14 +0000

    Added a script to recreate the rollback triggers without turning off rollback, running step_02, then re-enabling rollback

---
+++ scripts/recreate_rollback_triggers.php
@@ -0,0 +1,50 @@
+<?php
+/**
+* +--------------------------------------------------------------------+
+* | Squiz.net Open Source Licence                                      |
+* +--------------------------------------------------------------------+
+* | Copyright (c), 2003 Squiz Pty Ltd (ABN 77 084 670 600).            |
+* +--------------------------------------------------------------------+
+* | This source file may be used subject to, and only in accordance    |
+* | with, the Squiz Open Source Licence Agreement found at             |
+* | http://www.squiz.net/licence.                                      |
+* | Make sure you have read and accept the terms of that licence,      |
+* | including its limitations of liability and disclaimers, before     |
+* | using this software in any way. Your use of this software is       |
+* | deemed to constitute agreement to be bound by that licence. If you |
+* | modify, adapt or enhance this software, you agree to assign your   |
+* | intellectual property rights in the modification, adaptation and   |
+* | enhancement to Squiz Pty Ltd for use and distribution under that   |
+* | licence.                                                           |
+* +--------------------------------------------------------------------+
+*
+* $Id: recreate_rollback_triggers.php,v 1.1 2005/04/13 02:13:14 mmcintyre Exp $
+*
+*/
+
+/**
+* Recreate the Rollback Trigger functions
+*
+* @author  Marc McIntyre <mmcintyre@squiz.net>
+* @version $Revision: 1.1 $
+* @package MySource_Matrix
+*/
+error_reporting(E_ALL);
+if ((php_sapi_name() != 'cli')) trigger_error("You can only run this script from the command line\n", E_USER_ERROR);
+
+$SYSTEM_ROOT = (isset($_SERVER['argv'][1])) ? $_SERVER['argv'][1] : '';
+if (empty($SYSTEM_ROOT) || !is_dir($SYSTEM_ROOT)) {
+	trigger_error("You need to supply the path to the System Root as the first argument\n", E_USER_ERROR);
+}
+
+require_once $SYSTEM_ROOT.'/core/include/init.inc';
+require_once $SYSTEM_ROOT.'/core/lib/db_install/db_install.inc';
+require      $SYSTEM_ROOT.'/data/private/db/table_columns.inc';
+
+$GLOBALS['SQ_SYSTEM']->doTransaction('BEGIN');
+
+install_rollback_triggers($tables, true, true);
+
+$GLOBALS['SQ_SYSTEM']->doTransaction('COMMIT');
+
+?>
