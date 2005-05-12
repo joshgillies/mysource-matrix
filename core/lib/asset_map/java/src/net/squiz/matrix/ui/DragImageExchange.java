00f1202da96d155d0a0188afb1d7a2a598b2e99f
Marc McIntyre <mmcintyre@squiz.net>
2005-05-12 00:21:11 +0000

    Add drag image exchange which handles drag operations between hetrogenious objects

---
+++ core/lib/asset_map/java/src/net/squiz/matrix/ui/DragImageExchange.java
@@ -0,0 +1,66 @@
+package net.squiz.matrix.ui;
+
+import java.awt.image.BufferedImage;
+import java.awt.Point;
+
+/**
+ * DragImageExchange provides a means for moving sharing ghosted drag images
+ * between different components. When a drag operation starts, the source
+ * component renders the drag image and stores it in this class. The target
+ * component can then use the image when painting the ghosted drop image.
+ *
+ * @author Nathan de Vries <ndvries@squiz.net>
+ */
+public class DragImageExchange {
+
+	private static BufferedImage dragImage;
+	private static Point mouseOffset;
+	private static boolean inExchange = false;
+
+	// cannot instantiate
+	private DragImageExchange() {}
+
+	/**
+	 * Stores an drag image for later use.
+	 *
+	 * @param dragImage  a drag image which represents the dragged items
+	 * @param mouseOffset  the offset between the mouse and the top-left corner
+	 *                     of the dragImage
+	 */
+	public static void setDragImage(BufferedImage dragImage, Point mouseOffset) {
+		//if (inExchange)
+		//	throw new IllegalStateException("There is already an exchange open.");
+		DragImageExchange.dragImage = dragImage;
+		DragImageExchange.mouseOffset = mouseOffset;
+		inExchange = true;
+	}
+
+	/**
+	 * Returns the stored drag image
+	 */
+	public static BufferedImage getDragImage() {
+		if (!inExchange)
+			throw new IllegalStateException("There is no exchange open.");
+		return dragImage;
+	}
+
+	/**
+	 * Returns the offset between the stored drag image and the original mouse
+	 * pointer.
+	 */
+	public static Point getMouseOffset() {
+		if (!inExchange)
+			throw new IllegalStateException("There is no exchange open.");
+		return mouseOffset;
+	}
+
+	/**
+	 * Ends the current image exchange.
+	 */
+	public static void completeExchange() {
+		if (!inExchange)
+			throw new IllegalStateException("There is no exchange open.");
+		dragImage = null;
+		inExchange = false;
+	}
+}
