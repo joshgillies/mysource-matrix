cf9d1b3267b7fdb908ea5d3749954cbd1a7dabc1
Luke Wright <lwright@squiz.com.au>
2005-07-05 06:11:49 +0000

    Removed debug bams from script and added counters for how many significant links it has to trawl, and how many tree entries it has created (these need not necessarily be the same, due to orphaned assets, bridge issues and the like)

--- scripts/recreate_link_tree.php
+++ scripts/recreate_link_tree.php
@@ -18,7 +18,7 @@
 * | licence.                                                           |
 * +--------------------------------------------------------------------+
 *
-* $Id: recreate_link_tree.php,v 1.11 2005/05/24 07:08:27 lwright Exp $
+* $Id: recreate_link_tree.php,v 1.12 2005/07/05 06:11:21 lwright Exp $
 *
 */
 
@@ -30,7 +30,7 @@
 *
 *
 * @author  Blair Robertson <blair@squiz.net>
-* @version $Revision: 1.11 $
+* @version $Revision: 1.12 $
 * @package MySource_Matrix
 */
 
@@ -39,13 +39,20 @@ require_once SQ_INCLUDE_PATH.'/general_occasional.inc';
 
 $db = &$GLOBALS['SQ_SYSTEM']->db;
 
-$sql = 'TRUNCATE sq_ast_lnk_tree';
-$result = $db->query($sql);
-assert_valid_db_result($result);
-
 function recurse_tree_create($majorid)
 {
 	$db =& $GLOBALS['SQ_SYSTEM']->db;
+
+	$sql = 'SELECT
+				COUNT(*)
+			FROM
+				sq_ast_lnk
+			WHERE
+				link_type & '.SQ_SC_LINK_SIGNIFICANT.' > 0';
+	$link_count = $db->getOne($sql);
+	assert_valid_db_result($link_count);
+
+	bam('ANALYSING '.$link_count.' SIGNIFICANT LINKS - THIS MAY TAKE A WHILE...');
 
 	// no link to our children; we need to (re-)create the tree, obviously... darn it
 	// so grab the links we need (significant ones only)
@@ -67,8 +74,6 @@ function recurse_tree_create($majorid)
 		$link =& $links[$k];
 
 		$child_links = $GLOBALS['SQ_SYSTEM']->am->getLinks($link['minorid'], SQ_SC_LINK_SIGNIFICANT);
-		bam($k);
-		bam($child_links);
 		$child_index = 0;
 		foreach($child_links as $child_link) {
 			$treeid = $k.asset_link_treeid_convert($child_index,true);
@@ -80,7 +85,7 @@ function recurse_tree_create($majorid)
 		$link['num_kids'] = count($child_links);
 	}
 
-	bam($links);
+	bam('RE-CREATING TREE ENTRIES...');
 
 	// adding root folder?
 	if ($majorid == 1) {
@@ -100,7 +105,8 @@ function recurse_tree_create($majorid)
 
 		$result = $db->query($sql);
 		assert_valid_db_result($result);
-		echo 'Added treeid - for linkid 1'."\n";
+		echo sprintf('[ Link Id %10d ] %s', 1, '-')."\n";
+		//echo 'Added treeid - for linkid 1'."\n";
 	}
 
 	foreach($links as $treeid => $this_link) {
@@ -129,12 +135,19 @@ function recurse_tree_create($majorid)
 				)';
 
 		$result = $db->query($sql);
-		echo 'Added treeid '.$treeid.' for linkid '.$this_link['linkid']."\n";
+		echo sprintf('[ Link Id %10d ] %s', $this_link['linkid'], $treeid)."\n";
 		assert_valid_db_result($result);
 	}
 
+	bam((count($links)+($majorid == 1 ? 1 : 0)).' TREE ENTRIES CREATED');
+
 }//end recurse_tree_create()
 
+bam('TRUNCATING TREE...');
+
+$sql = 'TRUNCATE sq_ast_lnk_tree';
+$result = $db->query($sql);
+assert_valid_db_result($result);
 
 recurse_tree_create(1);
 
