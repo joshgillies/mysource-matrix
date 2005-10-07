6316f34acea275743602b5599955a0c1a1f24b69
Sertan Danis <sdanis@squiz.com.au>
2005-10-07 00:28:07 +0000

    fixed bug #950: Logging out of a site inside a site network causes an infinite reload loop

--- core/lib/session/pre_session.php
+++ core/lib/session/pre_session.php
@@ -21,14 +21,14 @@ function reload_browser($do_js_request=false, $site_network)
 		<html>
 			<head>
 				<script type="text/javascript" src="<?php echo sq_web_path('lib'); ?>/js/JsHttpConnector.js"></script>
-				<script type="text/javascript" src=\"<?php echo sq_web_path('lib'); ?>/session/session.php?in_primary=<?php echo (sq_web_path('root_url') == $primary_url) ? 1 : 0; ?>&site_network=<?php echo $site_network->id; ?>"></script>
+				<script type="text/javascript" src="<?php echo sq_web_path('lib'); ?>/session/session.php?in_primary=<?php echo (sq_web_path('root_url') == $primary_url) ? 1 : 0; ?>&site_network=<?php echo $site_network->id; ?>"></script>
 				<script type="text/javascript">
 					<?php
 						if ($do_js_request) {
 							echo 'start_session_handler("'.sq_web_path('lib').'/session/session.php?site_network='.$site_network->id.'");'."\n";
 						}
 					?>
-					document.location.href = document.location.href;
+					document.location.href = document.location.href.replace('SQ_ACTION=logout','');
 				</script>
 			</head>
 			<body>
