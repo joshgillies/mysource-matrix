3878a8f7355d453454eea60ba97f364c38a68092
Elden McDonald <emcdonald@squiz.net>
2006-01-30 06:00:14 +0000

    Script will now properly delete links and link tree entries for assets of the type being removed.

--- scripts/remove_assets_of_type.php
+++ scripts/remove_assets_of_type.php
@@ -18,7 +18,7 @@
 * | licence.                                                           |
 * +--------------------------------------------------------------------+
 *
-* $Id: remove_assets_of_type.php,v 1.3 2005/06/30 23:22:47 tbarrett Exp $
+* $Id: remove_assets_of_type.php,v 1.4 2006/01/30 06:00:14 emcdonald Exp $
 *
 */
 
@@ -29,11 +29,13 @@
 * assets of exactly the type you specify
 *
 * @author  Tom Barrett <tbarrett@squiz.net>
-* @version $Revision: 1.3 $
+* @version $Revision: 1.4 $
 * @package MySource_Matrix
 */
 error_reporting(E_ALL);
-if ((php_sapi_name() != 'cli')) trigger_error("You can only run this script from the command line\n", E_USER_ERROR);
+if ((php_sapi_name() != 'cli')) {
+	trigger_error("You can only run this script from the command line\n", E_USER_ERROR);
+}
 
 $SYSTEM_ROOT = (isset($_SERVER['argv'][1])) ? $_SERVER['argv'][1] : '';
 if (empty($SYSTEM_ROOT) || !is_dir($SYSTEM_ROOT)) {
@@ -55,7 +57,7 @@ echo 'Enter the root password for "'.SQ_CONF_SYSTEM_NAME.'": ';
 $root_password = rtrim(fgets(STDIN, 4094));
 
 // check that the correct root password was entered
-$root_user = &$GLOBALS['SQ_SYSTEM']->am->getSystemAsset('root_user');
+$root_user =& $GLOBALS['SQ_SYSTEM']->am->getSystemAsset('root_user');
 if (!$root_user->comparePassword($root_password)) {
 	echo "ERROR: The root password entered was incorrect\n";
 	exit();
@@ -68,19 +70,38 @@ if (!$GLOBALS['SQ_SYSTEM']->setCurrentUser($root_user)) {
 }
 
 // make some noiz
-$db = $GLOBALS['SQ_SYSTEM']->db;
+$GLOBALS['SQ_SYSTEM']->changeDatabaseConnection('db2');
+$GLOBALS['SQ_SYSTEM']->doTransaction('BEGIN');
 
-$assets_of_type = $db->getCol("SELECT assetid FROM sq_ast WHERE type_code=".$db->quote($DELETING_ASSET_TYPE));
+$db =& $GLOBALS['SQ_SYSTEM']->db;
+
+$assets_of_type = $db->getCol('SELECT assetid FROM sq_ast WHERE type_code='.$db->quote($DELETING_ASSET_TYPE));
+assert_valid_db_result($assets_of_type);
 if (!empty($assets_of_type)) {
 	$asset_ids_set = '('.implode(', ', $assets_of_type).')';
+
 	$res =& $db->query('DELETE FROM sq_ast_attr_val WHERE assetid in '.$asset_ids_set);
-	assert_valid_db_result($res);
-	$res =& $db->query('DELETE FROM sq_ast_lnk WHERE minorid in '.$asset_ids_set);
 	assert_valid_db_result($res);
 	$res =& $db->query('DELETE FROM sq_ast WHERE type_code = '.$db->quote($DELETING_ASSET_TYPE));
 	assert_valid_db_result($res);
-	$res =& $db->query('UPDATE sq_ast_lnk_tree SET linkid=0 where linkid in '.$links_set);
-	assert_valid_db_result($res);
 
+	$res =& $db->getAll('SELECT linkid FROM sq_ast_lnk WHERE minorid in '.$asset_ids_set.' OR majorid in '.$asset_ids_set);
+	assert_valid_db_result($res);
+	if (!empty($res)) {
+		$links_array = Array();
+		foreach ($res as $value) {
+			array_push($links_array, $value['linkid']);
+		}
+		$links_set = '('.implode(', ', $links_array).')';
+
+		$res =& $db->query('DELETE FROM sq_ast_lnk WHERE linkid in '.$links_set);
+		assert_valid_db_result($res);
+		$res =& $db->query('UPDATE sq_ast_lnk_tree SET linkid=0 where linkid in '.$links_set);
+		assert_valid_db_result($res);
+	}
+	$GLOBALS['SQ_SYSTEM']->doTransaction('COMMIT');
+	$GLOBALS['SQ_SYSTEM']->restoreDatabaseConnection();
 }
+
+
 ?>
