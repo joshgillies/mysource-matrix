27d5e4626cf4050d6935bd99f4aa61f9df9a819b
Darren McKee <dmckee@squiz.net>
2006-01-31 01:05:17 +0000

    Bug Fix #1112: Fix for keyword replacement. Script error caused keyword inserter to fail on insert

--- fudge/wysiwyg/core/core.js
+++ fudge/wysiwyg/core/core.js
@@ -17,7 +17,7 @@
 * | licence.                                                           |
 * +--------------------------------------------------------------------+
 *
-* $Id: core.js,v 1.20.2.3 2006/01/17 00:29:55 skim Exp $
+* $Id: core.js,v 1.20.2.4 2006/01/31 01:05:17 dmckee Exp $
 *
 */
 
@@ -290,8 +290,10 @@ HTMLArea.prototype.insertHTML = function(html, range) {
 		var sel = this._getSelection();
 		var range = this._createRange(sel);
 		var node = range.startContainer;
-		if (node.tagName == 'HTML') {
-			return false;
+		if (!HTMLArea.is_ie) {
+			if (node.tagName == 'HTML') {
+				return false;
+			}
 		}
 	}
 	if (HTMLArea.is_ie) {
