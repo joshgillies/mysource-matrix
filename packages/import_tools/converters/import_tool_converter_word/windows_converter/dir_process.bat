17d9a1b15bde204b2ea1b2a5fdfecac96ced48de
Darren McKee <dmckee@squiz.net>
2006-02-20 22:36:45 +0000

    This commit was generated by cvs2svn to compensate for changes in r10358, which included commits to RCS files with non-trunk default branches.

---
+++ packages/import_tools/converters/import_tool_converter_word/windows_converter/dir_process.bat
@@ -0,0 +1,20 @@
+@IF "%1" == "" GOTO END
+@IF "%2" == "" GOTO END
+
+mkdir %1intermediate
+
+@FOR %%Z IN (%1*.doc) DO CALL process.bat %%Z %1intermediate\
+@FOR %%Z IN (%1intermediate\*.htm) DO CALL filter.bat %%Z
+@CALL COPY %1*.doc %2
+XCOPY %1intermediate %2 /S /E
+@CALL DEL %1intermediate\*.htm
+
+@FOR %%Z IN (%1*.dot)  DO CALL process.bat %%Z %1intermediate
+@FOR %%Z IN (%1intermediate\*.htm) DO CALL filter.bat %%Z
+@CALL COPY %1*.dot %2
+XCOPY %1intermediate %2 /S /E
+@CALL DEL %1intermediate\*.htm
+
+@CALL RMDIR %1intermediate
+
+:END
---
+++ packages/import_tools/converters/import_tool_converter_word/windows_converter/process.bat
@@ -0,0 +1,31 @@
+@IF "%1" == "" GOTO NO_FILE
+@IF "%2" == "" GOTO NO_FILE_2
+
+@IF "%1" == "." GOTO GRACEFUL
+@IF "%1" == ".." GOTO GRACEFUL
+
+@ECHO Processing file %1
+
+@CALL "doc2html.exe" %1 /d:%2 /l:c:\scripts\log.log
+
+@GOTO SUCCESS
+
+:NO_FILE
+@ECHO No Input File Name Specified
+@GOTO END
+
+:NO_FILE_2
+@ECHO No Output Directory Specified
+@GOTO END
+
+:SUCCESS
+@ECHO %1 successfully Converted
+@ECHO FILE COMPLETED
+@GOTO END
+
+:GRACEFUL
+@ECHO %1 -> Directory or parent?
+@GOTO END
+
+:END
+
