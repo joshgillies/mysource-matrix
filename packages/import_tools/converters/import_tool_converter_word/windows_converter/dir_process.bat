17616bb98ba8d4f3964e67c368e2bf8b19d7bcfa
Darren McKee <dmckee@squiz.net>
2006-03-10 03:38:15 +0000

    Change to the windows side processing. Avoids any possible prompting (such as overwrite) so the script can execute without attention

--- packages/import_tools/converters/import_tool_converter_word/windows_converter/dir_process.bat
+++ packages/import_tools/converters/import_tool_converter_word/windows_converter/dir_process.bat
@@ -4,17 +4,11 @@
 mkdir %1intermediate
 
 @FOR %%Z IN (%1*.doc) DO CALL process.bat %%Z %1intermediate\
-@FOR %%Z IN (%1intermediate\*.htm) DO CALL filter.bat %%Z
-@CALL COPY %1*.doc %2
-XCOPY %1intermediate %2 /S /E
-@CALL DEL %1intermediate\*.htm
-
 @FOR %%Z IN (%1*.dot)  DO CALL process.bat %%Z %1intermediate
 @FOR %%Z IN (%1intermediate\*.htm) DO CALL filter.bat %%Z
+@CALL COPY %1*.doc %2
 @CALL COPY %1*.dot %2
-XCOPY %1intermediate %2 /S /E
-@CALL DEL %1intermediate\*.htm
-
-@CALL RMDIR %1intermediate
+XCOPY %1intermediate %2 /S /E /-Y
+@CALL RMDIR %1intermediate\ /q /s
 
 :END
