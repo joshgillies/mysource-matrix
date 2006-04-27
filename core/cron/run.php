a79e324d325d6550523de395e475cc0b221eb52e
Luke Wright <lwright@squiz.com.au>
2006-04-27 03:12:35 +0000

    Changes to make Cron script run with symlinked code. If it's on a *nix system (with /home/lwright/matrix_installs/matrix_oracle_dev/core/cron defined), it'll work out where the script is being run from - CLI doesn't give us a huge amount so we have to work some of it out ourselves. If it's on Windows, there'll be no chance of symbolic links anyway so this really has no effect

--- core/cron/run.php
+++ core/cron/run.php
@@ -18,7 +18,7 @@
 * | licence.                                                           |
 * +--------------------------------------------------------------------+
 *
-* $Id: run.php,v 1.12 2006/01/20 06:07:22 rong Exp $
+* $Id: run.php,v 1.13 2006/04/27 03:12:35 lwright Exp $
 *
 */
 
@@ -28,12 +28,32 @@
 * The one file through which everything runs
 *
 * @author  Blair Robertson <blair@squiz.net>
-* @version $Revision: 1.12 $
+* @version $Revision: 1.13 $
 * @package MySource_Matrix
 */
+
+// We need to work out where we have come from, such as whether it's a symbolically
+// linked file (PHP's functions give the resolved link). If the path given to
+// PHP is absolute, use that, otherwise tack on the working directory (PWD) to it.
+// TODO: this doesn't work if there are any '..' in the passed path
+//
+// (On Windows, you probably wouldn't have PWD so you can't use this - but then
+// you probably don't have symbolic links either!)
+if (!empty($_SERVER['PWD'])) {
+	$run_dir = $_SERVER['PWD'];
+	$script_path = $_SERVER['argv'][0];
+	if ($script_path{0} == '/') {
+		// absolute path
+		define('SQ_SYSTEM_ROOT', dirname(dirname(dirname(make_proper_path($script_path)))));
+	} else {
+		// relative path - append the run dir to the script path
+		define('SQ_SYSTEM_ROOT', dirname(dirname(dirname(make_proper_path($run_dir.'/'.$script_path)))));
+	}
+}
+
 ini_set('memory_limit', '16M');
-ini_set('error_log', dirname(dirname(dirname(__FILE__))).'/cache/error.log');
-require_once dirname(dirname(__FILE__)).'/include/init.inc';
+ini_set('error_log', SQ_SYSTEM_ROOT.'/cache/error.log');
+require_once SQ_SYSTEM_ROOT.'/core/include/init.inc';
 
 $root_user = &$GLOBALS['SQ_SYSTEM']->am->getSystemAsset('root_user');
 if (is_null($root_user)) {
@@ -60,5 +80,59 @@ if (!empty($_SERVER['argv'][1]) && $_SERVER['argv'][1] == 'RESET_RUNNING') {
 }
 
 $cron_mgr->run();
+exit(0);
 
+
+/**
+* Make a proper unix path (ripped from remote content)
+*
+* Works similar to php's realpath(), but doesn't rely on a file system
+* Given a string representing a path, it tries to remove the relative references
+* in short: given "/this/dir/../another/file.php" will produce "/this/another/file.php"
+**
+* NOTE: paths that try to jump outside the root will produce possibly erroneous result
+* eg.: "/root/dir/../../../another/file.php" will become "/another/file.php"
+* this behaviour is similar to how browsers treat relative paths
+*
+* @param string	$path	path that needs shortening
+*
+* @return string
+* @access public
+*/
+function make_proper_path($path='')
+{
+	if (empty($path))
+		return '';
+
+	$root = '';
+	$path_components = explode('/',$path);
+
+	if (empty($path_components[0])) {
+		$root = '/';
+		unset($path_components[0]);
+	}
+
+	$stack = Array();
+
+	foreach ($path_components as $component) {
+		switch ($component) {
+			case '..':
+				if (!empty($stack)) array_pop($stack);
+			break;
+
+			case '.':
+			case '':
+				continue;
+			break;
+
+			default:
+				array_push($stack, $component);
+		}
+	}
+
+	$new_path = implode('/', $stack);
+
+	return $root.$new_path;
+
+}//end make_proper_path()
 ?>
