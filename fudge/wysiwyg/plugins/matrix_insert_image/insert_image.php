a44211ab29d0a81c10029e4357982dcdfb62b6c9
Sertan Danis <sdanis@squiz.com.au>
2006-04-30 23:31:27 +0000

    Fixed bug #1320: Cannot edit image src after pasting it from external content. Original src will be used if the image is not a Matrix asset.

--- fudge/wysiwyg/plugins/matrix_insert_image/insert_image.php
+++ fudge/wysiwyg/plugins/matrix_insert_image/insert_image.php
@@ -18,7 +18,7 @@
 * | licence.                                                           |
 * +--------------------------------------------------------------------+
 *
-* $Id: insert_image.php,v 1.35 2005/11/28 04:19:31 dmckee Exp $
+* $Id: insert_image.php,v 1.35.2.1 2006/04/30 23:31:27 sdanis Exp $
 *
 */
 
@@ -26,7 +26,7 @@
 * Insert Image Popup for the WYSIWYG
 *
 * @author  Greg Sherwood <gsherwood@squiz.net>
-* @version $Revision: 1.35 $
+* @version $Revision: 1.35.2.1 $
 * @package MySource_Matrix
 */
 
@@ -280,7 +280,8 @@ if (!isset($_GET['f_imageid'])) $_GET['f_imageid'] = 0;
 											<tr>
 												<td class="label" nowrap="nowrap"><?php echo translate('image_url'); ?>:</td>
 												<td>
-													<?php asset_finder('f_imageid', $_GET['f_imageid'], Array('image' => 'D', 'image_variety' => 'D'), '', false, 'setImageInfo'); ?>
+													<?php
+													asset_finder('f_imageid', (assert_valid_assetid($_GET['f_imageid'], '', TRUE, FALSE) ? $_GET['f_imageid'] : 0), Array('image' => 'D', 'image_variety' => 'D'), '', false, 'setImageInfo'); ?>
 												</td>
 											</tr>
 											<tr>
