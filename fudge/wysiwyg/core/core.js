70fda9ecd5506e9e2eeecbf4f45577ef2323b65a
Ben Caldwell <bcaldwell@squiz.net>
2006-10-31 03:38:55 +0000

    Have hopefully _finally_ eradicated all of the WYSIWYG editor escaping/encoding problems. The Javascript of the WYSIWYG Editor was escaping its contents using the escape() and unescape() functions, which corrupts UTF-8 strings (i.e. backend encoding set to UTF-8 when entering a non-ASCII character). Changing this to encodeURI() and decodeURI() solved the issue of corrupt UTF-8 strings, but created a new escaping problem with certain characters (PHP rawurldecode() could not handle the complete encoded string from the encodeURI() javascript function).
    By completley removing the escaping a bug was identified where the backend after clicking the submit button did not have the correct encoding.
    
    WHEN UPGRADING THE BROWSER CACHE MUST BE CLEARED OTHERWISE THE OLD WYSIWYG JAVASCRIPT WILL BE USED AND CONTENTS ENTERED USING THE WSYIWYG EDITOR MAY BE CORRUPT.

--- fudge/wysiwyg/core/core.js
+++ fudge/wysiwyg/core/core.js
@@ -17,7 +17,7 @@
 * | licence.                                                           |
 * +--------------------------------------------------------------------+
 *
-* $Id: core.js,v 1.31 2006/10/30 23:41:15 bcaldwell Exp $
+* $Id: core.js,v 1.32 2006/10/31 03:38:55 bcaldwell Exp $
 *
 */
 
@@ -141,7 +141,7 @@ HTMLArea.prototype.generate = function () {
 		html += "</style>\n";
 		html += "</head>\n";
 		html += "<body>\n";
-		html += unescape(editor._textArea.value);
+		html += editor._textArea.value;
 		html += "</body>\n";
 		html += "</html>";
 		doc.write(html);
@@ -183,7 +183,7 @@ HTMLArea.prototype.generate = function () {
 		}
 
 
-		editor._doc.innerHTML = unescape(editor._textArea.value);
+		editor._doc.innerHTML = editor._textArea.value;
 		if (HTMLArea.is_ie) { editor._doc.contentEditable = true; }
 		// intercept some events; for updating the toolbar & keyboard handlers
 		HTMLArea._addEvents
