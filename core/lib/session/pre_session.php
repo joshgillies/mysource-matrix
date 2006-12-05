d89ecd0e276d8e68ca710f051ab92e12dad0ad80
Ben Caldwell <bcaldwell@squiz.net>
2006-12-05 05:07:54 +0000

    MySource Matrix licence headers updated.

--- core/lib/session/pre_session.php
+++ core/lib/session/pre_session.php
@@ -1,4 +1,18 @@
 <?php
+/**
+* +--------------------------------------------------------------------+
+* | This MySource Matrix CMS file is Copyright (c) Squiz Pty Ltd       |
+* | ACN 084 670 600                                                    |
+* +--------------------------------------------------------------------+
+* | IMPORTANT: Your use of this Software is subject to the terms of    |
+* | the Licence provided in the file licence.txt. If you cannot find   |
+* | this file please contact Squiz (www.squiz.net) so we may provide   |
+* | you a copy.                                                        |
+* +--------------------------------------------------------------------+
+*
+* $Id: pre_session.php,v 1.6 2006/12/05 05:07:54 bcaldwell Exp $
+*
+*/
 
 if (!isset($_SESSION['PRIMARY_SESSIONID'])) {
 	reload_browser(true, $SQ_SITE_NETWORK);
