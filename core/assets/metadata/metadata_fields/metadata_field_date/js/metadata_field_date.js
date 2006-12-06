502fa5f58665430e9d36c87cfe87ed0ca70c0bf0
Elden McDonald <emcdonald@squiz.net>
2006-12-06 01:07:37 +0000

    MySource Matrix licence headers updated

--- core/assets/metadata/metadata_fields/metadata_field_date/js/metadata_field_date.js
+++ core/assets/metadata/metadata_fields/metadata_field_date/js/metadata_field_date.js
@@ -1,3 +1,18 @@
+/**
+* +--------------------------------------------------------------------+
+* | This MySource Matrix CMS file is Copyright (c) Squiz Pty Ltd       |
+* | ACN 084 670 600                                                    |
+* +--------------------------------------------------------------------+
+* | IMPORTANT: Your use of this Software is subject to the terms of    |
+* | the Licence provided in the file licence.txt. If you cannot find   |
+* | this file please contact Squiz (www.squiz.net) so we may provide   |
+* | you a copy.                                                        |
+* +--------------------------------------------------------------------+
+*
+* $Id: metadata_field_date.js,v 1.2 2006/12/06 01:07:35 emcdonald Exp $
+*
+*/
+
 function setInputsEnabled(parent, enabled)
 {
 	var inputs = parent.getElementsByTagName('INPUT');
