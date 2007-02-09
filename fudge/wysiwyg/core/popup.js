1f8546ce2ef36f50256f35af8853ff2928d65a26
Tom Barrett <tbarrett@squiz.net>
2007-02-09 00:32:56 +0000

    Fixing bug #2101 (table properties popup not working in IE); now that several popups have been changed from modal (where arguments are passed natively by IE) to non-modal (where as far as the browser is concerned it's just another normal window), we need to pass arguments to them ourselves in both Moz and IE

--- fudge/wysiwyg/core/popup.js
+++ fudge/wysiwyg/core/popup.js
@@ -9,7 +9,7 @@
 * | you a copy.                                                        |
 * +--------------------------------------------------------------------+
 *
-* $Id: popup.js,v 1.10 2006/12/06 05:11:07 bcaldwell Exp $
+* $Id: popup.js,v 1.10.2.1 2007/02/09 00:32:56 tbarrett Exp $
 *
 */
 
@@ -20,28 +20,26 @@ function __dlg_onclose(code) {
 };
 
 function __dlg_init(code) {
-	if (!document.all) {
-		// init dialogArguments, as IE gets it
-		window.dialogArguments = opener.dialogWins[code].args;
+	// init dialogArguments, as IE gets it
+	window.dialogArguments = opener.dialogWins[code].args;
 
-		if (opener.dialogWins[code].isModal) {
-			window.sizeToContent();
-			window.sizeToContent();	// for reasons beyond understanding,
-									// only if we call it twice we get the
-									// correct size.
-			window.addEventListener("unload", __dlg_onclose(code), true);
-			// center on parent
-			var px1 = opener.screenX;
-			var px2 = opener.screenX + opener.outerWidth;
-			var py1 = opener.screenY;
-			var py2 = opener.screenY + opener.outerHeight;
-			var x = (px2 - px1 - window.outerWidth) / 2;
-			var y = (py2 - py1 - window.outerHeight) / 2;
-			window.moveTo(x, y);
-			var body = document.body;
-			window.innerHeight = body.offsetHeight;
-			window.innerWidth = body.offsetWidth;
-		}
+	if (opener.dialogWins[code].isModal) {
+		window.sizeToContent();
+		window.sizeToContent();	// for reasons beyond understanding,
+								// only if we call it twice we get the
+								// correct size.
+		window.addEventListener("unload", __dlg_onclose(code), true);
+		// center on parent
+		var px1 = opener.screenX;
+		var px2 = opener.screenX + opener.outerWidth;
+		var py1 = opener.screenY;
+		var py2 = opener.screenY + opener.outerHeight;
+		var x = (px2 - px1 - window.outerWidth) / 2;
+		var y = (py2 - py1 - window.outerHeight) / 2;
+		window.moveTo(x, y);
+		var body = document.body;
+		window.innerHeight = body.offsetHeight;
+		window.innerWidth = body.offsetWidth;
 	}
 };
 
