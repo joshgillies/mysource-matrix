9f8fe756bbc9267e7b840ec25415b38cd11ae1db
Tom Barrett <tbarrett@squiz.net>
2007-04-12 06:53:08 +0000

    Fixing bug #2323 (Table Properties - Vertical alignment wasn't working properly) by correcting capitalisation in the line that finds the vertical-alignment from the imported DOM objects

--- fudge/wysiwyg/plugins/table_editing/table-editor.js
+++ fudge/wysiwyg/plugins/table_editing/table-editor.js
@@ -17,7 +17,7 @@
 * | licence.                                                           |
 * +--------------------------------------------------------------------+
 *
-* $Id: table-editor.js,v 1.17 2006/06/16 05:00:03 sdanis Exp $
+* $Id: table-editor.js,v 1.17.2.1 2007/04/12 06:53:08 tbarrett Exp $
 *
 */
 
@@ -1249,7 +1249,7 @@ TTable = function(name, rows, cols)
 					Cell.content	= cell.innerHTML;
 					Cell.th = (cell.tagName == "TH");
 					Cell.align		= (cell.align == "")?"left":cell.align;
-					Cell.valign		= (cell.valign + "" == "undefined")?"middle":cell.valign;
+					Cell.valign		= (cell.vAlign + "" == "undefined")?"middle":cell.vAlign;
 					Cell.colspan	= cell.colSpan;
 					Cell.rowspan	= cell.rowSpan;
 					Cell.abbr		= cell.abbr;
