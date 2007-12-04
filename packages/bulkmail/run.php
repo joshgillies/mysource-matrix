1c7de37ad07eb4032c870665cefb72c1cd63b549
Rayn Ong <rong@squiz.net>
2007-12-04 23:41:30 +0000

    ref fix

--- packages/bulkmail/run.php
+++ packages/bulkmail/run.php
@@ -11,7 +11,7 @@
 * | Module if you have the written consent of Squiz.                   |
 * +--------------------------------------------------------------------+
 *
-* $Id: run.php,v 1.3 2006/12/07 05:55:55 bcaldwell Exp $
+* $Id: run.php,v 1.4 2007/12/04 23:41:12 rong Exp $
 *
 */
 
@@ -22,14 +22,14 @@
 *
 * @author  Nathan de Vries <ndvries@squiz.net>
 * @author  Rayne Ong <rong@squiz.net>
-* @version $Revision: 1.3 $
+* @version $Revision: 1.4 $
 * @package MySource_Matrix
 */
 
 ini_set('memory_limit', -1);
 require_once dirname(__FILE__).'/bulk_mailer.inc';
 
-$bulk_mailer =& new Bulk_Mailer();
+$bulk_mailer = new Bulk_Mailer();
 $bulk_mailer->start();
 
 ?>
