6c21b2655b03ca304026000fff05e742c212683a
Mark Brydon <mbrydon@squiz.net>
2008-07-16 08:01:43 +0000

    Rejiggied the script to provide two more command-line options. The first (--make-folders-live) allows the user to specify whether newly-created dated folders are to be set Live (default is Under Construction). The second (--move-asset-status) is used to further filter the candidate assets by asset status before performing the move operation

--- scripts/move_assets_to_dated_folders.php
+++ scripts/move_assets_to_dated_folders.php
@@ -10,7 +10,7 @@
 * | you a copy.                                                        |
 * +--------------------------------------------------------------------+
 *
-* $Id: move_assets_to_dated_folders.php,v 1.3 2008/07/11 01:33:11 mbrydon Exp $
+* $Id: move_assets_to_dated_folders.php,v 1.4 2008/07/16 08:00:28 mbrydon Exp $
 *
 */
 
@@ -21,10 +21,10 @@
 *
 * If a suitable folder structure exists under the parent asset, it will be re-used.
 *
-* Credit to Richard Hulse for this concept which is now available to the Matrix Community!
+* Credit to Richard Hulse (Radio NZ) for this concept which is now available to the Matrix Community!
 *
 * @author  Mark Brydon <mbrydon@squiz.net>
-* @version $Revision: 1.3 $
+* @version $Revision: 1.4 $
 * @package MySource_Matrix
 */
 
@@ -38,14 +38,20 @@
 function printUsage()
 {
         echo "Move assets into dated folders\n\n";
-        echo "Usage: move_assets_to_dated_folders [system root] [root node asset ID] [asset type code] [asset date field] [time period] (folder link type)\n";
+	echo "Usage: move_assets_to_dated_folders [system root] --root=[root node asset ID] --type=[asset type code]\n";
+	echo "                                    --field=[asset date field] --period=[time period] (--folder-link-type=[folder link type])\n";
+	echo "                                    (--move-asset-status=[move asset status]) (--make-folders-live)\n\n";
+	echo "REQUIRED PARAMETERS ==========\n";
         echo "system root         : The Matrix System Root directory\n";
 	echo "root node asset ID  : The asset under which child assets are to be moved\n";
 	echo "asset type code     : The type of assets to be moved\n";
 	echo "asset date field    : The date field determining the destination for each asset - either 'created' or 'published'\n";
         echo "time period         : A specification of the folder structure to be created - either 'year', 'month' or 'day'\n";
-	echo "                      eg; when 'asset date field' is 'created', 'day' will create or re-use a structure of 2008 > 03 > 28 for assets created on 28th March 2008\n";
-	echo "folder link type    : The link type for newly-created folders (either 1 or 2). This is optional and will default to Type 2\n\n";
+	echo "                      eg; when 'asset date field' is 'created', 'day' will create or re-use a structure of 2008 > 03 > 28 for assets created on 28th March 2008\n\n";
+	echo "OPTIONAL PARAMETERS ==========\n";
+	echo "folder link type    : The link type for newly-created folders (either 1 or 2). Type 2 links are used by default\n";
+	echo "move asset status   : The status code which must be matched for each asset before moving (eg; set to '16' to move only 'Live' assets)\n";
+	echo "-make-folders-live  : Set the new date folders to 'Live' upon creation. Otherwise they will be 'Under Construction'\n\n";
 
 }//end printUsage()
 
@@ -55,15 +61,16 @@ function printUsage()
 * If the expected folders do not exist under the parent, they will be created.
 * The asset ID of the matching year/month/day folder is returned.
 *
-* @param int	$parent_id			The asset ID from which to search for matching a year/month folder combination
-* @param int	$create_timestamp	The folder specification to search (eg; where period = "month", create_timestamp = 0 = 1 Jan 1970 = search for folder structure 1970 > 01)
-* @param string	$period				The period to search or create - one of "year", "month", or "day"
-* @param int	$folder_link_type	The asset link type to use when creating new dated folders
+* @param int		$parent_id			The asset ID from which to search for matching a year/month folder combination
+* @param int		$create_timestamp	The folder specification to search (eg; where period = "month", create_timestamp = 0 = 1 Jan 1970 = search for folder structure 1970 > 01)
+* @param string		$period				The period to search or create - one of "year", "month", or "day"
+* @param int		$folder_link_type	The asset link type to use when creating new dated folders
+* @param boolean	$make_folders_live      Whether to make the newly-created dated folders live (default = Under Construction)
 *
 * @return int
 * @access public
 */
-function searchExistingDatedFolder($parent_id, $create_timestamp, $period, $folder_link_type=SQ_LINK_TYPE_2)
+function searchExistingDatedFolder($parent_id, $create_timestamp, $period, $folder_link_type, $make_folders_live=FALSE)
 {
 	$am =& $GLOBALS['SQ_SYSTEM']->am;
 
@@ -84,7 +91,7 @@ function searchExistingDatedFolder($parent_id, $create_timestamp, $period, $fold
 
 	if ($num_found_assets > 1) {
 		echo "\n- FAILED - found ".$num_found_assets.' year folders for '.$year."\n";
-		exit();
+		return 0;
 	}
 
 	if ($num_found_assets == 1) {
@@ -95,6 +102,9 @@ function searchExistingDatedFolder($parent_id, $create_timestamp, $period, $fold
 		echo "\n- Creating Year Folder ".$year.'... ';
 		$matching_year_folder_id = createAsset('folder', $year, $parent_id, $folder_link_type);
 		echo 'asset #'.$matching_year_folder_id."\n";
+		if ($make_folders_live) {
+			setAssetStatus($matching_year_folder_id, SQ_STATUS_LIVE);
+		}
 	}
 
 	$matching_folder_id = $matching_year_folder_id;
@@ -107,7 +117,7 @@ function searchExistingDatedFolder($parent_id, $create_timestamp, $period, $fold
 
 		if ($num_found_assets > 1) {
 			echo "\n- FAILED - found ".$num_found_assets.' month folders for year/month '.$year.'/'.$month."\n";
-			exit();
+			return 0;
 		}
 
 		if ($num_found_assets == 1) {
@@ -118,6 +128,9 @@ function searchExistingDatedFolder($parent_id, $create_timestamp, $period, $fold
 			echo "\n- Creating Month Folder ".$month.' under Year '.$year.'... ';
 			$matching_month_folder_id = createAsset('folder', $month, $matching_year_folder_id, $folder_link_type);
 			echo 'asset #'.$matching_month_folder_id."\n";
+			if ($make_folders_live) {
+				setAssetStatus($matching_month_folder_id, SQ_STATUS_LIVE);
+			}
 		}
 
 		$matching_folder_id = $matching_month_folder_id;
@@ -131,7 +144,7 @@ function searchExistingDatedFolder($parent_id, $create_timestamp, $period, $fold
 
 		if ($num_found_assets > 1) {
 			echo "\n- FAILED - found ".$num_found_assets.' day folders for year/month/day '.$year.'/'.$month.'/'.$day."\n";
-			exit();
+			return 0;
 		}
 
 		if ($num_found_assets == 1) {
@@ -142,6 +155,9 @@ function searchExistingDatedFolder($parent_id, $create_timestamp, $period, $fold
 			echo "\n- Creating Day Folder ".$day.' under Year/Month '.$year.'/'.$month.'... ';
 			$matching_day_folder_id = createAsset('folder', $day, $matching_month_folder_id, $folder_link_type);
 			echo 'asset #'.$matching_day_folder_id."\n";
+			if ($make_folders_live) {
+				setAssetStatus($matching_day_folder_id, SQ_STATUS_LIVE);
+			}
 		}
 
 		$matching_folder_id = $matching_day_folder_id;
@@ -181,7 +197,9 @@ function searchExistingAsset($parent_id, $asset_name, $asset_type_code='')
 		$query = MatrixDAL::preparePdoQuery($sql);
 		MatrixDAL::bindValueToPdo($query, 'majorid', $parent_id);
 		MatrixDAL::bindValueToPdo($query, 'asset_name', $asset_name);
-		if (!empty($asset_type_code)) MatrixDAL::bindValueToPdo($query, 'type_code', $asset_type_code);
+		if (!empty($asset_type_code)) {
+			MatrixDAL::bindValueToPdo($query, 'type_code', $asset_type_code);
+		}
 		$matching_assets = MatrixDAL::executePdoAssoc($query, 0);
 	} catch (Exception $e) {
 		throw new Exception('Unable to search for an existing '.$asset_name.' asset: '.$e->getMessage());
@@ -238,16 +256,17 @@ function createAsset($asset_type, $asset_name, $parent_asset_id, $link_type=SQ_L
 /**
 * Moves an asset to a dated Folder structure under the specified parent asset
 *
-* @param int	$asset_id			The asset to move
-* @param int	$parent_id			The destination for the folder structure
-* @param string	$asset_date_field	One of 'created' or 'published' - the asset timestamp value to be used to associate the asset with a folder
-* @param string	$time_period		One of 'year', 'month' or 'day' - the folder structure to create (ie; three-folder structure created/used for day - 2008 > 03 > 28)
-* @param int	$folder_link_type	The asset link type to use when creating new dated folders
+* @param int		$asset_id			The asset to move
+* @param int		$parent_id			The destination for the folder structure
+* @param string		$asset_date_field	One of 'created' or 'published' - the asset timestamp value to be used to associate the asset with a folder
+* @param string		$time_period		One of 'year', 'month' or 'day' - the folder structure to create (ie; three-folder structure created/used for day - 2008 > 03 > 28)
+* @param int		$folder_link_type	The asset link type to use when creating new dated folders
+* @param boolean	$make_folders_live	Whether to make the newly-created dated folders live (default = Under Construction)
 *
 * @return boolean
 * @access public
 */
-function moveAssetToDatedFolder($asset_id, $parent_id, $asset_date_field, $time_period, $folder_link_type=SQ_LINK_TYPE_2)
+function moveAssetToDatedFolder($asset_id, $parent_id, $asset_date_field, $time_period, $folder_link_type, $make_folders_live=FALSE)
 {
 	$result = FALSE;
 	$am =& $GLOBALS['SQ_SYSTEM']->am;
@@ -261,7 +280,7 @@ function moveAssetToDatedFolder($asset_id, $parent_id, $asset_date_field, $time_
 
 		if ($asset_timestamp != NULL) {
 			// Find a destination folder for our asset
-			$destination_folder_id = searchExistingDatedFolder($parent_id, $asset_timestamp, $time_period, $folder_link_type);
+			$destination_folder_id = searchExistingDatedFolder($parent_id, $asset_timestamp, $time_period, $folder_link_type, $make_folders_live);
 
 			if ($destination_folder_id > 0) {
 				if ($asset_id == $destination_folder_id) {
@@ -332,6 +351,29 @@ function moveAsset($source_asset_id, $source_parent_id, $destination_parent_id)
 }//end moveAsset()
 
 
+/**
+* Immediately sets the status of a given asset
+*
+* @param int    $asset_id       The asset ID of the asset to set the status
+* @param int    $status         The status to set
+*
+* @return void
+* @access public
+*/
+function setAssetStatus($asset_id, $status)
+{
+	$am =& $GLOBALS['SQ_SYSTEM']->am;
+	$vars = Array(
+			'new_status'	=> $status,
+			'assetid'	=> $asset_id,
+		);
+	$hh =& $GLOBALS['SQ_SYSTEM']->getHipoHerder();
+	$errors = $hh->freestyleHipo('hipo_job_edit_status', $vars);
+	return (count($errors) == 0);
+
+}//end setAssetStatus()
+
+
 /************************** MAIN PROGRAM ****************************/
 
 error_reporting(E_ALL);
@@ -342,6 +384,19 @@ if ((php_sapi_name() != 'cli')) {
 	trigger_error("You can only run this script from the command line\n", E_USER_ERROR);
 }
 
+require_once 'Console/Getopt.php';
+
+$shortopt = '';
+$longopt = Array('root=', 'type=', 'field=', 'period=', 'folder-link-type=', 'make-folders-live', 'move-asset-status=');
+
+$args = Console_Getopt::readPHPArgv();
+array_shift($args);
+$options = Console_Getopt::getopt($args, $shortopt, $longopt);
+if (empty($options[0])) {
+	printUsage();
+	exit();
+}
+
 $SYSTEM_ROOT = (isset($_SERVER['argv'][1])) ? $_SERVER['argv'][1] : '';
 if (empty($SYSTEM_ROOT) || !is_dir($SYSTEM_ROOT)) {
 	printUsage();
@@ -350,61 +405,114 @@ if (empty($SYSTEM_ROOT) || !is_dir($SYSTEM_ROOT)) {
 }
 
 require_once $SYSTEM_ROOT.'/core/include/init.inc';
-
-$root_node = (isset($_SERVER['argv'][2])) ? (int)$_SERVER['argv'][2] : 0;
-if ($root_node <= 0) {
-	printUsage();
-	echo "Please specify a root node asset ID as the second argument\n";
-	exit();
-}
-
 $am =& $GLOBALS['SQ_SYSTEM']->am;
-$parent_asset =& $am->getAsset($root_node);
-if (!$parent_asset->id) {
-	printUsage();
-	echo "The specified root node asset was not found\n";
-	exit();
+
+$SYSTEM_ROOT = '';
+$root_node = 0;
+$asset_type_code = '';
+$asset_date_field = '';
+$time_period = '';
+$folder_link_type = SQ_LINK_TYPE_2;
+$make_folders_live = FALSE;
+$move_asset_status = -1;
+
+$mandatory_options = Array('--root', '--type', '--field', '--period');
+$found_options = Array();
+
+// Initial check for mandatory parameters
+foreach ($options[0] as $option) {
+	$found_options[] = $option[0];
 }
-$am->forgetAsset($parent_asset);
+reset($options[0]);
 
-$asset_type_code = (isset($_SERVER['argv'][3])) ? $_SERVER['argv'][3] : '';
-if (empty($asset_type_code)) {
-	printUsage();
-	echo "Please specify an asset type code as the third argument\n";
-	exit();
-}
-
-// Verify that the supplied asset code is correct
-$am->includeAsset($asset_type_code);
-
-$asset_date_field = (isset($_SERVER['argv'][4])) ? strtolower($_SERVER['argv'][4]) : '';
-if (empty($asset_date_field)) {
-	printUsage();
-	echo "Please specify an asset date field as the fourth argument\n";
-	exit();
-} else if (($asset_date_field != 'created') && ($asset_date_field != 'published')) {
-	printUsage();
-	echo "Please specify either 'created' or 'published' for the asset date field\n";
-	exit();
+$num_found_options = 0;
+foreach ($found_options as $found_option) {
+	if (in_array($found_option, $mandatory_options)) {
+		$num_found_options++;
+	}
 }
 
-$time_period = (isset($_SERVER['argv'][5])) ? strtolower($_SERVER['argv'][5]) : '';
-if (empty($time_period)) {
+if ($num_found_options <> count($mandatory_options)) {
 	printUsage();
-	echo "Please specify a time period as the fifth argument\n";
-	exit();
-} else if (($time_period != 'year') && ($time_period != 'month') && ($time_period != 'day')) {
-	printUsage();
-	echo "Please specify either 'year', 'month' or 'day' for the time period\n";
 	exit();
 }
 
-$folder_link_type = (isset($_SERVER['argv'][6])) ? (int)$_SERVER['argv'][6] : SQ_LINK_TYPE_2;
-if (($folder_link_type != SQ_LINK_TYPE_1) && ($folder_link_type != SQ_LINK_TYPE_2)) {
-	printUsage();
-	echo "Please specify either link type 1 or 2 for the folder link type\n";
-	exit();
-}
+// Process dynamic command-line parameters
+foreach ($options[0] as $option) {
+	switch ($option[0]) {
+		case '--root':
+			$root_node = (isset($option[1])) ? (int)$option[1] : 0;
+			if ($root_node <= 0) {
+				printUsage();
+				echo "Please specify a root node asset ID (--root)\n";
+				exit();
+			}
+
+			$parent_asset =& $am->getAsset($root_node);
+			if (!$parent_asset->id) {
+				printUsage();
+				echo "The specified root node asset was not found\n";
+				exit();
+			}
+			$am->forgetAsset($parent_asset);
+		break;
+
+		case '--type':
+			$asset_type_code = (isset($option[1])) ? $option[1] : '';
+			if (empty($asset_type_code)) {
+				printUsage();
+				echo "Please specify an asset type (--type)\n";
+				exit();
+			} else {
+				// Verify that the supplied asset code is correct
+				$am->includeAsset($asset_type_code);
+			}
+		break;
+
+		case '--field':
+			$asset_date_field = (isset($option[1])) ? strtolower($option[1]) : '';
+			if (empty($asset_date_field)) {
+				printUsage();
+				echo "Please specify an asset date field (--field)\n";
+				exit();
+			} else if (($asset_date_field != 'created') && ($asset_date_field != 'published')) {
+				printUsage();
+				echo "Please specify either 'created' or 'published' for the asset date field\n";
+				exit();
+			}
+		break;
+
+		case '--period':
+			$time_period = (isset($option[1])) ? strtolower($option[1]) : '';
+			if (empty($time_period)) {
+				printUsage();
+				echo "Please specify a time period (--period)\n";
+				exit();
+			} else if (($time_period != 'year') && ($time_period != 'month') && ($time_period != 'day')) {
+				printUsage();
+				echo "Please specify either 'year', 'month' or 'day' for the time period\n";
+				exit();
+			}
+		break;
+
+		case '--folder-link-type':
+			$folder_link_type = (isset($option[1])) ? (int)$option[1] : SQ_LINK_TYPE_2;
+			if (($folder_link_type != SQ_LINK_TYPE_1) && ($folder_link_type != SQ_LINK_TYPE_2)) {
+				printUsage();
+				echo "Please specify either link type 1 or 2 for the folder link type (--folder-link-type)\n";
+				exit();
+			}
+		break;
+
+		case '--make-folders-live':
+			$make_folders_live = TRUE;
+		break;
+
+		case '--move-asset-status':
+			$move_asset_status = (isset($option[1])) ? (int)$option[1] : -1;
+		break;
+	}//end switch
+}//end foreach
 
 // Ask for the root password for the system
 echo 'Enter the root password for "'.SQ_CONF_SYSTEM_NAME.'": ';
@@ -426,18 +534,32 @@ if (!$GLOBALS['SQ_SYSTEM']->setCurrentUser($root_user)) {
 
 // Get the immediate children of the parent asset that match the supplied type code
 $assets_to_move = $am->getChildren($root_node, $asset_type_code, TRUE, NULL, NULL, NULL, TRUE, 1, 1);
+$failed_asset_move = Array();
 
 if (count($assets_to_move) > 0) {
 	// Move the child assets
 	foreach (array_keys($assets_to_move) as $asset_id_to_move) {
-		echo '- Moving asset #'.$asset_id_to_move.'... ';
-		$result = moveAssetToDatedFolder($asset_id_to_move, $root_node, $asset_date_field, $time_period, $folder_link_type);
-		if ($result) {
-			echo "done\n";
-		} else {
-			echo "FAILED!\n";
-			exit();
+		// Only process assets in our "move list" that match the "move state"
+		$asset =& $am->getAsset($asset_id_to_move);
+		if (($move_asset_status == -1) || ($asset->status == $move_asset_status)) {
+			echo '- Moving asset #'.$asset_id_to_move.'... ';
+			$result = moveAssetToDatedFolder($asset_id_to_move, $root_node, $asset_date_field, $time_period, $folder_link_type, $make_folders_live);
+			if ($result) {
+				echo "done\n";
+			} else {
+				echo "FAILED!\n";
+				$failed_asset_move[] = $asset_id_to_move;
+			}
 		}
+		$am->forgetAsset($asset);
+	}
+
+	if (!empty($failed_asset_move) > 0) {
+		echo "\n*** The following assets could not be moved due to errors detailed above:\n";
+		foreach ($failed_asset_move as $failed_asset_move_id) {
+			echo '- Asset # '.$failed_asset_move_id."\n";
+		}
+		echo "\n";
 	}
 } else {
 	echo "- No matching assets were found to move. Script aborted\n";
