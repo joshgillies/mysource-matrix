c67e7491c54627465a637ed5fcbaf47eea87f684
Benjamin Pearson <bpearson@squiz.com.au>
2008-08-22 05:14:26 +0000

    New script: System integrity script to re-sort the sort_order on the sq_ast_lnk table. An incorrect sort order on the sq_ast_lnk can cause Asset Maps errors. Use the format: php system_integrity_fix_sort_orders.php [system_root] [root_node] to run the script. If no root node is supplied, it will start from the root folder. Be careful when running this script on large systems! This can take a long time!

---
+++ scripts/system_integrity_fix_sort_orders.php
@@ -0,0 +1,216 @@
+<?php
+/**
+* +--------------------------------------------------------------------+
+* | This MySource Matrix CMS file is Copyright (c) Squiz Pty Ltd       |
+* | ACN 084 670 600                                                    |
+* +--------------------------------------------------------------------+
+* | IMPORTANT: Your use of this Software is subject to the terms of    |
+* | the Licence provided in the file licence.txt. If you cannot find   |
+* | this file please contact Squiz (www.squiz.net) so we may provide   |
+* | you a copy.                                                        |
+* +--------------------------------------------------------------------+
+*
+* $Id: system_integrity_fix_sort_orders.php,v 1.1.2.1 2008/08/22 05:14:26 bpearson Exp $
+*
+*/
+
+/**
+* This script resorts the sort order of the SQ_AST_LNK table
+*
+* @author  Benjamin Pearson <bpearson@squiz.net>
+* @version $Revision: 1.1.2.1 $
+* @package MySource_Matrix
+*/
+error_reporting(E_ALL);
+if ((php_sapi_name() != 'cli')) {
+	trigger_error("You can only run this script from the command line\n", E_USER_ERROR);
+}
+
+$system_root = (isset($_SERVER['argv'][1])) ? $_SERVER['argv'][1] : '';
+if (empty($system_root) || !is_dir($system_root)) {
+	echo "ERROR: You need to supply the path to the System Root as the first argument\n";
+	exit();
+}
+
+require_once $system_root.'/core/include/init.inc';
+
+// If none set, use the default root
+$rootnodeid = (isset($_SERVER['argv'][2])) ? $_SERVER['argv'][2] : '1';
+$rootnode =& $GLOBALS['SQ_SYSTEM']->am->getAsset($rootnodeid);
+if (is_null($rootnode)) {
+	trigger_error("ERROR: Unable to load that root node\n");
+	exit();
+}//end if
+
+// Check for confirmation on doing the whole system ... it is going to take a bloody long time
+if ($rootnodeid == '1') {
+	$confirmation = NULL;
+	while ($confirmation != 'y' && $confirmation != 'n') {
+		echo "You have opted for running this script across the whole system. This may take a long time.\n";
+		echo 'Are you sure you want to continue? (Y/N) ';
+		$confirmation = rtrim(fgets(STDIN, 4094));
+		$confirmation = strtolower($confirmation);
+		if ($confirmation != 'y' && $confirmation != 'n') {
+			echo "Please answer Y or N\n";
+		} else if ($confirmation == 'n') {
+			exit();
+		}//end else
+	}//end while
+}//end if
+
+// ask for the root password for the system
+echo 'Enter the root password for "'.SQ_CONF_SYSTEM_NAME.'": ';
+$root_password = rtrim(fgets(STDIN, 4094));
+
+// check that the correct root password was entered
+$root_user =& $GLOBALS['SQ_SYSTEM']->am->getSystemAsset('root_user');
+if (!$root_user->comparePassword($root_password)) {
+	echo "ERROR: The root password entered was incorrect\n";
+	exit();
+}
+
+// log in as root
+if (!$GLOBALS['SQ_SYSTEM']->setCurrentUser($root_user)) {
+	trigger_error("Failed login in as root user\n", E_USER_ERROR);
+}
+
+// Connect to the DB
+$GLOBALS['SQ_SYSTEM']->changeDatabaseConnection('db2');
+$db =& $GLOBALS['SQ_SYSTEM']->db;
+
+///////////////////////////// START RESORTING ///////////////////////////////////////////
+// Start message
+echo 'Starting the re-sorting of "'.SQ_CONF_SYSTEM_NAME."\"\n";
+echo "Please wait...\n";
+
+$errors = Array();
+$stats = Array();
+
+$return_status = resort($rootnode->id, $errors, $stats);
+
+// End message
+echo 'Finished the re-sorting of "'.SQ_CONF_SYSTEM_NAME."\"\n";
+echo count($stats)." Assets haved been processed\n";
+if (!$return_status) {
+	echo count($errors)." Errors occured:\n";
+	if (empty($errors)) {
+		echo "No errors reported\n";
+	} else {
+		foreach ($errors as $error) {
+			echo 'ERROR: '.$error."\n";
+		}//end foreach
+	}//end else
+}//end if
+//////////////////////////// FINISH RESORTING //////////////////////////////////////////
+
+// Restore the Database connection
+$GLOBALS['SQ_SYSTEM']->restoreDatabaseConnection();
+
+// Finish
+
+
+/**
+* This function resorts the sort of the children under the root node supplied
+*
+* @param string	$node		The root node assetid to resort
+* @param array	&$errors	The error message holding array (by reference)
+* @param array	&$stats		The stats holding array (by reference)
+*
+* @return boolean
+* @access public
+*/
+function resort($node, &$errors, &$stats)
+{
+	// Globalise
+	global $db;
+
+	// TRUE by default until something goes wrong
+	$return_value = TRUE;
+
+	// We need each link type done separately
+	$link_types = Array(SQ_LINK_TYPE_1, SQ_LINK_TYPE_2, SQ_LINK_TYPE_3, SQ_LINK_NOTICE);
+
+	$children = get_children($node, $errors);
+
+	// If a valid asset do the sorting
+	$is_asset = $GLOBALS['SQ_SYSTEM']->am->assetExists($node);
+	if ($is_asset) {
+		foreach ($link_types as $link_type) {
+			$links_sql = 'SELECT linkid FROM sq_ast_lnk WHERE majorid = '.$db->quoteSmart($node).' AND link_type = '.$db->quoteSmart($link_type).' ORDER BY sort_order';
+			$links_result = $db->getCol($links_sql);
+			assert_valid_db_result($links_result);
+
+			foreach ($links_result as $i => $link) {
+				$GLOBALS['SQ_SYSTEM']->doTransaction('BEGIN');
+
+				$sql = 'UPDATE sq_ast_lnk SET sort_order = '.$db->quote($i).' WHERE linkid = '.$db->quote($link);
+				$result = $db->query($sql);
+				if (assert_valid_db_result($result)) {
+					$GLOBALS['SQ_SYSTEM']->doTransaction('COMMIT');
+				} else {
+					$message = 'Unable to the sort_order for Link ID #'.$link;
+					error_log($message);
+					$errors[] = $message;
+					$return_value = FALSE;
+					$GLOBALS['SQ_SYSTEM']->doTransaction('ROLLBACK');
+				}//end else
+
+				// Update the stats
+				if ($return_value) $stats[] = $node;
+			}//end foreach
+		}//end foreach
+
+	}//end if
+
+	// Now descend into this node's children if any
+	foreach ($children as $current_child) {
+		// If a valid asset do the sorting
+		$is_asset = $GLOBALS['SQ_SYSTEM']->am->assetExists($current_child);
+		if ($is_asset) {
+			$child_status = resort($current_child, $errors, $stats);
+			if (!$child_status) {
+				// Woo woo, I found an error, I better report it
+				$return_value = FALSE;
+			}//end if
+		}//end if
+	}//end foreach
+
+	// Report my status
+	return $return_value;
+
+}//end resort()
+
+
+/**
+* This function returns a list of the children under the supplied root node
+*
+* @param string	$node		The root node
+* @param array	&$errors	The error message holding array (by reference)
+*
+* @return array
+* @access public
+*/
+function get_children($node, &$errors)
+{
+	// Globalise
+	global $db;
+
+	// Empty by default until something goes into it
+	$children = Array();
+
+	$children_sql = 'SELECT minorid FROM sq_ast_lnk WHERE majorid='.$db->quoteSmart($node).' ORDER BY link_type';
+	$found_children	= $db->getCol($children_sql);
+	assert_valid_db_result($found_children);
+
+	// Sort the children into a format this script likes
+	foreach ($found_children as $found_child) {
+		$children[] = $found_child;
+	}//end foreach
+
+	// Return all the children (none if none found)
+	return $children;
+
+}//end get_children()
+
+
+?>
