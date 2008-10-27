f4b9dc4c3cb6e7507b84ecec31d03fc5c2474e15
Luke Wright <lwright@squiz.com.au>
2008-10-27 23:35:14 +0000

    Whoops, somehow changing the memory limit to 2047, rather than error_reporting, isn't going to help.

--- packages/bulkmail/run.php
+++ packages/bulkmail/run.php
@@ -11,7 +11,7 @@
 * | Module if you have the written consent of Squiz.                   |
 * +--------------------------------------------------------------------+
 *
-* $Id: run.php,v 1.5 2008/10/27 23:34:32 lwright Exp $
+* $Id: run.php,v 1.6 2008/10/27 23:35:14 lwright Exp $
 *
 */
 
@@ -22,12 +22,12 @@
 *
 * @author  Nathan de Vries <ndvries@squiz.net>
 * @author  Rayne Ong <rong@squiz.net>
-* @version $Revision: 1.5 $
+* @version $Revision: 1.6 $
 * @package MySource_Matrix
 */
 
 ini_set('memory_limit', -1);
-ini_set('memory_limit', E_ALL & (~E_STRICT));
+ini_set('error_reporting', E_ALL & (~E_STRICT));
 require_once dirname(__FILE__).'/bulk_mailer.inc';
 
 $bulk_mailer = new Bulk_Mailer();
