642e38917fb0152a383d7e9f62e3e85a9efd9b8f
Luke Wright <lwright@squiz.com.au>
2008-10-27 23:34:32 +0000

    Remove E_STRICT from error list

--- packages/bulkmail/run.php
+++ packages/bulkmail/run.php
@@ -11,7 +11,7 @@
 * | Module if you have the written consent of Squiz.                   |
 * +--------------------------------------------------------------------+
 *
-* $Id: run.php,v 1.4 2007/12/04 23:41:12 rong Exp $
+* $Id: run.php,v 1.5 2008/10/27 23:34:32 lwright Exp $
 *
 */
 
@@ -22,11 +22,12 @@
 *
 * @author  Nathan de Vries <ndvries@squiz.net>
 * @author  Rayne Ong <rong@squiz.net>
-* @version $Revision: 1.4 $
+* @version $Revision: 1.5 $
 * @package MySource_Matrix
 */
 
 ini_set('memory_limit', -1);
+ini_set('memory_limit', E_ALL & (~E_STRICT));
 require_once dirname(__FILE__).'/bulk_mailer.inc';
 
 $bulk_mailer = new Bulk_Mailer();
