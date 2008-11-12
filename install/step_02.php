1fb3f2b3f6c649b2c894df88607f0973f59c4f89
Mark Brydon <mbrydon@squiz.net>
2008-11-12 00:45:01 +0000

    Gracefully handle dropping of views in PostGres 8.1. Like Oracle, PostGres 8.1 does not support the 'IF EXISTS' clause. We get around this by expecting failure on the first 'DROP VIEW ... IF EXISTS' attempt and then if we're PostGres we'll have another go with 'DROP VIEW ...'. By this time there should be no permissions or roles views so creating the new views should be like a blissful metaphor. Step_02 has also been slightly altered to run the Roles modifications after other views have been committed. This was due to views not being acknowledged inside the transaction and causing the 'CREATE OR REPLACE VIEW...' statements to fail on PostGres 8.1

--- install/step_02.php
+++ install/step_02.php
@@ -10,7 +10,7 @@
 * | you a copy.                                                        |
 * +--------------------------------------------------------------------+
 *
-* $Id: step_02.php,v 1.71.2.1 2008/11/07 00:01:11 mbrydon Exp $
+* $Id: step_02.php,v 1.71.2.2 2008/11/12 00:45:01 mbrydon Exp $
 *
 */
 
@@ -20,7 +20,7 @@
 * Purpose
 *
 * @author  Greg Sherwood <greg@squiz.net>
-* @version $Revision: 1.71.2.1 $
+* @version $Revision: 1.71.2.2 $
 * @package MySource_Matrix
 * @subpackage install
 */
@@ -118,8 +118,13 @@ foreach ($packages as $package) {
 // Install all views except for Roles-related views which are handled further below
 install_stored_relations('views');
 
-// grant permissions to the tables for the secondary user
-grant_secondary_user_perms();
+$fv = $GLOBALS['SQ_SYSTEM']->getFileVersioning();
+
+if (!$fv->initRepository()) {
+	trigger_error('Unable to initialise File Versioning Repository', E_USER_ERROR);
+}
+
+$GLOBALS['SQ_SYSTEM']->doTransaction('COMMIT');
 
 /*
 * Verify that Roles views are all cool as determined by the system-wide config settings.
@@ -136,13 +141,9 @@ if ($roles_configured) {
 	echo "FAILED! Existing definition retained\n\n";
 }
 
-$fv = $GLOBALS['SQ_SYSTEM']->getFileVersioning();
+// grant permissions to the tables for the secondary user
+grant_secondary_user_perms();
 
-if (!$fv->initRepository()) {
-	trigger_error('Unable to initialise File Versioning Repository', E_USER_ERROR);
-}
-
-$GLOBALS['SQ_SYSTEM']->doTransaction('COMMIT');
 $GLOBALS['SQ_SYSTEM']->restoreDatabaseConnection();
 $GLOBALS['SQ_SYSTEM']->restoreRunLevel();
 
