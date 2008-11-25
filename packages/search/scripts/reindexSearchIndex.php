2c7db58ae5920e15c1b175fa29e967e22857a279
Mark Brydon <mbrydon@squiz.net>
2008-11-25 05:03:16 +0000

    Licence header changes and removal of SSV checking code in preparation for GPL release of Search in 3.20

--- packages/search/scripts/reindexSearchIndex.php
+++ packages/search/scripts/reindexSearchIndex.php
@@ -1,4 +1,18 @@
 <?php
+/**
+* +--------------------------------------------------------------------+
+* | This MySource Matrix CMS file is Copyright (c) Squiz Pty Ltd       |
+* | ACN 084 670 600                                                    |
+* +--------------------------------------------------------------------+
+* | IMPORTANT: Your use of this Software is subject to the terms of    |
+* | the Licence provided in the file licence.txt. If you cannot find   |
+* | this file please contact Squiz (www.squiz.net) so we may provide   |
+* | you a copy.                                                        |
+* +--------------------------------------------------------------------+
+*
+* $Id: reindexSearchIndex.php,v 1.2 2008/11/25 05:03:13 mbrydon Exp $
+*
+*/
 
 error_reporting(E_ALL);
 if ((php_sapi_name() != 'cli')) {
