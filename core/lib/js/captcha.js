f42655432c53f56752ac1c4894c651ded70dba02
Mark Brydon <mbrydon@squiz.net>
2009-03-16 05:59:23 +0000

    Added Accessible CAPTCHA Functionality. Also brought recent CAPTCHA modifications (colours, fonts, numbers etc.) into Account Manager.
    A new switch is provided in the "CAPTCHA" section of  the "Details" screen of Account Manager and Asset Builder assets and the
    "Contents" screen of Custom Form to enable an Accessible CAPTCHA. When enabled, a link will appear next to the CAPTCHA image, whether used
    in isolation or within a "%form_captcha%", to toggle between using the CAPTCHA image or an email address field.
    
    When clicked, the image CAPTCHA interface will be replaced with a text field and button, prompting for input of the user's email address.
    A verification email will be sent to the specified address once the "Submit Address" button is clicked. The user's session and the
    resultant email is populated with a key which is used to match up the intention to submit the form with the corresponding user.
    
    A link is provided in the email which when clicked will verify the key against that stored in the session and flag the form as verified
    for CAPTCHA purposes. Once verified, the same key cannot be used again (unless you are extremely lucky - notify us, then buy a lottery ticket).
    
    Once the form is submitted, the verification flag is cleared, requiring the user to start the Accessible CAPTCHA process again for subsequent forms
    or upon verification failure of fields in the same form - as is the case with the traditional CAPTCHA.

---
+++ core/lib/js/captcha.js
@@ -0,0 +1,121 @@
+/**
+* +--------------------------------------------------------------------+
+* | This MySource Matrix CMS file is Copyright (c) Squiz Pty Ltd       |
+* | ACN 084 670 600                                                    |
+* +--------------------------------------------------------------------+
+* | IMPORTANT: Your use of this Software is subject to the terms of    |
+* | the Licence provided in the file licence.txt. If you cannot find   |
+* | this file please contact Squiz (www.squiz.net) so we may provide   |
+* | you a copy.                                                        |
+* +--------------------------------------------------------------------+
+*
+* Accessible CAPTCHA Functions
+* @author Mark Brydon <mbrydon@squiz.net>
+*
+* $Id: captcha.js,v 1.2 2009/03/16 05:59:18 mbrydon Exp $
+*
+*/
+
+
+function show_accessible_captcha()
+{
+	// Would love these to be global however the CAPTCHA interface can be manually built, so we can
+	// only be guaranteed to have all of these elements once the link is clicked
+	var captcha_textbox = document.getElementById('SQ_SYSTEM_SECURITY_KEY_VALUE');
+	var captcha_image = document.getElementById('sq_security_key');
+	var captcha_accessible_link = document.getElementById('sq_accessible_validation_link');
+	var unreadable_captcha = document.getElementById('sq_regen_captcha');
+	var accessible_captcha_div = document.getElementById('sq_accessible_captcha');
+
+	captcha_textbox.style.display = 'none';
+	captcha_textbox.style.visibility = 'hidden';
+
+	captcha_image.style.display = 'none';
+	captcha_image.style.visibility = 'hidden';
+
+	// This field is optional - we expect the original CAPTCHA textbox and CAPTCHA image to be present so
+	// we're not too concerned about any JS errors above if these fields are not printed. But we care about this one...
+	if (typeof unreadable_captcha == 'undefined') {
+		unreadable_captcha.style.visibility = 'hidden';
+	}
+
+	accessible_captcha_div.style.display = 'inline';
+	accessible_captcha_div.style.visibility = 'visible';
+
+	captcha_accessible_link.innerHTML = '&nbsp;<a href="javascript:hide_accessible_captcha();">Use image validation</a>';
+
+	// Give focus to the input field just to be super nice
+	document.getElementById('SQ_SYSTEM_SECURITY_KEY_EMAIL').focus();
+}
+
+
+function hide_accessible_captcha()
+{
+	// Would love these to be global however the CAPTCHA interface can be manually built, so we can
+	// only be guaranteed to have all of these elements once the link is clicked
+	var captcha_textbox = document.getElementById('SQ_SYSTEM_SECURITY_KEY_VALUE');
+	var captcha_image = document.getElementById('sq_security_key');
+	var captcha_accessible_link = document.getElementById('sq_accessible_validation_link');
+	var unreadable_captcha = document.getElementById('sq_regen_captcha');
+	var accessible_captcha_div = document.getElementById('sq_accessible_captcha');
+
+	accessible_captcha_div.style.display = 'none';
+	accessible_captcha_div.style.visibility = 'hidden';
+
+	captcha_textbox.style.display = 'inline';
+	captcha_textbox.style.visibility = 'visible';
+
+	captcha_image.style.display = 'inline';
+	captcha_image.style.visibility = 'visible';
+
+	// This field is optional - we expect the original CAPTCHA textbox and CAPTCHA image to be present so
+	// we're not too concerned about any JS errors above if these fields are not printed. But we care about this one...
+	if (typeof unreadable_captcha == 'undefined') {
+		unreadable_captcha.style.visibility = 'visible';
+	}
+
+	captcha_accessible_link.innerHTML = '&nbsp;<a href="javascript:show_accessible_captcha();">Use accessible validation</a>';
+}
+
+
+// AJAX Callback
+function email_captcha_sent(responseText)
+{
+	var user_email_field = document.getElementById('SQ_SYSTEM_SECURITY_KEY_EMAIL');
+	var user_email_address = user_email_field.value;
+	var submit_button = document.getElementById('sq_submit_accessible_captcha');
+
+	user_email_field.disabled = 0;
+	submit_button.value = 'Email message sent';
+}
+
+
+function submit_email_captcha(lib_url)
+{
+	var user_email_field = document.getElementById('SQ_SYSTEM_SECURITY_KEY_EMAIL');
+	var user_email_address = user_email_field.value;
+
+
+	// Allow for a@b.zz
+	if (user_email_address.length > 5)
+	{
+		var submit_button = document.getElementById('sq_submit_accessible_captcha');
+
+		user_email_field.disabled = 1;
+		submit_button.disabled = 1;
+		submit_button.value = 'Sending email...';
+
+		// Send AJAX request
+		var params = "email="+user_email_address;
+		JsHttpConnector.submitRequest(lib_url+'/web/accessible_captcha.php?'+params, email_captcha_sent);
+	}
+}
+
+
+function enable_submission_button()
+{
+	var submit_button = document.getElementById('sq_submit_accessible_captcha');
+
+	submit_button.value = 'Submit address';
+	submit_button.disabled = 0;
+}
---
+++ core/lib/web/accessible_captcha.php
@@ -0,0 +1,156 @@
+<?php
+/**
+* +--------------------------------------------------------------------+
+* | This MySource Matrix CMS file is Copyright (c) Squiz Pty Ltd       |
+* | ACN 084 670 600                                                    |
+* +--------------------------------------------------------------------+
+* | IMPORTANT: Your use of this Software is subject to the terms of    |
+* | the Licence provided in the file licence.txt. If you cannot find   |
+* | this file please contact Squiz (www.squiz.net) so we may provide   |
+* | you a copy.                                                        |
+* +--------------------------------------------------------------------+
+*
+* Accessible CAPTCHA Functions
+* @author Mark Brydon <mbrydon@squiz.net>
+*
+* $Id: accessible_captcha.php,v 1.2 2009/03/16 05:59:19 mbrydon Exp $
+*
+*/
+
+
+require_once dirname(dirname(dirname(__FILE__))).'/include/init.inc';
+
+
+/**
+* Send a CAPTCHA verification email to the supplied address
+*
+* @param string	$to_email_address	The destination email address
+* @param string	$key				The shared CAPTCHA verification key (in the email and the user's session)
+*
+* @access public
+* @return void
+*/
+function sendAccessibleCaptchaEmail($to_email_address, $key)
+{
+	require_once 'Mail.php';
+	require_once 'Mail/mime.php';
+
+	// Strip spaces from around the "To" address in case these are present
+	$to_email_address = trim($to_email_address);
+
+	// Provide the name of the system as supplied in the main.inc configuration for use in the email (if it is set)
+	$from_address = '"Accessible CAPTCHA Form"';
+	if (SQ_CONF_SYSTEM_NAME != '') {
+		$from_system_name = 'from the '.SQ_CONF_SYSTEM_NAME.' website ';
+		$from_address = SQ_CONF_SYSTEM_NAME;
+	}
+
+	// Quote the System Name as it could contain apos'rophes
+	$from_address = '"'.$from_address.'"';
+	
+	$current_url = current_url();
+	$body = 'This email has been generated '.$from_system_name."as part of a form submission which includes an Accessible CAPTCHA field.\n\n".
+			"Please visit the following page to validate your submission before submitting the form\n\n".
+			$current_url.'?key='.$key;
+
+	$mime = new Mail_mime("\n");
+	$mime->setTXTBody($body);
+
+	$from_address .= ' <'.SQ_CONF_DEFAULT_EMAIL.'>';
+
+	$headers = Array(
+				'From'		=> $from_address,
+				'Subject'	=> 'Accessible CAPTCHA Form Verification',
+			   );
+
+	$param = Array(
+				'head_charset'  => SQ_CONF_DEFAULT_CHARACTER_SET,
+				'text_charset'  => SQ_CONF_DEFAULT_CHARACTER_SET,
+				'html_charset'  => SQ_CONF_DEFAULT_CHARACTER_SET,
+			 );
+	$body = @$mime->get($param);
+	$headers = @$mime->headers($headers);
+	$mail =& Mail::factory('mail');
+	$status = @$mail->send($to_email_address, $headers, $body);
+
+}//end sendAccessiblecaptchaEmail()
+
+
+/**
+* Verify the supplied CAPTCHA verification key against the one stored in the user's session
+* If we have a match, then flag it as a pass so submission of the underlying form can proceed unhindered
+*
+* @param string	$key				The shared CAPTCHA verification key (in the email and the user's session)
+*
+* @access public
+* @return void
+*/
+function validateAccessibleCaptcha($key)
+{
+	$verified_captcha = FALSE;
+
+	if ((isset($_SESSION['SQ_ACCESSIBLE_CAPTCHA_KEY'])) && ($_SESSION['SQ_ACCESSIBLE_CAPTCHA_KEY'] === $key)) {
+		// F.A.B - CAPTCHAs are go!
+		$verified_captcha = TRUE;
+		$_SESSION['SQ_ACCESSIBLE_CAPTCHA_PASSED'] = 1;
+
+		// Clear the CAPTCHA key from the session and access to run this script
+		unset($_SESSION['SQ_ACCESSIBLE_CAPTCHA_KEY']);
+		unset($_SESSION['SQ_ACCESSIBLE_CAPTCHA_GENERATED']);
+	}
+
+	if ($verified_captcha) {
+?>
+<p>Thank you for verifying the Accessible CAPTCHA input.<br />
+Please proceed to submit your form.</p>
+<?php
+	} else {
+?>
+<p>This Accessible CAPTCHA key has expired for this session.<br />
+Please return to the form to generate a new key.</p>
+<?php
+	}
+
+}//end validateAccessibleCaptcha()
+
+
+
+/**
+* The main bit
+*
+* GET requests:
+* 'email'	=> Send a CAPTCHA verification to the supplied email address
+* 'key'		=> Verify the supplied CAPTCHA key and flag the form as good
+*/
+
+// Ensure that the request originated from *this* Matrix system, otherwise drop the request like some sort of heavy feather
+if (!isset($_SESSION['SQ_ACCESSIBLE_CAPTCHA_GENERATED'])) exit;
+
+// Generate an Accessible CAPTCHA Key
+if (isset($_GET['email'])) {
+
+	$user_email = addslashes($_GET['email']);
+
+	// Ensure that we have *one* valid email address
+	if ((strpos($user_email, ',') === FALSE) || (strpos($user_email, ';') === FALSE)) {
+		// Return a key to be used in an email message to clear this CAPTCHA hurdle
+		// The trinity of email address, timestamp, user ID and a locally-generated integer should be unique enough to generate a robust key
+		$local_megadice = rand(1, 1000000);
+
+		$submission_time = time();
+		$key = md5($user_email.$submission_time.$local_megadice);
+
+		// Log our generated key in the user's session and send a nice email to the user for CAPTCHA validation
+		$_SESSION['SQ_ACCESSIBLE_CAPTCHA_KEY'] = $key;
+		sendAccessibleCaptchaEmail($user_email, $key);
+	}
+
+} else if (isset($_GET['key'])) {
+// Verify an Accessible CAPTCHA Key from an email message
+
+	$key = addslashes($_GET['key']);
+	validateAccessibleCaptcha($key);
+
+}
+
+?>
