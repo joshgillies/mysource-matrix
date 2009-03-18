a15a2424a685f788246760da8da040d5e2e0ca87
Benjamin Pearson <bpearson@squiz.com.au>
2009-03-18 03:22:19 +0000

    Bug Fix #3560 - Previous bug fix was fine for single option lists on a page, but not multiple. So reverting that change and found a better solution which doesn't break it

--- core/lib/js/edit.js
+++ core/lib/js/edit.js
@@ -9,7 +9,7 @@
 * | you a copy.                                                        |
 * +--------------------------------------------------------------------+
 *
-* $Id: edit.js,v 1.51.2.2 2009/03/17 05:56:11 bpearson Exp $
+* $Id: edit.js,v 1.51.2.3 2009/03/18 03:20:34 bpearson Exp $
 *
 */
 
@@ -280,7 +280,6 @@ function expandOptionList(input)
 	newInput.onfocus = expandListFn;
 	newInput.value = '';
 	newInput.id = optionItemPrefix+'_options['+inputs.length+']';
-	newInput.name = optionItemPrefix+'_options['+inputs.length+']';
 	input.parentNode.appendChild(newInput);
 	var delButton = input.nextSibling;
 	while (delButton.tagName != 'BUTTON') {
