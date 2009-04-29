013dc556f574a63455484cfd87d44f8ec3db9da3
Huan Nguyen <hnguyen@squiz.net>
2009-04-29 04:04:07 +0000

    added function doc

--- packages/web_services/api/soap_api_design_lookup_service/design_lookup_service_requests.js
+++ packages/web_services/api/soap_api_design_lookup_service/design_lookup_service_requests.js
@@ -5,8 +5,22 @@
 
 
 /**
- * This operation will apply a design to an asset
- */
+* Description: This operation will apply a design to an asset
+*
+* @param array  $request	The request information
+* <pre>
+* Array (
+*		 'DesignID'	 				=> [The Design ID],
+*		 'AssetID'			 		=> [The AssetID of the asset being applied the design],
+*		 'DesignType'				=> [Type of Design: Frontend, OverrideFrontend, Login, OverrideLogin, UserDefined, OverrideUserDefined],
+*        'UserDefinedDesignName'    => [Name of the new User Defined design],
+
+*        )
+* </pre>
+*
+* @return string
+* @access public
+*/
 function ApplyDesign(designid, assetid, design_type, user_defined_design_name)
 {
 	var soapBody	= "\
@@ -16,13 +30,29 @@ function ApplyDesign(designid, assetid, design_type, user_defined_design_name)
 <DesignType>"+design_type+"</DesignType>\
 <UserDefinedDesignName>"+user_defined_design_name+"</UserDefinedDesignName>\
 </ns1:ApplyDesign>";
+	
 	return soapBody;
+	
 }//end ApplyDesign
 
 
 /**
- * This operation will remove a design to an asset
- */
+* Description: This operation will remove a design to an asset
+*
+* @param array  $request	The request information
+* <pre>
+* Array (
+*		 'DesignID'	 				=> [The Design ID],
+*		 'AssetID'			 		=> [The AssetID of the asset being applied the design],
+*		 'DesignType'				=> [Type of Design: Frontend, OverrideFrontend, Login, OverrideLogin, UserDefined, OverrideUserDefined],
+*        'UserDefinedDesignName'    => [Name of the new User Defined design],
+
+*        )
+* </pre>
+*
+* @return void
+* @access public
+*/
 function RemoveDesign(designid, assetid, design_type, user_defined_design_name)
 {
 	var soapBody	= "\
@@ -32,13 +62,27 @@ function RemoveDesign(designid, assetid, design_type, user_defined_design_name)
 <DesignType>"+design_type+"</DesignType>\
 <UserDefinedDesignName>"+user_defined_design_name+"</UserDefinedDesignName>\
 </ns1:RemoveDesign>";
-	return soapBody;	
+	
+	return soapBody;
+
 }//end RemoveDesign
 
 
 /**
- * This operation returns the effective design of an URL
- */
+* Description: This operation returns the effective design of an URL
+*
+* @param array $asset_info the info array to find children of a specific asset
+* <pre>
+* Array (
+*		'URL'					=> [The URL in question],
+*		'DesignType'			=> [Type of Design: Frontend, OverrideFrontend, Login, OverrideLogin, UserDefined, OverrideUserDefined],
+*		'UserDefinedDesignName'	=> [Name of the new User Defined design],
+*        )
+* </pre>
+*
+* @return void
+* @access public
+*/
 function GetDesignFromURL(url, design_type, user_defined_design_name)
 {
 	var soapBody	= "\
@@ -47,13 +91,27 @@ function GetDesignFromURL(url, design_type, user_defined_design_name)
 <DesignType>"+design_type+"</DesignType>\
 <UserDefinedDesignName>"+user_defined_design_name+"</UserDefinedDesignName>\
 </ns1:GetDesignFromURL>";
-	return soapBody;	
+	
+	return soapBody;
+
 }//end GetDesignFromURL
 
 
 /**
- * This operation will apply a paintlayout to an asset
- */
+* Description: This operation will apply a paintlayout to an asset
+*
+* @param array $request		the request information
+* <pre>
+* Array (
+*		'PaintLayoutID'		=> [The assetid of the paintlayout],
+*		'AssetID'			=> [The asset being applied paintlayout to],
+*		'PaintLayoutType'	=> [Type of paint layout Frontend or OverrideFrontend],
+*        )
+* </pre>
+*
+* @return void
+* @access public
+*/
 function ApplyAssetPaintLayout(paint_layout_id, assetid, paint_layout_type)
 {
 	var soapBody	= "\
@@ -62,13 +120,27 @@ function ApplyAssetPaintLayout(paint_layout_id, assetid, paint_layout_type)
 <AssetID>"+assetid+"</AssetID>\
 <PaintLayoutType>"+paint_layout_type+"</PaintLayoutType>\
 </ns1:ApplyAssetPaintLayout>";
-	return soapBody;	
+	
+	return soapBody;
+
 }//end ApplyAssetPaintLayout
 
 
 /**
- * This operation will remove a paintlayout to an asset
- */
+* Description: This operation will remove a paintlayout to an asset
+*
+* @param array $request		the request information
+* <pre>
+* Array (
+*		'PaintLayoutID'		=> [The assetid of the paintlayout],
+*		'AssetID'			=> [The asset being remove paintlayout from],
+*		'PaintLayoutType'	=> [Type of paint layout Frontend or OverrideFrontend],
+*        )
+* </pre>
+*
+* @return void
+* @access public
+*/
 function RemoveAssetPaintLayout(paint_layout_id, assetid, paint_layout_type)
 {
 	var soapBody	= "\
@@ -77,5 +149,7 @@ function RemoveAssetPaintLayout(paint_layout_id, assetid, paint_layout_type)
 <AssetID>"+assetid+"</AssetID>\
 <PaintLayoutType>"+paint_layout_type+"</PaintLayoutType>\
 </ns1:RemoveAssetPaintLayout>";
-	return soapBody;	
+	
+	return soapBody;
+
 }//end RemoveAssetPaintLayout
