8f5aa5633b30d1181f32e8f49d8822d9e929645d
Huan Nguyen <hnguyen@squiz.net>
2009-04-29 06:10:05 +0000

    added function doc

--- packages/web_services/api/soap_api_permission_role_service/permission_role_service_requests.js
+++ packages/web_services/api/soap_api_permission_role_service/permission_role_service_requests.js
@@ -5,8 +5,19 @@
 
 
 /**
- * This operation will check whether the requesting user have a certain access level to a particular asset
- */
+* Description: This operation will check whether the requesting user have a certain access level to a particular asset
+*
+* @param array  $request	The request information
+* <pre>
+* Array (
+*		 'AssetID'	 		=> [type code of new asset],
+*		 'PermissionLevel'	=> [name for new asset],
+*        )
+* </pre>
+*
+* @return void
+* @access public
+*/
 function HasAccess(assetid, permission_level)
 {
 	var soapBody	= "\
@@ -14,13 +25,29 @@ function HasAccess(assetid, permission_level)
 <AssetID>"+assetid+"</AssetID>\
 <PermissionLevel>"+permission_level+"</PermissionLevel>\
 </ns1:HasAccess>";
+
 	return soapBody;
+
 }//end HasAccess
 
 
 /**
- * This operation will set the permission for a user on an asset
- */
+* Description: This operation will set the permission for a user on an asset
+*
+* @param array $asset_info the info array to find children of a specific asset
+* <pre>
+* Array (
+*		'AssetID'			=> [The asset in query],
+*		'UserID'			=> [The user which the permission is applying for],
+*		'PermissionLevel'	=> [Read Write Admin],
+*		'Granted'			=> [Apply, Deny, Revoke],
+*		'Cascade'			=> [Whether to cascade permission down to children],
+*        )
+* </pre>
+*
+* @return void
+* @access public
+*/
 function SetPermission(assetid, userid, permission_level, cascade)
 {
 	var soapBody	= "\
@@ -30,13 +57,30 @@ function SetPermission(assetid, userid, permission_level, cascade)
 <PermissionLevel>"+permission_level+"</PermissionLevel>\
 <Cascade>"+cascade+"</Cascade>\
 </ns1:SetPermission>";
+
 	return soapBody;
+
 }//end SetPermission
 
 
 /**
- * This operation will return the permission set for on an asset
- */
+* Description: This operation will return the permission set for on an asset
+*
+* @param array $asset_info the info array to find children of a specific asset
+* <pre>
+* Array (
+*		'AssetID'			=> [The asset in query],
+*		'PermissionLevel'	=> [Read Write Admin],
+*		'Granted'			=> [Apply, Deny, Revoke],
+*		'Effective'			=> [Check for effective permission (write = read + write)],
+*		'ExpandGroups'		=> [Return userids inside a group instead just groupid],
+*		'AllInfo'			=> [Apply, Deny, Revoke],
+*        )
+* </pre>
+*
+* @return void
+* @access public
+*/
 function GetPermission(assetid, permission_level, granted, effective, expand_groups, all_info)
 {
 	var soapBody	= "\
@@ -48,13 +92,31 @@ function GetPermission(assetid, permission_level, granted, effective, expand_gro
 <ExpandGroups>"+expand_groups+"</ExpandGroups>\
 <AllInfo>"+all_info+"</AllInfo>\
 </ns1:GetPermission>";
+
 	return soapBody;
+
 }//end GetPermission
 
 
 /**
- * This operation will return an array of roles and the users/groups which can perform it
- */
+* Description: This operation will return an array of roles and the users/groups which can perform it
+*
+* @param array $asset_info the info array to find children of a specific asset
+* <pre>
+* Array (
+*		'AssetID'				=> [The assetid of the asset the role is applied to],
+*		'RoleID'				=> [The assetid of the the role that is applied],
+*		'UserID'				=> [The assetid of the user performing the role],
+*		'IncludeAssetID'		=> [Whether to include the assetid in the returned array],
+*		'IncludeGlobals'		=> [Whether to query the role view which includes expanded global roles as individual users],
+*		'IncludeDependants'		=> [If false it will filter out the dependant assets],
+*		'ExpandGroups'			=> [when TRUE, any groups defined within a role will be replaced with the userids in that group]
+*        )
+* </pre>
+*
+* @return void
+* @access public
+*/
 function GetRole(assetid, roleid, userid, include_assetid, include_globals, include_dependants, expand_groups)
 {
 	var soapBody	= "\
@@ -67,13 +129,30 @@ function GetRole(assetid, roleid, userid, include_assetid, include_globals, incl
 <IncludeDependants>"+include_dependants+"</IncludeDependants>\
 <ExpandGroups>"+expand_groups+"</ExpandGroups>\
 </ns1:GetRole>";
+
 	return soapBody;
+
 }//end GetRole
 
 
 /**
- * This operation will set role for an user to perform on an asset.
- */
+* Description: This operation will set role for an user to perform on an asset.
+*
+* @param array $asset_info the info array to find children of a specific asset
+* <pre>
+* Array (
+*		'AssetID'				=> [The assetid of the asset the role is applied to],
+*		'RoleID'				=> [The assetid of the the role that is applied],
+*		'UserID'				=> [The assetid of the user performing the role],
+*		'Action'				=> [Add | Delete Role],
+*		'GlobalRole'			=> [Global or non-global role],
+*		'Cascade'				=> [Whether to cascade the role down to children],
+*        )
+* </pre>
+*
+* @return void
+* @access public
+*/
 function SetRole(assetid, roleid, userid, action, global_role, cascade)
 {
 	var soapBody	= "\
@@ -85,7 +164,9 @@ function SetRole(assetid, roleid, userid, action, global_role, cascade)
 <GlobalRole>"+global_role+"</GlobalRole>\
 <Cascade>"+cascade+"</Cascade>\
 </ns1:SetRole>";
+
 	return soapBody;
+
 }//end SetRole
 
 
