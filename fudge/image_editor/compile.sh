8f3dc34fcd5f067efd6479cfc6d08accb4937a9e
Ashish Karelia <akarelia@squiz.com.au>
2009-06-17 23:43:31 +0000

    added the correct path to point at java, also added command to sign and verify the newly created jar file.

--- fudge/image_editor/compile.sh
+++ fudge/image_editor/compile.sh
@@ -1,4 +1,4 @@
-export CLASSPATH=/opt/blackdown-jdk-1.4.1/jre/lib/rt.jar:.:.
+export CLASSPATH=/opt/java-1.4/jre/lib/rt.jar:.:.
 cd src
 javac  ij/*.java ij/*/*.java ij/*/*/*.java
 jar cf imagej.jar ij/*.* ij/*/*.* ij/*/*/*.* IJ_Props.txt
@@ -8,3 +8,5 @@ do
   rm $i
 done
 cd ..
+jarsigner -keystore SquizKeyStore imagej.jar mykey
+jarsigner -verify imagej.jar
