dc916130657b6bd24893ae9bcffeac2d7d36607f
Christopher Smith <csmith@squiz.com.au>
2009-07-09 03:56:07 +0000

    fix bad logic in session cleanup. use_default_session_save_path refers to the default php session path, not default matrix session path

--- scripts/session_cleanup.sh
+++ scripts/session_cleanup.sh
@@ -10,7 +10,7 @@
 #* | you a copy.                                                        |
 #* +--------------------------------------------------------------------+
 #*
-#* $Id: session_cleanup.sh,v 1.7 2009/05/21 03:32:23 csmith Exp $
+#* $Id: session_cleanup.sh,v 1.8 2009/07/09 03:56:07 csmith Exp $
 #*
 #*/
 
@@ -158,8 +158,6 @@ require_once '${SYSTEM_ROOT}/data/private/conf/main.inc';
 echo \$var . '=\"' . (int)SQ_CONF_USE_DEFAULT_SESSION_SAVE_PATH . '\";';
 echo 'export ' . \$var . ';';
 
-\$session_path = session_save_path();
-
 \$var = 'SESSION_TYPE';
 \$handler = strtolower(SQ_CONF_SESSION_HANDLER);
 if (\$handler != '' && \$handler != 'default') {
@@ -171,11 +169,8 @@ echo 'export ' . \$var . ';';
 
 \$var = 'SESSION_LOCATION';
 
-if (SQ_CONF_USE_DEFAULT_SESSION_SAVE_PATH == true || SQ_CONF_CUSTOM_SESSION_SAVE_PATH === '') {
-	# if no save path is set, use the cache dir.
-	if (\$session_path === '') {
-		\$session_path = '${SYSTEM_ROOT}/cache';
-	}
+if (SQ_CONF_USE_DEFAULT_SESSION_SAVE_PATH == false || SQ_CONF_CUSTOM_SESSION_SAVE_PATH === '') {
+	\$session_path = '${SYSTEM_ROOT}/cache';
 } else {
 	\$session_path = SQ_CONF_CUSTOM_SESSION_SAVE_PATH;
 }
