831a531600e53eceff341917943850c4f98f1c18
Edison Wang <xwan510@gmail.com>
2009-07-29 00:27:00 +0000

    Bug fix #3805 -- Removed previous workaround code for oracle + fork = lost connection issue. Decent fix is applied. It also fixes #3805 which is caused by the previous workaround.

--- scripts/system_update_lookups.php
+++ scripts/system_update_lookups.php
@@ -10,7 +10,7 @@
 * | you a copy.                                                        |
 * +--------------------------------------------------------------------+
 *
-* $Id: system_update_lookups.php,v 1.7 2008/12/16 22:04:35 bshkara Exp $
+* $Id: system_update_lookups.php,v 1.7.2.1 2009/07/29 00:26:31 ewang Exp $
 *
 */
 
@@ -18,7 +18,7 @@
 * Run updateLookups() on each site-based asset in the system
 *
 * @author  Blair Robertson <brobertson@squiz.co.uk>
-* @version $Revision: 1.7 $
+* @version $Revision: 1.7.2.1 $
 * @package MySource_Matrix
 */
 error_reporting(E_ALL);
@@ -38,6 +38,21 @@ require_once $SYSTEM_ROOT.'/core/include/init.inc';
 echo 'Enter the root password for "'.SQ_CONF_SYSTEM_NAME.'": ';
 $root_password = rtrim(fgets(STDIN, 4094));
 
+//set to oci commit on success mode to avoid oracle + folk = lost connection issue
+if (MatrixDAL::getDbType() === 'oci') {
+	  MatrixDAL::setOciCommitOnSuccess(TRUE);
+}
+
+$root_user = $GLOBALS['SQ_SYSTEM']->am->getSystemAsset('root_user');
+if (!$root_user->comparePassword($root_password)) {
+	trigger_error("The root password entered was incorrect\n", E_USER_ERROR);
+	exit;
+}
+// log in as root
+if (!$GLOBALS['SQ_SYSTEM']->setCurrentUser($root_user)) {
+	trigger_error("Failed logging in as root user\n", E_USER_ERROR);
+}
+
 $am = $GLOBALS['SQ_SYSTEM']->am;
 $hh = $GLOBALS['SQ_SYSTEM']->getHipoHerder();
 $site_ids = $am->getTypeAssetids('site', FALSE, TRUE);
@@ -46,8 +61,6 @@ foreach ($site_ids as $assetid => $info) {
 	$sites[] = $am->getAsset($assetid, $info['type_code']);
 }
 
-// disconnect completely to avoid Oracle end-of-file errors when forking
-_disconnectDB();
 
 foreach ($sites as $key => $site) {
 
@@ -56,31 +69,6 @@ foreach ($sites as $key => $site) {
 	// only run this if we are in the forked process
 	if (!$pid) {
 		_reconnectDB();
-
-			$root_user = $GLOBALS['SQ_SYSTEM']->am->getSystemAsset('root_user');
-
-			// This ridiculousness allows us to workaround Oracle, forking and CLOBs
-			// if a query is executed that returns more than 1 LOB before a fork occurs,
-			// the Oracle DB connection will be lost inside the fork.
-			// In this case, because a user asset has more than 1 attribute and custom_val in sq_ast_attr_val
-			// is of type CLOB, we attempt to check the root password inside our forked process.
-
-			// Check that the correct root password was entered.
-			// Yes this is checked for each site because even if this individual forked process is killed
-			// the parent process still runs and continues to fork more processes.
-			if (!$root_user->comparePassword($root_password)) {
-				// only show the error once
-				if ($key === 0) {
-					trigger_error("The root password entered was incorrect\n", E_USER_ERROR);
-				}
-				exit;
-			}
-
-			// log in as root
-			if (!$GLOBALS['SQ_SYSTEM']->setCurrentUser($root_user)) {
-				trigger_error("Failed logging in as root user\n", E_USER_ERROR);
-			}
-
 			printName('Updating Lookups for "'.$site->name.'" #'.$site->id);
 
 			$vars = Array('assetids' => Array($site->id));
@@ -96,6 +84,9 @@ foreach ($sites as $key => $site) {
 		// exit the child fork process and return to the parent where it's still pcntl_wait()ing.
 		exit;
 	}
+}
+if (MatrixDAL::getDbType() === 'oci') {
+	  MatrixDAL::setOciCommitOnSuccess(FALSE);
 }
 
 exit;
@@ -170,7 +161,8 @@ function fork()
 */
 function _reconnectDB()
 {
-	$GLOBALS['SQ_SYSTEM']->changeDatabaseConnection('db2');
+	$GLOBALS['SQ_SYSTEM']->restoreDatabaseConnection(TRUE);
+	$GLOBALS['SQ_SYSTEM']->changeDatabaseConnection('db2', TRUE);
 
 }//end _reconnectDB()
 
