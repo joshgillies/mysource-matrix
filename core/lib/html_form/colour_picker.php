ad91a453f40f81bee0bf95615ea75ef2dc954a72
Benjamin Pearson <bpearson@squiz.com.au>
2009-10-07 22:10:39 +0000

    Reverting fix - htmlentities doesn't need encoding here

--- core/lib/html_form/colour_picker.php
+++ core/lib/html_form/colour_picker.php
@@ -10,7 +10,7 @@
 * | you a copy.                                                        |
 * +--------------------------------------------------------------------+
 *
-* $Id: colour_picker.php,v 1.11 2009/10/07 04:37:06 bpearson Exp $
+* $Id: colour_picker.php,v 1.12 2009/10/07 22:10:39 bpearson Exp $
 *
 */
 
@@ -18,13 +18,13 @@
 * Pop-Up for the colour picker
 *
 * @author  Greg Sherwood <gsherwood@squiz.net>
-* @version $Revision: 1.11 $
+* @version $Revision: 1.12 $
 * @package MySource_Matrix
 */
 
 // Sanitise data
-$colour = htmlentities($_GET['colour'], ENT_COMPAT, SQ_CONF_DEFAULT_CHARACTER_SET);
-$pickerid = htmlentities($_GET['pickerid'], ENT_COMPAT, SQ_CONF_DEFAULT_CHARACTER_SET);
+$colour = htmlentities($_GET['colour']);
+$pickerid = htmlentities($_GET['pickerid']);
 ?>
 <!-- note: this version of the color picker is optimized for IE 5.5+ only -->
 
