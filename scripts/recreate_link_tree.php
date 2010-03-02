b5cb786fc74ca542f6714cf105a7018316397493
Chiranjivi Upreti <cupreti@squiz.com.au>
2010-03-02 05:03:33 +0000

    Re-fix to bug #4169 - recreate_link_tree script not working in postgres. In select query to get links from link table, ORDER BY clause was using sort_order column which couldn't uniquely  to identify records, hence chucking the SELECT using LIMIT and OFFSET was not returing the unique chucks as expected but the with few duplicates. For fix specify 'sort_order' and the unique column 'linkid' (along with 'majorid', 'minorid', just to make sure) while selecting the records from link table so that the chucks obtianed are all unique.

--- scripts/recreate_link_tree.php
+++ scripts/recreate_link_tree.php
@@ -10,7 +10,7 @@
 * | you a copy.                                                        |
 * +--------------------------------------------------------------------+
 *
-* $Id: recreate_link_tree.php,v 1.26 2010/02/26 04:19:11 cupreti Exp $
+* $Id: recreate_link_tree.php,v 1.27 2010/03/02 05:03:33 cupreti Exp $
 *
 */
 
@@ -28,7 +28,7 @@
 * @author  Blair Robertson <blair@squiz.net>
 * @author  Luke Wright <lwright@squiz.net>
 * @author  Avi Miller <avi.miller@squiz.net>
-* @version $Revision: 1.26 $
+* @version $Revision: 1.27 $
 * @package MySource_Matrix
 */
 error_reporting(E_ALL);
@@ -83,7 +83,8 @@ $base_sql = 'SELECT l.majorid, l.linkid, l.minorid
 			sq_ast_lnk l
 		WHERE
 			'.db_extras_bitand(MatrixDAL::getDbType(), 'l.link_type', MatrixDAL::quote(SQ_SC_LINK_SIGNIFICANT)).' > 0
-		';
+		ORDER BY
+			l.sort_order, l.linkid, l.majorid, l.minorid';
 
 $offset = 0;
 $chunk_size = 2000;
