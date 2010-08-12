f0deb4539b5e7a88a07025d5e42b46e00b8e5414
Edison Wang <xwan510@gmail.com>
2010-08-12 00:43:40 +0000

    Added extra md5 sum check for the old squiz logo. Some string edits to make it more friendly. This script has to be run as apache user, since it updates the data folder in which apache user has full read/write permission.

--- install/update_squiz_logo_login_design.php
+++ install/update_squiz_logo_login_design.php
@@ -10,7 +10,7 @@
 * | you a copy.                                                        |
 * +--------------------------------------------------------------------+
 *
-* $Id: update_squiz_logo_login_design.php,v 1.2 2010/08/11 05:12:51 ewang Exp $
+* $Id: update_squiz_logo_login_design.php,v 1.3 2010/08/12 00:43:40 ewang Exp $
 *
 */
 
@@ -65,7 +65,7 @@ require_once SQ_FUDGE_PATH.'/general/file_system.inc';
 echo 'Updating new Squiz Logo for login design, password change design, EES login design ...'."\n";
 
 do {
-	echo "Note: If the Squiz Logo image have been customized/modified in above login designs, this script will overwrite the change. Proceed? (Yes/No) : ";
+	echo "Note: If the Logo image have been modified in above designs, this script will skip it. Proceed? (Yes/No) : ";
 	$answer = rtrim(fgets(STDIN, 4094));
 	if ($answer == 'Yes') {
 		break;
@@ -88,31 +88,83 @@ foreach ($children as $id => $content) {
 	}
 	switch ($content[0]['type_code']) {
 		case 'login_design':
-			$file = 'login_image.gif';
-			$path = $SYSTEM_ROOT.'/core/assets/system/login_design/design_files/files/'.$file;
+			$file_name = 'login_image.gif';
+			$path = $SYSTEM_ROOT.'/core/assets/system/login_design/design_files/files/'.$file_name;
+			$md5 = '99029df0982274ce2f1c3404b6902f0f';
 			break;
 		case 'password_change_design':
-			$file = 'login_image.gif';
-			$path = $SYSTEM_ROOT.'/core/assets/system/password_change_design/design_files/files/'.$file;
+			$file_name = 'login_image.gif';
+			$path = $SYSTEM_ROOT.'/core/assets/system/password_change_design/design_files/files/'.$file_name;
+			$md5 = '99029df0982274ce2f1c3404b6902f0f';
 			break;
 		case 'ees_login_design':
-			$file = 'matrix-logo.png';
-			$path = $SYSTEM_ROOT.'/core/assets/system/ees_login_design/design_files/files/'.$file;
+			$file_name = 'matrix-logo.png';
+			$path = $SYSTEM_ROOT.'/core/assets/system/ees_login_design/design_files/files/'.$file_name;
+			$md5 = '9ade1af14bab1611aef70b91f1401d3e';
 			break;
 	}
 
 	$design = $GLOBALS['SQ_SYSTEM']->am->getAsset($id);
-	$ef = $design->getEditFns();
 	$info = Array();
-	$info['name'] = $file;
+	$info['name'] = $file_name;
 	$info['tmp_name'] = $path;
 	$info['non_uploaded_file'] = TRUE;
-	if (!$ef->_processUploadedFile($design, $info)) {
-		trigger_localised_error('CORE0030', E_USER_WARNING, $file);
+
+	$existing_ids = Array();
+	$existing = $GLOBALS['SQ_SYSTEM']->am->getLinks($design->id, SQ_LINK_TYPE_2, 'file', FALSE);
+
+	foreach ($existing as $link) {
+		$existing_ids[$link['minorid']] = $link['linkid'];
 	}
+
+	$existing_info = $GLOBALS['SQ_SYSTEM']->am->getAssetInfo(array_keys($existing_ids));
+
+	// must web-path-ify the new file name to compare, as this is eventually
+	// it's what the file will be named
+	$file_name = make_valid_web_paths(Array($info['name']));
+	$file_name = array_shift($file_name);
+
+	$existing_fileid = 0;
+	foreach ($existing_info as $asset_id => $asset_info) {
+		// if the name is the same, delete the link
+		if ($asset_info['name'] == $file_name) {
+			$existing_fileid = $asset_id;
+			break;
+		}
+	}
+	
+	if(empty($existing_fileid)) {
+		trigger_error('Can not find original logo image');
+		exit();
+	}
+	
+	// we already have a design file with the same name, so just upload over the top of it
+	$file = $GLOBALS['SQ_SYSTEM']->am->getAsset($existing_fileid);
+
+	// check MD5 hash, make sure it is the file we want to update
+	$file_info = $file->getExistingFile();
+	if(md5_file($file_info['path']) !== $md5) {
+		trigger_error('MD5 hash does not match, '.$file_name.' has been previously modified. Skip...');
+		continue;
+	}
+	
+	$lock_status = $GLOBALS['SQ_SYSTEM']->am->acquireLock($file->id, 'attributes');
+	$edit_fns = $file->getEditFns();
+	$o = NULL;
+	$success = $edit_fns->processFileUpload($file, $o, $file->getPrefix(), $info);
+
+	if ($lock_status === 1) {
+		$GLOBALS['SQ_SYSTEM']->am->releaseLock($file->id, 'attributes');
+	}
+
+	if(!$success) {
+		trigger_error('Failed to update squiz logo image');
+		exit();
+	}
+	echo $file_name.' in '.$content[0]['type_code'].' has been successfully updated'."\n";
 }
 
-echo 'Squiz logo has been successfully updated'."\n";
+echo 'Done'."\n";
 
 $GLOBALS['SQ_SYSTEM']->restoreRunLevel();
 
