db3778ebcfced4feb33331aac64326f80275b919
Chiranjivi Upreti <cupreti@squiz.com.au>
2011-02-21 04:35:59 +0000

    Simple edit user should be allowed to use this popup

--- fudge/wysiwyg/plugins/matrix_insert_image/upload_image.php
+++ fudge/wysiwyg/plugins/matrix_insert_image/upload_image.php
@@ -10,7 +10,7 @@
 * | you a copy.                                                        |
 * +--------------------------------------------------------------------+
 *
-* $Id: upload_image.php,v 1.3 2011/02/18 03:17:35 cupreti Exp $
+* $Id: upload_image.php,v 1.4 2011/02/21 04:35:34 cupreti Exp $
 *
 */
 
@@ -18,14 +18,14 @@
 * Upload Image Popup for the WYSIWYG
 *
 * @author  Benjamin Pearson <bpearson@squiz.net>
-* @version $Revision: 1.3 $
+* @version $Revision: 1.4 $
 * @package MySource_Matrix
 */
 
 require_once dirname(__FILE__).'/../../../../core/include/init.inc';
 require_once dirname(__FILE__).'/../../../../core/assets/files/image/image.inc';
 
-if (empty($GLOBALS['SQ_SYSTEM']->user) || !$GLOBALS['SQ_SYSTEM']->user->canAccessBackend()) {
+if (empty($GLOBALS['SQ_SYSTEM']->user) || !($GLOBALS['SQ_SYSTEM']->user->canAccessBackend() || $GLOBALS['SQ_SYSTEM']->user->type() == 'simple_edit_user')) {
 	echo return_javascript_error('You cannot upload file as a non-backend user');
 	exit;
 }
