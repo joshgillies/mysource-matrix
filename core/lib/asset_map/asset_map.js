2f7923e48e5b78619c794e8edce153f0bd139626
Mohamed Haidar <mhaidar@squiz.com.au>
2011-05-03 02:17:50 +0000

    Bug #5064: HIPO popup windows lose focus in IE. Popup window does not get focus at any stage, focus remains with parent window. Using window.focus() before window.open() is enough to fix this problem but I am unsure why it works. Could be something to do with iframes. Additional popup.focus() added to assure we get focus.

--- core/lib/asset_map/asset_map.js
+++ core/lib/asset_map/asset_map.js
@@ -9,7 +9,7 @@
 * | you a copy.                                                        |
 * +--------------------------------------------------------------------+
 *
-* $Id: asset_map.js,v 1.25 2008/01/09 23:57:59 colivar Exp $
+* $Id: asset_map.js,v 1.25.14.1 2011/05/03 02:17:50 mhaidar Exp $
 *
 */
 
@@ -62,7 +62,9 @@ function init_asset_map()
 */
 function open_hipo(url)
 {
-	window.open(url, 'hipo_job', 'width=650,height=400,scrollbars=1,toolbar=0,menubar=0,location=0,resizable=1');
+	window.focus();
+	var popup = window.open(url, 'hipo_job', 'width=650,height=400,scrollbars=1,toolbar=0,menubar=0,location=0,resizable=1');
+	popup.focus();
 
 }//end open_hipo()
 
