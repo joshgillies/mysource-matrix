151c143f9975428c2e135d807bbb42a01a27a68d
Mohamed Haidar <mhaidar@squiz.com.au>
2011-06-08 00:07:59 +0000

    Enhancement #5142 System Integrity Incomplete Attachments script to clean up files of completed submissions. Backporting. See HEAD commit message.

--- scripts/system_integrity_incomplete_attachments.php
+++ scripts/system_integrity_incomplete_attachments.php
@@ -10,18 +10,19 @@
 * | you a copy.                                                        |
 * +--------------------------------------------------------------------+
 *
-* $Id: system_integrity_incomplete_attachments.php,v 1.4 2010/07/26 23:15:15 mhaidar Exp $
+* $Id: system_integrity_incomplete_attachments.php,v 1.4.6.1 2011/06/08 00:07:59 mhaidar Exp $
 *
 */
 
 /**
-* Report on the incomplete attachments that do NOT match an submission assets 
-*
+* 1- Report on the incomplete attachment directories that do NOT match submission assets. 
+* 2- Report on the incomplete attachment directories that are now complete submissions. These may or may not be empty directories.
+* 
 * Syntax: system_integrity_incomplete_attachments.php [Matrix_Root] [Action]
 * 		where [ACTION] is --fix (delete the attachments) or --check (just report)
 *
 * @author  Benjamin Pearson <bpearson@squiz.net>
-* @version $Revision: 1.4 $
+* @version $Revision: 1.4.6.1 $
 * @package MySource_Matrix
 */
 error_reporting(E_ALL);
@@ -51,9 +52,13 @@ ini_set('memory_limit', '-1');
 
 $count = 0;
 $count_rm = 0;
+$count_2 = 0;
+$count_rm_2 = 0;
+
 $form_assetids = $GLOBALS['SQ_SYSTEM']->am->getTypeAssetIds('form', FALSE);
 foreach ($form_assetids as $assetid) {
 	$asset = $GLOBALS['SQ_SYSTEM']->am->getAsset($assetid);
+	$complete_link   = $GLOBALS['SQ_SYSTEM']->am->getLink($assetid, SQ_LINK_TYPE_2, 'folder', TRUE, 'submissions_folder');
 	$path  = $asset->data_path;
 	$path .= '/incomplete_attachments';
 	$files = list_dirs($path);
@@ -64,7 +69,7 @@ foreach ($form_assetids as $assetid) {
 			$incomplete_submission = $GLOBALS['SQ_SYSTEM']->am->getAsset($incomplete_submission_assetid, '', TRUE);
 			if (is_null($incomplete_submission)) {
 				// Report only
-				echo 'Form #'.$assetid.' still has some incomplete attachments for Submission #'.$incomplete_submission_assetid.'.';
+				echo 'Form #'.$assetid.' still has some incomplete attachments for non-existent Submission #'.$incomplete_submission_assetid.'.';
 				$count++;
 				if ($CORRECT) {
 					// Remove the dir, not needed
@@ -75,7 +80,20 @@ foreach ($form_assetids as $assetid) {
 				}//end if
 				echo "\n";
 			} else {
-				$GLOBALS['SQ_SYSTEM']->am->forgetAsset($incomplete_submission_asset, TRUE);
+				$parents = $GLOBALS['SQ_SYSTEM']->am->getParents($incomplete_submission_assetid, 'folder', TRUE, NULL, NULL, TRUE, 1, 1);
+				if (!empty($complete_link) && !empty($parents) && array_key_exists($complete_link['minorid'], $parents)){
+					echo 'Form #'.$assetid.' still has some incomplete attachments for completed Submission #'.$incomplete_submission_assetid.'.';
+					$count_2++;
+					if ($CORRECT) {
+						// Remove the dir, not needed
+						echo ' Removing.';
+						delete_directory($path.'/'.$file);
+						$count_rm_2++;
+						echo ' Done.';
+					}//end if
+					echo "\n";
+				}
+				$GLOBALS['SQ_SYSTEM']->am->forgetAsset($incomplete_submission, TRUE);
 			}//end if
 			unset($incomplete_submission);
 		}//end if
@@ -84,7 +102,9 @@ foreach ($form_assetids as $assetid) {
 	unset($asset);
 }//end foreach
 
-echo "Incomplete attachments found:    ".$count."\n";
-echo "Incomplete attachments deleted:  ".$count_rm."\n";
+echo "Incomplete attachments for non-existent submissions found:    ".$count."\n";
+echo "Incomplete attachments for non-existent submissions deleted:  ".$count_rm."\n";
+echo "Incomplete attachments for completed submissions found:    ".$count_2."\n";
+echo "Incomplete attachments for completed submissions deleted:  ".$count_rm_2."\n";
 echo "All done!\n";
 ?>
