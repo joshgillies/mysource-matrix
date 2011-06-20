83a8837f975ed69a993c466f1a8abaf5d908aa9e
Ashish Karelia <akarelia@squiz.com.au>
2011-06-20 22:16:56 +0000

    Commit to bug #5169Typo in the permission change script. Fixed typo in status_change.php and permission_change.php scripts

--- scripts/status_change.php
+++ scripts/status_change.php
@@ -10,10 +10,10 @@
 * | you a copy.                                                        |
 * +--------------------------------------------------------------------+
 *
-* $Id: status_change.php,v 1.2 2010/06/25 03:11:12 bpearson Exp $
+* $Id: status_change.php,v 1.3 2011/06/20 22:13:11 akarelia Exp $
 *
 */
-ini_set('mem_limit', '-1');
+ini_set('memory_limit', '-1');
 error_reporting(E_ALL);
 if ((php_sapi_name() != 'cli')) {
 	trigger_error("You can only run this script from the command line\n", E_USER_ERROR);
