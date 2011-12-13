bcaa7211f2e17b9f3166dfc9635a5a0df89711ff
Christopher Smith <csmith@squiz.com.au>
2011-12-13 05:51:04 +0000

    bug #5482 - further improvements to the reimport content script. it now has a --report mode to show the number of pages that have a particular encoding (limited set of charsets are detected, but this can be expanded as required). if we detect a windows encoding (something with smart-quotes) or iso-8859-1, we can try to convert that to their utf-8 equivalent before updating the db. requires the iconv module to do that work.

--- scripts/system_reimport_content.php
+++ scripts/system_reimport_content.php
@@ -10,7 +10,7 @@
 * | you a copy.                                                        |
 * +--------------------------------------------------------------------+
 *
-* $Id: system_reimport_content.php,v 1.3 2011/11/29 22:26:44 csmith Exp $
+* $Id: system_reimport_content.php,v 1.4 2011/12/13 05:51:04 csmith Exp $
 *
 */
 
@@ -26,7 +26,7 @@
 *
 * Usage: php scripts/system_reimport_content.php [SYSTEM_ROOT]
 *
-* @version $Revision: 1.3 $
+* @version $Revision: 1.4 $
 * @package MySource_Matrix
 */
 
@@ -40,6 +40,11 @@ if (empty($SYSTEM_ROOT) || !is_dir($SYSTEM_ROOT)) {
 }
 $db_conf = require $SYSTEM_ROOT.'/data/private/conf/db.inc';
 
+$reportOnly = FALSE;
+if (isset($_SERVER['argv'][2]) && $_SERVER['argv'][2] == '--report') {
+	$reportOnly = TRUE;
+}
+
 function usage()
 {
 	global $db_conf;
@@ -47,6 +52,7 @@ function usage()
 	if (isset($db_conf['db']['encoding']) === TRUE) {
 		$encoding = $db_conf['db']['encoding'];
 	}
+	echo $_SERVER['argv'][0]." SYSTEM_ROOT [--report]\n";
 	echo "This script will re-import content from files into the database.\n";
 	echo "This is mainly useful for oracle systems where the wrong database\n";
 	echo "encoding has been used and needs to be changed.\n";
@@ -59,9 +65,9 @@ function usage()
 		echo "It is currently set to:".$encoding."\n";
 		echo "\n";
 	}
+	echo "If you pass --report to the script, a report will be generated based on the\n";
+	echo "number of pages this script will affect.\n\n";
 }
-
-usage();
 
 /*
  * Most commonly this script will be used to allow utf-8 chars, but in case it's not:
@@ -69,30 +75,33 @@ usage();
  * If it doesn't contain utf-8 chars, the database content is *not* updated.
  * If it does contain utf-8 chars, the content is updated.
  *
- * If checkUtf8 is FALSE, the update happens (the utf-8 character check is skipped).
+ * If encodeToUtf8 is FALSE, the update happens (the utf-8 character check is skipped).
  * If it's TRUE, the update is skipped IF there are no utf-8 chars in the content.
  *
  * If you're converting to something other than al32utf8, then the update will happen.
  * You can also overwrite this setting if you need to (small code change to comment out the encoding
  * check) so the updates happen no matter what.
  */
-$checkUtf8 = FALSE;
+$encodeToUtf8 = FALSE;
 $encoding = '(empty)';
 if (isset($db_conf['db']['encoding']) === TRUE) {
 	$encoding = $db_conf['db']['encoding'];
 }
 if ($db_conf['db']['type'] == 'oci' && strtolower($encoding) == 'al32utf8') {
-	$checkUtf8 = TRUE;
+	$encodeToUtf8 = TRUE;
 }
 
-// Make sure the person running the script understands
-// the consequences. Content *WILL* be overwritten.
-echo "OK to proceed (type 'yes' to continue): ";
-$response = trim(fgets(STDIN));
+if ($reportOnly === FALSE) {
+	usage();
+	// Make sure the person running the script understands
+	// the consequences. Content *WILL* be overwritten.
+	echo "OK to proceed (type 'yes' to continue): ";
+	$response = trim(fgets(STDIN));
 
-if ($response != 'yes') {
-	echo "Aborting.\n";
-	exit(1);
+	if ($response != 'yes') {
+		echo "Aborting.\n";
+		exit(1);
+	}
 }
 
 require_once $SYSTEM_ROOT.'/core/include/init.inc';
@@ -153,10 +162,29 @@ $count = count($results);
 $done  = 0;
 echo "Processing ".$count." assets .. \n";
 
+// Currently we're reporting on the following charsets:
+// utf-8
+// iso-8859-1
+// ascii
+// Windows-1252
+$charsetReport = array(
+		'ascii'        => 0,
+		'iso-8859-1'   => 0,
+		'utf-8'        => 0,
+		'windows-1252' => 0,
+);
+
+$start = time();
 foreach ($results as $assetInfo) {
 	$done++;
-	if ($done % 10 == 0) {
-		echo $done." assets complete.\r";
+	if ($done % 50 == 0) {
+	    $elapsed = time() - $start;
+	    if ($elapsed > 0) {
+			$frac = $done / $count;
+			$remain = $elapsed / $frac - $elapsed;
+			$pct = $frac * 100;
+			printf("%.2f%%: %d assets processed in %d seconds, %.2f remaining.\r", $pct, $done, $elapsed, $remain);
+	    }
 	}
 
 	$contextid = $assetInfo['contextid'];
@@ -177,33 +205,70 @@ foreach ($results as $assetInfo) {
 	if (file_exists($filename) === FALSE) {
 		continue;
 	}
-	$content = file_get_contents($filename);
+	$oldContent = file_get_contents($filename);
 
+	$contentIsUpdated = FALSE;
 	// If we're checking for utf-8 chars, do it here (and also log the outcome).
-	if ($checkUtf8 === TRUE) {
+	if ($encodeToUtf8 === TRUE) {
 		// If we don't find any utf-8 chars, we don't need to update the content.
-		$foundUtf8 = preg_match('%([\xc2-\xf4][\x80-\xbf]{1,3})%', $content);
-		log_write('Found utf8 characters in file '.$filename.': '.($foundUtf8 == TRUE ? 'yes' : 'no'), 'reimport_content');
-		if ($foundUtf8 == FALSE) {
+		if (preg_match('%([\xc2-\xf4][\x80-\xbf]{1,3})%', $oldContent)) {
+			# No need to convert content into UTF-8, but we do need to save it.
+			$contentIsUpdated = TRUE;
+			$content = $oldContent;
+			$charsetReport['utf-8']++;
+		} else if (preg_match('%([\x80-\x9f])%', $oldContent)) {
+			$charsetReport['windows-1252']++;
+			# Convert content from Windows-1252 to UTF-8
+			$content = iconv('Windows-1252','UTF-8',$oldContent);
+			if ($content !== $oldContent) {
+				$contentIsUpdated = TRUE;
+			}
+		} else if (preg_match('%([\xa0-\xff])%', $oldContent)) {
+			$charsetReport['iso-8859-1']++;
+			# Convert content from ISO-8859-1 to UTF-8
+			$content = iconv('ISO-8859-1','UTF-8',$oldContent);
+			if ($content !== $oldContent) {
+				$contentIsUpdated = TRUE;
+			}
+		} else {
+			# else it's 7-bit ASCII which we can ignore, but we need for our report.
+			$charsetReport['ascii']++;
+		}
+	} # Handle other encoding scenarios here.
+
+	if ($reportOnly === FALSE) {
+		if ($contentIsUpdated) { # We need up update the content in the database
+			log_write('Updating assetid '.$assetInfo['minorid'].' (context '.$contextid.') with content from '.$filename, 'reimport_content');
+			try {
+				$sql   = "UPDATE sq_ast_attr_val SET custom_val=:custom_val WHERE assetid=:assetid AND attrid=:attrid AND contextid=:contextid";
+				$query = MatrixDAL::preparePdoQuery($sql);
+				MatrixDAL::bindValueToPdo($query, 'assetid', $assetInfo['minorid'], PDO::PARAM_STR);
+				MatrixDAL::bindValueToPdo($query, 'attrid',  $attributeid, PDO::PARAM_INT);
+				MatrixDAL::bindValueToPdo($query, 'contextid',  $contextid, PDO::PARAM_INT);
+				MatrixDAL::bindValueToPdo($query, 'custom_val', $content, PDO::PARAM_LOB);
+				MatrixDAL::execPdoQuery($query);
+			} catch (DALException $e) {
+				throw new Exception('Unable to update asset '.$assetInfo['minorid'].' with content from '.$filename.': '.$e->getMessage());
+			}
+		} else {
 			log_write('Updating of assetid '.$assetInfo['minorid'].' (context '.$contextid.') skipped', 'reimport_content');
-			continue;
 		}
 	}
+}
 
-	log_write('Updating assetid '.$assetInfo['minorid'].' (context '.$contextid.') with content from '.$filename, 'reimport_content');
-	try {
-		$sql   = "UPDATE sq_ast_attr_val SET custom_val=:custom_val WHERE assetid=:assetid AND attrid=:attrid AND contextid=:contextid";
-		$query = MatrixDAL::preparePdoQuery($sql);
-		MatrixDAL::bindValueToPdo($query, 'assetid', $assetInfo['minorid'], PDO::PARAM_STR);
-		MatrixDAL::bindValueToPdo($query, 'attrid',  $attributeid, PDO::PARAM_INT);
-		MatrixDAL::bindValueToPdo($query, 'contextid',  $contextid, PDO::PARAM_INT);
-		MatrixDAL::bindValueToPdo($query, 'custom_val', $content, PDO::PARAM_LOB);
-		MatrixDAL::execPdoQuery($query);
-	} catch (DALException $e) {
-		throw new Exception('Unable to update asset '.$assetInfo['minorid'].' with content from '.$filename.': '.$e->getMessage());
+$elapsed = time() - $start;
+printf("%.2f%%: %d assets processed in %d seconds, 0 remaining.\n", 100, $done, $elapsed);
+
+foreach ($charsetReport as $charset => $count) {
+	if ($count > 0) {
+		echo "Found ".number_format($count)." pages with character set ".$charset.".";
+		if ($reportOnly === FALSE) {
+			echo " These were converted to UTF-8.";
+		}
+		echo "\n";
 	}
 }
-echo $done." assets complete.\n";
+
 echo "All done!\n";
 
 $GLOBALS['SQ_SYSTEM']->doTransaction('COMMIT');
