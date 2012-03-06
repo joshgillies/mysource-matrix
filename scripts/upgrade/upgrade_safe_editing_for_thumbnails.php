62d63966e3a839f74cdaa4b96d319bff78331389
Ashish Karelia <akarelia@squiz.com.au>
2012-03-06 05:58:28 +0000

    Additional script to support feature #3632 When reverting to live version of asset in workflow asset thumbnail doesn't revert. This script will pick all the assets in safe-editing during upgrade and see for any thumbnails attached. If found, will process these notice links

---
+++ scripts/upgrade/upgrade_safe_editing_for_thumbnails.php
@@ -0,0 +1,117 @@
+<?php
+/**
+* +--------------------------------------------------------------------+
+* | This MySource Matrix CMS file is Copyright (c) Squiz Pty Ltd       |
+* | ACN 084 670 600                                                    |
+* +--------------------------------------------------------------------+
+* | IMPORTANT: Your use of this Software is subject to the terms of    |
+* | the Licence provided in the file licence.txt. If you cannot find   |
+* | this file please contact Squiz (www.squiz.net) so we may provide   |
+* | you a copy.                                                        |
+* +--------------------------------------------------------------------+
+*
+* $Id: upgrade_safe_editing_for_thumbnails.php,v 1.2.2.2 2012/03/06 05:58:28 akarelia Exp $
+*
+*/
+
+/**
+* Purpose : 
+*			This script will pick up all the assets that are in safe edit 
+* 			and check if they have thumbnails on it. If they do it will
+*			write the link information to the .sq_notice_links file
+*
+*
+* @author Ash Karelia <akarelia@squiz.com.au>
+* @version $Revision: 1.2.2.2 $
+* @package MySource_Matrix
+*/
+error_reporting(E_ALL);
+if ((php_sapi_name() != 'cli')) {
+	trigger_error("You can only run this script from the command line\n", E_USER_ERROR);
+}
+
+$SYSTEM_ROOT = (isset($_SERVER['argv'][1])) ? $_SERVER['argv'][1] : '';
+if (empty($SYSTEM_ROOT) || !is_dir($SYSTEM_ROOT)) {
+	trigger_error("You need to supply the path to the System Root as the first argument\n", E_USER_ERROR);
+}
+
+require_once $SYSTEM_ROOT.'/core/include/init.inc';
+require_once SQ_FUDGE_PATH.'/general/file_system.inc';
+
+// log in as root
+$root_user = $GLOBALS['SQ_SYSTEM']->am->getSystemAsset('root_user');
+if (!$GLOBALS['SQ_SYSTEM']->setCurrentUser($root_user)) {
+	trigger_error("Failed logging in as root user\n", E_USER_ERROR);
+}
+
+// forced run level
+$GLOBALS['SQ_SYSTEM']->setRunLevel(SQ_RUN_LEVEL_FORCED);
+
+
+$am = $GLOBALS['SQ_SYSTEM']->am;
+$count = 0;
+$done_asset = 1;
+$bad_assetids = Array();
+
+// get all the assets in the system who are in safe edit status and have a thumbnail attached to it
+$sql = "SELECT assetid from sq_ast where status = '64' and assetid IN (select majorid from sq_ast_lnk where value = 'thumbnail');";
+
+$results = MatrixDAL::executeSqlAssoc($sql);
+echo "Found ".count($results)." assets that are in 'Safe Editing' status and has thumbnail applied.\n";
+
+foreach ($results as $result) {
+	$content = Array();
+	echo "Updating $done_asset of ".count($results)." Assets\r";
+	$assetid = $result['assetid'];
+	$asset   = $GLOBALS['SQ_SYSTEM']->am->getAsset($assetid);
+	$notice_link = $GLOBALS['SQ_SYSTEM']->am->getLink($assetid, SQ_LINK_NOTICE, '', TRUE, 'thumbnail');
+
+	// if file exists, write to it else create new one
+	if (file_exists($asset->data_path.'/.sq_system/.sq_notice_links')) {
+		$content = unserialize(file_to_string($asset->data_path.'/.sq_system/.sq_notice_links'));
+
+		$thumbnail_exists = FALSE;
+		foreach ($content as $link) {
+			if ($link['value'] == 'thumbnail') {
+				$thumbnail_exists = TRUE;
+				break;
+			}
+		}
+
+		$content[] = $notice_link;
+		if (!$thumbnail_exists && !string_to_file(serialize($content), $asset->data_path.'/.sq_system/.sq_notice_links')) {
+			print_r('Still writing :(');
+			$count++;
+			$bad_assetids[] = $assetid;
+		}
+	} else {
+		$content[] = $notice_link;
+		if (!string_to_file(serialize($content), $asset->data_path.'/.sq_system/.sq_notice_links')) {
+			$count++;
+			$bad_assetids[] = $assetid;
+		}
+	}
+	$done_asset++;
+
+	unset($assetid);
+}
+
+
+// Warn the user if an error happened
+if (!empty($count)) {
+	echo "There were $count errors in creating .sq_notice_links files.\nThese assets may need to be reviewed manually.\n";
+	echo "Assetids that need to be looked into (These are in error.log too):\n";
+	print_r($bad_assetids);
+
+	// put them in error.log too
+	log_dump("There were ".$count." errors in creating .sq_notice_links files.\nThese assets may need to be reviewed manually.");
+	log_dump("Assetids that need to be looked into :");
+	log_dump($bad_assetids);
+} else {
+	echo "Successfully upgraded ".count($results)." assets.\n";
+}
+
+$GLOBALS['SQ_SYSTEM']->restoreRunLevel();
+
+
+?>
