aa38ebe6f681de91c9b9694c1fccfd58fd69d2fe
Luke Wright <lwright@squiz.com.au>
2013-12-06 13:45:08 +1100

    Fixes for bugs #6696 #6709 #6711, backported to 4.18. (Dev commits are 6340fcca...192e7245.)
    Bug summaries:
      .. Because it was in the same window, it caused issues.
      .. Moved the asset map into an iframe and made appropriate adjustments to code calling it.
    
      .. By "not loading", it means "it loads the Java asset map instead".
      .. This causes IE to return a document mode of "5" because it's using IE5.5 rendering.
      .. Made sure the new iframe contained a doctype (just used HTML5); added one to the main frame too.
    
      .. because it was always using _admin for asset map requests.
      .. Use whatever the backend suffix was when the asset map was loaded instead.

---
+++ fudge/wysiwyg/plugins/matrix_insert_image/insert_image_asset_map.php
@@ -0,0 +1,83 @@
+<?php
+/**
+* +--------------------------------------------------------------------+
+* | This MySource Matrix CMS file is Copyright (c) Squiz Pty Ltd       |
+* | ABN 77 084 670 600                                                 |
+* +--------------------------------------------------------------------+
+* | IMPORTANT: Your use of this Software is subject to the terms of    |
+* | the Licence provided in the file licence.txt. If you cannot find   |
+* | this file please contact Squiz (www.squiz.com.au) so we may provide|
+* | you a copy.                                                        |
+* +--------------------------------------------------------------------+
+*
+* $Id: insert_link.php,v 1.62 2013/09/15 23:53:23 lwright Exp $
+*
+*/
+
+/**
+* Insert Link Popup for the WYSIWYG
+*
+* @author  Greg Sherwood <gsherwood@squiz.net>
+* @version $Revision: 1.62 $
+* @package MySource_Matrix
+*/
+
+require_once dirname(__FILE__).'/../../../../core/include/init.inc';
+require_once SQ_LIB_PATH.'/html_form/html_form.inc';
+require_once SQ_FUDGE_PATH.'/var_serialise/var_serialise.inc';
+
+if (empty($GLOBALS['SQ_SYSTEM']->user) || !($GLOBALS['SQ_SYSTEM']->user->canAccessBackend() || $GLOBALS['SQ_SYSTEM']->user->type() == 'simple_edit_user' || (method_exists($GLOBALS['SQ_SYSTEM']->user, 'isShadowSimpleEditUser') && $GLOBALS['SQ_SYSTEM']->user->isShadowSimpleEditUser()))) {
+	exit;
+}
+?><!DOCTYPE html>
+<html>
+	<head>
+		<title>Insert Image</title>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<?php
+		// add required js translation files, as we are using asset finder
+		$include_list = Array(sq_web_path('lib').'/js/translation.js');
+
+		$locales = $GLOBALS['SQ_SYSTEM']->lm->getCumulativeLocaleParts($GLOBALS['SQ_SYSTEM']->lm->getCurrentLocale());
+
+		foreach ($locales as $locale) {
+			if (file_exists(SQ_DATA_PATH.'/public/system/core/js_strings.'.$locale.'.js')) {
+				$include_list[] = sq_web_path('data').'/system/core/js_strings.'.$locale.'.js';
+			}
+		}
+
+		foreach ($include_list as $link) {
+			?><script type="text/javascript" src="<?php echo $link; ?>"></script>
+		<?php
+		}
+		?>
+		<script type="text/javascript" src="../../core/popup.js"></script>
+		<script type="text/javascript" src="<?php echo sq_web_path('lib').'/asset_map/javaExternalCall.js' ?>"></script>
+		<script type="text/javascript" src="<?php echo sq_web_path('fudge').'/var_serialise/var_serialise.js' ?>"></script>
+		<script type="text/javascript" src="<?php echo sq_web_path('lib').'/html_form/html_form.js' ?>"></script>
+		<script type="text/javascript" src="<?php echo sq_web_path('lib').'/js/general.js' ?>"></script>
+		<script type="text/javascript" src="<?php echo sq_web_path('lib').'/js/tooltip.js' ?>"></script>
+		<script type="text/javascript" src="<?php echo sq_web_path('lib').'/web/dfx/dfx.js' ?>"></script>
+		<link rel="stylesheet" type="text/css" href="<?php echo sq_web_path('lib').'/asset_map/js/js_asset_map.css' ?>" />
+
+		<style type="text/css">
+			html, body {
+				background: #FCFCFC;
+				color: #000000;
+				font: 11px Tahoma,Verdana,sans-serif;
+				margin: 0px;
+				padding: 0px;
+			}
+		</style>
+	</head>
+
+	<body>
+	    <div id="asset_map">
+		<?php
+            include_once(SQ_LIB_PATH.'/asset_map/asset_map.inc');
+            $asset_map = new Asset_Map();
+            $asset_map->embedAssetMap('simple', 200, 350);
+        ?>
+        </div>
+    </body>
+</html>
