10773631ba1af73b2ca30681011d880f924142b3
Edison Wang <xwan510@gmail.com>
2014-02-07 12:20:51 +1100

    Support Viper in backend admin. First stage commit. All should work, expect a few minor issues with some plugins.

--- core/lib/js/email_format.js
+++ core/lib/js/email_format.js
@@ -35,39 +35,48 @@ function emailFormatSwitchReadingMode(editor_name, html_label, text_label) {
 
 var initialisedEmailEditors = new Array();
 function emailFormatSwitchEditingMode(editor_name, html_label, text_label) {
-	var editor = eval('editor_' + editor_name);
-
+	
 	var textDiv = document.getElementById(editor_name + "_text_body_div");
 	var htmlDiv = document.getElementById(editor_name + "_html_body_div");
+	var viperDiv = document.getElementById(editor_name + "_contents_div_viper");
 	var formatSpan = document.getElementById(editor_name + "_format_mode");
 
 	if (htmlDiv.style.display == "none") {
-		var setDesignMode = true;
+		if(typeof Matrix_Viper === 'undefined')  {
+		    var editor = eval('editor_' + editor_name);
+		    var setDesignMode = true;
 
-		// initilise the wysiwg if this is the first time
-		// it is being shown - skip this otherwise
-		if (initialisedEmailEditors[editor._uniqueID] == null) {
-			initialisedEmailEditors[editor._uniqueID] = true;
-			editor.generate();
-			setDesignMode = false;
-		} else if (editor._initialised != true) {
-			return;
+		    // initilise the wysiwg if this is the first time
+		    // it is being shown - skip this otherwise
+		    if (initialisedEmailEditors[editor._uniqueID] == null) {
+			    initialisedEmailEditors[editor._uniqueID] = true;
+			    editor.generate();
+			    setDesignMode = false;
+		    } else if (editor._initialised != true) {
+			    return;
+		    }
+
+
+		    // if we are using an iframe for this editor, we set its designMode property if we need to
+		    if (editor._iframe) {
+			    editor._iframe.style.width = editor.config.width;
+			    if (editor._iframe.contentWindow.document.designMode) {
+				    editor._iframe.contentWindow.document.designMode = "on";
+			    }
+			    editor._iframe.style.height = editor.config.height;
+		    }
 		}
-		textDiv.style.display = "none";
-		htmlDiv.style.display = "";
-		formatSpan.innerHTML = html_label; //'HTML Email Version';
-
-		// if we are using an iframe for this editor, we set its designMode property if we need to
-		if (editor._iframe) {
-			editor._iframe.style.width = editor.config.width;
-			if (editor._iframe.contentWindow.document.designMode) {
-				editor._iframe.contentWindow.document.designMode = "on";
-			}
-			editor._iframe.style.height = editor.config.height;
+		else {
+		    // init viper editor
+		     //Matrix_Viper.viper.setEditableElement(viperDiv);
 		}
+		
+		    textDiv.style.display = "none";
+		    htmlDiv.style.display = "";
+		    formatSpan.innerHTML = html_label; //'HTML Email Version';
 
 
-	} else if (editor._initialised == true) {
+	} else {
 
 		textDiv.style.display = "";
 		htmlDiv.style.display = "none";
