56d1eaf323735362679e2a8ad36c510c4abd6442
Luke Wright <lwright@squiz.com.au>
2014-09-12 13:38:48 +1000

    Next stage of string collection: adding the Display Name and Notes from (newly-merged) Edit Interface files into.
    Looking at close to 9,000 localisable strings in Matrix's core and basic
    packages - so far.

--- Translation/get_strings.php
+++ Translation/get_strings.php
@@ -9,69 +9,73 @@
         $INCLUDE[] = str_replace($DIRNAME.'/', '', $pkg);
     }
 
-    print_r($INCLUDE);
-
+    $all_strings = [];
     foreach ($INCLUDE as $inc_dir) {
+        $all_strings[$inc_dir] = [];
+        $strings =& $all_strings[$inc_dir];
+
+        $output  = [];
         exec('find '.$DIRNAME.'/'.$inc_dir.' -name lang_strings.xml', $output);
-    }
 
-    $strings = [];
-    $output  = [];
-    foreach ($output as $line) {
-        $sxml = simplexml_load_file($line);
-        foreach ($sxml->string as $string) {
-            $code = (string) $string->attributes()->source;
-            if (!empty($strings[$code]) && ($strings[$code] !== (string) $string)) {
-                echo 'DUPLICATE: '.$code."\n";
-                // echo 'DUPLICATE: '.$code.' (old: '.$strings[$code].', new: '.((string) $string).')'."\n";
-            } else {
-                $strings[$code] = (string) $string;
+        foreach ($output as $line) {
+            $sxml = simplexml_load_file($line);
+            foreach ($sxml->string as $string) {
+                $code = (string) $string->attributes()->source;
+                $strings[] = trim((string) $string);
             }
         }
-    }
-    $strings = array_unique($strings);
-    //print_r($strings);
-    //print_r($output);
 
-    $messages_subj = [];
-    $messages_body = [];
-    $output        = [];
-    foreach ($INCLUDE as $inc_dir) {
+        $output = [];
         exec('find '.$DIRNAME.'/'.$inc_dir.' -name lang_messages.xml', $output);
-    }
 
-    foreach ($output as $line) {
-        $sxml = simplexml_load_file($line);
-        foreach ($sxml->message as $msg) {
-            $code = (string) $msg->attributes()->type;
-            if (!empty($messages_subj[$code])) {
-                echo 'DUPLICATE MESSAGE: '.$code."\n";
-            } else {
-                $messages_subj[$code] = (string) $msg->subject;
-                $messages_body[$code] = (string) $msg->body;
+        foreach ($output as $line) {
+            $sxml = simplexml_load_file($line);
+            foreach ($sxml->message as $msg) {
+                $code = (string) $msg->attributes()->type;
+                $string[] = trim((string) $msg->subject);
+                $string[] = trim((string) $msg->body);
             }
         }
-    }
-    //print_r(implode(', ', array_keys($messages_subj)));
 
-    $errors = [];
-    $output = [];
-    foreach ($INCLUDE as $inc_dir) {
+        $output = [];
         exec('find '.$DIRNAME.'/'.$inc_dir.' -name lang_errors.xml', $output);
-    }
 
-    foreach ($output as $line) {
-        $sxml = simplexml_load_file($line);
-        foreach ($sxml->error as $string) {
-            $code = (string) $string->attributes()->code;
-            if (!empty($errors[$code])) {
-                echo 'DUPLICATE ERROR: '.$code.' (old: '.$errors[$code].', new: '.((string) $string).')'."\n";
-            } else {
-                $errors[$code] = (string) $string;
+        foreach ($output as $line) {
+            $sxml = simplexml_load_file($line);
+            foreach ($sxml->error as $string) {
+                $code = (string) $string->attributes()->code;
+                $strings[] = trim((string) $string);
             }
         }
-    }
-    $errors = array_unique($errors);
+
+        $output = [];
+        exec('find '.$DIRNAME.'/'.$inc_dir.' -name edit_interface_*screen*.xml', $output);
+
+        foreach ($output as $line) {
+            $sxml = simplexml_load_file($line);
+            foreach ($sxml->xpath('//display_name') as $string) {
+                $code = (string) $string->attributes()->code;
+                $strings[] = trim((string) $string);
+            }
+
+            foreach ($sxml->xpath('//note') as $string) {
+                $code = (string) $string->attributes()->code;
+                $strings[] = trim((string) $string);
+            }
+        }
+
+        $strings = array_values($strings);
+        $strings = array_unique($strings);
+        if ($inc_dir !== 'core') {
+            $strings = array_diff($strings, $all_strings['core']);
+        }
+
+        echo str_pad($inc_dir, 35).' : '.number_format(count($strings))."\n";
+    }//end for
+
+    echo str_pad('Total', 35).' : '.number_format(array_reduce($all_strings, function($carry, $next) {
+        return $carry + count($next);
+    }, 0))."\n";
     //print_r($errors);
     //print_r($output);
 ?>
